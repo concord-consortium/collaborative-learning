89ec0f5477ec7231654564ade0ee6e697ccada9e
Doug Martin <doug@zoopdoop.com>
2018-08-28 10:02:08 -0400

    Add hot.accept

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { Provider } from "mobx-react";
+import { destroy, getSnapshot } from "mobx-state-tree";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
@@ -20,42 +21,83 @@ export interface IAllStores {
 const host = window.location.host.split(":")[0];
 const devMode = (host === "localhost") || (host === "127.0.0.1");
 
-const user = UserModel.create({
-  authenticated: devMode,
-  name: devMode ? "Jane Q. Developer" : null,
-});
+const stores: IAllStores = {
+  devMode,
 
-const problem = ProblemModel.create({
-  name: "Sample Problem",
-  sections: [
-    {
-      name: "Introduction",
-      shortName: "In",
-    },
-    {
-      name: "Initial Challenge",
-      shortName: "IC",
-    },
-    {
-      name: "What If...?",
-      shortName: "W?",
-    },
-    {
-      name: "Now What Do You Know?",
-      shortName: "N?",
-    },
-  ],
-});
+  user: UserModel.create({
+    authenticated: devMode,
+    name: devMode ? "Jane Q. Developer" : null,
+  }),
 
-const ui = UIModel.create({
-  learningLogExpanded: false,
-  leftNavExpanded: false,
-  myWorkExpanded: false,
-});
+  problem: ProblemModel.create({
+    name: "Sample Problem",
+    sections: [
+      {
+        name: "Introduction",
+        shortName: "In",
+      },
+      {
+        name: "Initial Challenge",
+        shortName: "IC",
+      },
+      {
+        name: "What If...?",
+        shortName: "W?",
+      },
+      {
+        name: "Now What Do You Know?",
+        shortName: "N?",
+      },
+    ],
+  }),
 
-ReactDOM.render(
-  <Provider devMode={devMode} user={user} problem={problem} ui={ui}>
-    <AppComponent />
-  </Provider>,
-  document.getElementById("app"),
-);
+  ui: UIModel.create({
+    learningLogExpanded: false,
+    leftNavExpanded: false,
+    myWorkExpanded: false,
+  }),
+};
+
+const renderApp = () => {
+  ReactDOM.render(
+    <Provider devMode={devMode} user={stores.user} problem={stores.problem} ui={stores.ui}>
+      <AppComponent />
+    </Provider>,
+    document.getElementById("app"),
+  );
+};
+
+renderApp();
+
+if (devMode) {
+  debugger;
+  const hot = (module as any).hot;
+  if (hot) {
+    hot.accept();
+    /*
+    hot.accept(["./src/components/app.tsx"], () => {
+      debugger;
+      renderApp();
+    });
+
+    hot.accept(["./models/problem", "./models/ui", "./models/user"], () => {
+      debugger;
+      const snapshots = {
+        problem: getSnapshot(stores.problem),
+        ui: getSnapshot(stores.ui),
+        user: getSnapshot(stores.user),
+      };
+
+      destroy(stores.problem);
+      destroy(stores.ui);
+      destroy(stores.user);
+
+      stores.problem = ProblemModel.create(snapshots.problem);
+      stores.ui = UIModel.create(snapshots.ui);
+      stores.user = UserModel.create(snapshots.user);
+
+      renderApp();
+    });
+    */
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = (env, argv) => {
               plugins: [
                 // plugin-proposal-decorators is only needed if you're using experimental decorators in TypeScript
                 ["@babel/plugin-proposal-decorators", { legacy: true }],
-                //["@babel/plugin-proposal-class-properties", { loose: true }],
+                ["@babel/plugin-proposal-class-properties", { loose: true }],
                 "react-hot-loader/babel"
               ]
             }
