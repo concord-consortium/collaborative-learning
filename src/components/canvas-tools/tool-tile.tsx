89bea0a9f159167bc5714824eb1ea08624cb5240
Kirk Swenson <kswenson@concord.org>
2018-09-19 12:49:58 -0700

    [With Dave] Support dragging tool tiles [#159979496]

--- src/components/canvas-tools/tool-tile.tsx
+++ src/components/canvas-tools/tool-tile.tsx
@@ -1,11 +1,13 @@
-import { observer } from "mobx-react";
 import * as React from "react";
+import { observer } from "mobx-react";
+import { getSnapshot } from "mobx-state-tree";
 import { ToolTileModelType } from "../../models/tools/tool-tile";
 import { kGeometryToolID } from "../../models/tools/geometry/geometry-content";
 import { kTableToolID } from "../../models/tools/table/table-content";
 import { kTextToolID } from "../../models/tools/text/text-content";
 import GeometryToolComponent from "./geometry-tool";
 import TextToolComponent from "./text-tool";
+import { cloneDeep } from "lodash";
 
 interface IProps {
   model: ToolTileModelType;
@@ -16,6 +18,17 @@ interface IProps {
 export class ToolTileComponent extends React.Component<IProps, {}> {
 
   public render() {
+    return (
+      <div className="tool-tile-component"
+        onDragStart={this.handleToolDragStart}
+        draggable={true}
+      >
+        {this.renderTile()}
+      </div>
+    );
+  }
+
+  private renderTile() {
     const { model } = this.props;
     switch (model.content.type) {
       case kGeometryToolID: return this.renderGeometryTile();
@@ -23,6 +36,13 @@ export class ToolTileComponent extends React.Component<IProps, {}> {
       case kTextToolID: return this.renderTextTile();
     }
     return this.renderUnknownTile();
+  }
+
+  private handleToolDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    const snapshot = cloneDeep(getSnapshot(this.props.model));
+    delete snapshot.id;
+    const dragData = JSON.stringify(snapshot);
+    e.dataTransfer.setData("org.concord.clue.tile", dragData);
   }
 
   private renderGeometryTile() {
