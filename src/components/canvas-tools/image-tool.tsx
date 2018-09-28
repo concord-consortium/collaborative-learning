b577d83de17e4b655f7cde2319459ff0ee7af51c
dalove1025 <dalove1025@gmail.com>
2018-09-28 10:38:21 -0400

    Move selection handler to enclosing div

--- src/components/canvas-tools/image-tool.tsx
+++ src/components/canvas-tools/image-tool.tsx
@@ -17,23 +17,18 @@ interface IProps {
 export default class ImageToolComponent extends BaseComponent<IProps, {}> {
 
   public render() {
-    const { readOnly, model } = this.props;
-    const { content } = model;
-    const { ui } = this.stores;
+    const { readOnly, model: { content } } = this.props;
     const imageContent = content as ImageContentModelType;
     const editableClass = readOnly ? "read-only" : "editable";
     const classes = `image-tool ${editableClass}`;
     return (
-      <div className={classes}>
-        <img
-          src={imageContent.url}
-          onMouseDown={this.handleMouseDown}
-        />
+      <div className={classes} onMouseDown={this.handleMouseDown}>
+        <img src={imageContent.url} />
       </div>
     );
   }
 
-  private handleMouseDown = (e: React.MouseEvent<HTMLImageElement>) => {
+  private handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
     this.stores.ui.setSelectedTile(this.props.model);
   }
 }
