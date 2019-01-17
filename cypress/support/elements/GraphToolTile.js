cfc03275db9a48e1dfe4a2bff364434a20133cbf
eireland <eireland@concord.org>
2019-01-17 15:02:22 -0800

    Adding graph test cases
    Added test cases for restore of angles and rotation of polygon
    Added a todo for drag and drop of a polygon
    Added test scripts for angles and deleting graph points
    Removed graph tile related selectors from Canvas element file

--- cypress/support/elements/GraphToolTile.js
+++ cypress/support/elements/GraphToolTile.js
@@ -1,4 +1,14 @@
+const graphUnit=18.3;
+
 class GraphToolTile{
+    transformCoordinate(axis, num){
+        if (axis=='x'){
+            return (num+1)*18.3;
+        }
+        if (axis=='y'){
+            return 320-((num+1.2)*18.3);
+        }
+    }
 
     getBottomNavExpandedSpace(){
         return cy.get('.bottom-nav.expanded');
@@ -15,25 +25,50 @@ class GraphToolTile{
     getGraphPoint(){
         return cy.get('.geometry-tool.editable > svg > g > ellipse');
     }
+    selectGraphPoint(x,y){
+        let transX=this.transformCoordinate('x', x),
+            transY=this.transformCoordinate('y', y);
+
+        this.getGraphTile().last().click(transX,transY, {force:true});
+    }
 
     getGraphPointID(){
+        let pointId='';
          cy.get('.geometry-tool.editable > svg > g > ellipse').last()
             .then(($el)=>{
                 return $el.attr('id');
-            });
+            }).then((id)=>{
+                pointId=id;
+                cy.log('in getGraphPointId, pointId: '+pointId);
+         });
+         return pointId;
+
     }
     getGraphPolygon(){
         return cy.get('.geometry-tool.editable > svg > g > polygon');
     }
 
     addPointToGraph(x,y){
-        this.getGraphTile().last().click(x,y, {force:true});
+        let transX=this.transformCoordinate('x', x),
+            transY=this.transformCoordinate('y', y);
+
+        this.getGraphTile().last().click(transX,transY, {force:true});
     }
 
     getRotateTool(){
         return cy.get('.rotate-polygon-icon.enabled');
     }
 
-
+    getGraphToolMenuIcon(){
+        return cy.get('.geometry-menu-button')
+    }
+    showAngle(){
+        this.getGraphToolMenuIcon().click();
+        cy.get('.bp3-text-overflow-ellipsis').contains('Show Angle').click();
+    }
+    hideAngle(){
+        this.getGraphToolMenuIcon().click();
+        cy.get('.bp3-text-overflow-ellipsis').contains('Hide Angle').click();
+    }
 }
 export default GraphToolTile;
