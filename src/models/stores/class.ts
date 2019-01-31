4c762e6964bd30ef1b05b3fb04363a3e3934d277
Eli Kosminsky <ekosminsky@concord.org>
2019-01-31 14:42:06 -0800

    Showing last names for teacher logs

--- src/models/stores/class.ts
+++ src/models/stores/class.ts
@@ -1,20 +1,53 @@
 import { types } from "mobx-state-tree";
 import { ClassInfo } from "../../lib/auth";
 
+export enum ClassUserType {
+  teacher = "teacher",
+  student = "student"
+}
+
 export const ClassStudentModel = types
   .model("ClassStudent", {
+    type: ClassUserType.student,
     id: types.string,
     firstName: types.string,
     lastName: types.string,
     fullName: types.string,
     initials: types.string,
+  })
+  .views((self) => {
+    return {
+      get displayName() {
+        return self.fullName;
+      }
+    };
   });
+
+export const ClassTeacherModel = types
+  .model("ClassTeacer", {
+    type: ClassUserType.teacher,
+    id: types.string,
+    firstName: types.string,
+    lastName: types.string,
+    fullName: types.string,
+    initials: types.string,
+  })
+  .views((self) => {
+    return {
+      get displayName() {
+        return `Teacher ${self.lastName}`;
+      }
+    };
+  });
+
+export const ClassUserModel = types.union(ClassStudentModel, ClassTeacherModel);
 
 export const ClassModel = types
   .model("Class", {
     name: types.string,
     classHash: types.string,
     students: types.array(ClassStudentModel),
+    teachers: types.array(ClassTeacherModel),
   })
   .actions((self) => {
     return {
@@ -32,14 +65,33 @@ export const ClassModel = types
             });
           })
         );
+        self.teachers.replace(
+          classInfo.teachers.map((teacher) => {
+            return ClassTeacherModel.create({
+              id: teacher.id,
+              firstName: teacher.firstName,
+              lastName: teacher.lastName,
+              fullName: teacher.fullName,
+              initials: teacher.initials,
+            });
+          })
+        );
       }
     };
   })
   .views((self) => {
+    const getStudentById = (uid: string) => {
+      return self.students.find((student) => student.id === uid);
+    };
+    const getTeacherById = (uid: string) => {
+      return self.teachers.find((teacher) => teacher.id === uid);
+    };
     return {
-      getStudentById(uid: string) {
-        return self.students.find((student) => student.id === uid);
-      }
+      getStudentById,
+      getTeacherById,
+      getUserById(uid: string) {
+        return getStudentById(uid) || getTeacherById(uid);
+      },
     };
   });
 
