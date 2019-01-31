6e628ddb6bd9d36c864f7e10902e8ea525fb50e6
eireland <eireland@concord.org>
2019-01-31 15:45:23 -0800

    Moved adding content to student workspaces to this test instead of in setup command

--- cypress/integration/group_test_spec.js
+++ cypress/integration/group_test_spec.js
@@ -9,13 +9,29 @@ context('Test group functionalities', function(){
     let qaClass = 10,
         qaOffering = 10,
         qaGroup = 10,
-        problem = 2.3,
+        problem = 3.3,
         studentArr=[15,16,17,18];
     context('test the views', function(){
         describe('set-up for 4-up view tests', function(){
-            it('will enter text into the 1-up canvas', function(){
+            it('will set up groups', function(){
                 cy.setupGroup(studentArr, qaGroup)
             });
+
+            it('will add content to each student canvas', function(){
+                let i=0;
+                for (i=0; i<studentArr.length; i++){
+                    cy.visit(baseUrl+'?appMode=qa&qaGroup='+qaGroup+'&fakeClass='+qaClass+'&fakeUser=student:'+studentArr[i]+'&problem='+problem);
+                    leftNav.openToWorkspace('Now What');
+                    cy.wait(3000);
+                    canvas.addTextTile();
+                    canvas.enterText('This is to test the 4-up view of S'+studentArr[i]);
+                    canvas.getTextTile().last().should('contain', '4-up').and('contain','S'+studentArr[i]);
+                    canvas.addGraphTile();
+                    canvas.shareCanvas();//all students will share their canvas
+                    cy.wait(1000);
+                }
+            });
+
             it('verify 4-up view comes up correctly with students', function(){
                 canvas.openFourUpView();
                 canvas.getFourToOneUpViewToggle().should('be.visible');
@@ -65,46 +81,46 @@ context('Test group functionalities', function(){
             });
 
             //TODO: have to figure out drag and drop
-            it('will copy text from one canvas to own canvas', function(){
-                cy.log('need to write this test');
-            });
+            // it('will copy text from one canvas to own canvas', function(){
+            //     cy.log('need to write this test');
+            // });
         });
 
-        describe('test sharing and unsharing canvases', function(){
-            it('will verify canvas is visible in groupmates 4-up view', function(){ //canvas is shared during set up
-                cy.log('need to write this test');
-            });
-            it('will unshare canvas and verify canvas is not visible in groupmates 4-up view', function(){
-                cy.log('need to write this test');
-            });
-            it('restore a 4-up canvas where a groupmate has shared a canvas while it was not open', function(){
-                cy.log('need to write this test');
-            });
-            it('restore a 4-up canvas where a groupmate has unshared a canvas while it was not open', function(){
-                cy.log('need to write this test');
-            });
-            it('will open a new 4-up canvas with shared canvas from other students updated', function(){
-                cy.log('need to write this test');
-            });
-        });
+        // describe('test sharing and unsharing canvases', function(){
+        //     it('will verify canvas is visible in groupmates 4-up view', function(){ //canvas is shared during set up
+        //         cy.log('need to write this test');
+        //     });
+        //     it('will unshare canvas and verify canvas is not visible in groupmates 4-up view', function(){
+        //         cy.log('need to write this test');
+        //     });
+        //     it('restore a 4-up canvas where a groupmate has shared a canvas while it was not open', function(){
+        //         cy.log('need to write this test');
+        //     });
+        //     it('restore a 4-up canvas where a groupmate has unshared a canvas while it was not open', function(){
+        //         cy.log('need to write this test');
+        //     });
+        //     it('will open a new 4-up canvas with shared canvas from other students updated', function(){
+        //         cy.log('need to write this test');
+        //     });
+        // });
 
-        describe('test copy and paste from another canvas to another canvas', function(){
-            it('verify that student can copy text field from another student canvas into own', function(){
-                cy.log('need to write this test');
-            });
-            it('verify that student can copy graph field from another student canvas into own', function(){
-                cy.log('need to write this test');
-            });
-            it('verify that student cannot copy text field from own canvas into another student canvas', function(){
-                cy.log('need to write this test');
-            });
-            it('verify that student cannot copy graph field from own canvas into another student', function(){
-                cy.log('need to write this test');
-            });
-            it('verify student cannot copy text and graph field from another student to another student', function(){
-                cy.log('need to write this test');
-            });
-        });
+        // describe('test copy and paste from another canvas to another canvas', function(){
+        //     it('verify that student can copy text field from another student canvas into own', function(){
+        //         cy.log('need to write this test');
+        //     });
+        //     it('verify that student can copy graph field from another student canvas into own', function(){
+        //         cy.log('need to write this test');
+        //     });
+        //     it('verify that student cannot copy text field from own canvas into another student canvas', function(){
+        //         cy.log('need to write this test');
+        //     });
+        //     it('verify that student cannot copy graph field from own canvas into another student', function(){
+        //         cy.log('need to write this test');
+        //     });
+        //     it('verify student cannot copy text and graph field from another student to another student', function(){
+        //         cy.log('need to write this test');
+        //     });
+        // });
 
     });
 });
