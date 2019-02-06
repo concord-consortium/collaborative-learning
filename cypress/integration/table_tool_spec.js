cf6522365fee285275edd712d80b78ccc45a3079
eireland <eireland@concord.org>
2019-02-06 15:49:47 -0800

    Added test scripts for tables in Learning logs

--- cypress/integration/table_tool_spec.js
+++ cypress/integration/table_tool_spec.js
@@ -19,6 +19,10 @@ context('Table Tool Tile',function(){
           canvas.addTableTile();
           tableToolTile.getTableToolTile().should('be.visible');
       });
+      it('will verify remove row menu item is disabled when no new rows have been added', function(){
+        tableToolTile.openTableMenu();
+        tableToolTile.getRemoveRowMenuItem().should('be.disabled');
+      });
       it('will add a row to the table', function(){
           tableToolTile.addNewRow();
           tableToolTile.getTableRow().should('have.length',2);
@@ -47,7 +51,11 @@ context('Table Tool Tile',function(){
           tableToolTile.addNewRow();
           tableToolTile.removeRows("1");
           tableToolTile.getTableRow().should('have.length',2);
-      })
+      });
+      it('will verify remove row menu item is disabled when no rows can be removed', function(){
+          tableToolTile.openTableMenu();
+          tableToolTile.getRemoveRowMenuItem().should('be.disabled');
+      });
    });
 
     describe('table in different views', function(){
@@ -62,6 +70,7 @@ context('Table Tool Tile',function(){
             let nwCanvas= canvas.northWestCanvas();
             let table = tableToolTile.tableToolTile();
             cy.get(nwCanvas + ' ' + table).should('be.visible');
+            //TODO verify rightside workspace is open
         });
         it('will reset to original', function(){
             canvas.openOneUpViewFromTwoUp();
@@ -78,21 +87,43 @@ context('Table Tool Tile',function(){
         // })
    });
    describe('publish table', function(){
-        canvas.publishCanvas()
-        // need to verify that it is in the Class Work right nav
+       it('will publish canvas', function(){
+           canvas.publishCanvas();
+           // need to verify that it is in the Class Work right nav
+       });
    });
    describe('table in learning logs', function(){
-
+       it('will add a table in a learning log canvas', function(){
+            learningLog.createLearningLog('table LL');
+            learningLog.getLLTableTool().click();
+            learningLog.getLLTableTile().should('be.visible');
+       });
+       it('will publish learning log to Class Logs', function(){
+            learningLog.publishLearningLog();
+            //verify Class log has 'table LL' and thumbnail has table
+       });
    });
    describe('save and restores table from different areas', function(){
        it('will restore from My Work tab', function(){
-
+            leftNav.openToWorkspace('Now What');
+            rightNav.openMyWorkTab();
+        //    rightNav.getMyWorkTab().click();
+            // cy.wait(3000);
+            // rightNav.getRightNavExpandedSpace().should('be.visible');
+           // cy.wait(3000);
+        //    rightNav.getAllMyWorkAreaCanvasItems().then(($items)=>{
+        //        cy.log($items);
+        //        cy.wrap($items[0]).click({force:click});
+        //    })
+           rightNav.openMyWorkAreaCanvasItem('Introduction');
+            tableToolTile.getTableToolTile().should('be.visible');
        });
        it('will restore from Class Work tab', function(){
-
+           leftNav.openToWorkspace('Now What');
+            //verify can open published canvas to rightside workspace 2up view
        });
        it('will restore from Class Logs tab', function(){
-
+            //open class logs tab and open 'table LL' to rightside workspace 2up view
        });
    })
 
