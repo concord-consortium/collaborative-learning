d71b0ff51d93446c3500da9cf48deb9d7c85e3cf
eireland <eireland@concord.org>
2019-07-29 09:48:31 -0700

    moved all cypress related bash commands into the run_cypress_test script

---
+++ run_cypress_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+npm run start &
+wait-on http://localhost:8080
+
+if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[dev-build]"* ]]; then 
+    npm run test:cypress:smoke
+else 
+    npm run test:cypress
+fi   
--- run_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ "$TRAVIS_COMMIT_MESSAGE" == *"dev-build"* ]]; then 
-    cypress run --spec "cypress/integration/smoke/single_student_canvas_test.js" --config video=false,defaultCommandTimeout=8000 --env testEnv=local
-else 
-    npm run test:cypress
-fi   
