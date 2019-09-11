ec25503ff6dd0b4b0753123ef80f6585bf18d002
Doug Martin <dmartin@concord.org>
2019-09-11 15:12:08 -0400

    Added debug localstorage flag [#168439774]
    Extensible flag with first use being to show the document key over the canvas displaying the document.

---
+++ src/lib/debug.ts
@@ -0,0 +1,8 @@
+const debug = (window.localStorage ? window.localStorage.getItem("debug") : undefined) || "";
+if (debug.length > 0) {
+  console.info("DEBUG:", debug);
+}
+
+const debugContains = (key: string) => debug.indexOf(key) !== -1;
+
+export const DEBUG_CANVAS = debugContains("canvas");
