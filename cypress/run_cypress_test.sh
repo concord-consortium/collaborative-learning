56368cf1b85e03563dba6b50aacfb59e4c767d38
eireland <eireland@concord.org>
2019-10-12 05:45:53 +0800

    [clue] added travis build conditionals to run specific cypress tests

--- cypress/run_cypress_test.sh
+++ cypress/run_cypress_test.sh
@@ -5,6 +5,10 @@ wait-on http://localhost:8080
 
 if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[dev-build]"* ]]; then 
     npm run test:cypress:smoke
+elif [[ "$TRAVIS_COMMIT_MESSAGE" == *"[clue]"* ]]; then 
+    npm run test:cypress:clue
+elif [[ "$TRAVIS_COMMIT_MESSAGE" == *"[dataflow]"* ]]; then 
+    npm run test:cypress:clue    
 else 
     npm run test:cypress
 fi   
