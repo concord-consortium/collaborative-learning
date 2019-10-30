1b85ca12ceb146e9ed10f062669777e473509fb8
Kirk Swenson <kswenson@concord.org>
2019-10-30 00:18:52 -0700

    Shared selection implementation

---
+++ src/models/stores/selection.test.ts
@@ -0,0 +1,86 @@
+import { DataSetSelectionModel, SelectionStoreModel } from "./selection";
+
+describe("DataSetSelectionModel", () => {
+
+  const model = DataSetSelectionModel.create();
+
+  it("works as expected", () => {
+    expect(model.isSelected("foo")).toBe(false);
+    expect(model.getSelected()).toEqual([]);
+    model.select("foo", true);
+    expect(model.isSelected("foo")).toBe(true);
+    expect(model.getSelected()).toEqual(["foo"]);
+    model.select("bar", false);
+    expect(model.isSelected("foo")).toBe(true);
+    expect(model.isSelected("bar")).toBe(false);
+    expect(model.getSelected()).toEqual(["foo"]);
+    model.toggleSelected("bar");
+    expect(model.isSelected("foo")).toBe(true);
+    expect(model.isSelected("bar")).toBe(true);
+    expect(model.getSelected()).toEqual(["foo", "bar"]);
+    model.setSelected(["bar", "roo"]);
+    expect(model.isSelected("foo")).toBe(false);
+    expect(model.isSelected("bar")).toBe(true);
+    expect(model.isSelected("roo")).toBe(true);
+    expect(model.getSelected()).toEqual(["bar", "roo"]);
+  });
+
+  it("can be observed", () => {
+    const counts = {
+            add: 0,
+            update: 0,
+            delete: 0
+          } as { [name: string]: number };
+    model.observe(change => {
+      ++counts[change.type];
+    });
+    model.toggleSelected("bar");
+    expect(counts).toEqual({ add: 0, update: 1, delete: 0 });
+    model.clear();
+    expect(counts).toEqual({ add: 0, update: 1, delete: 2 });
+    model.select("baz", true);
+    expect(counts).toEqual({ add: 1, update: 1, delete: 2 });
+  });
+});
+
+describe("SelectionStoreModel", () => {
+
+  const model = SelectionStoreModel.create();
+
+  it("works as expected", () => {
+    expect(model.isSelected("foo", "bar")).toBe(false);
+    expect(model.getSelected("foo")).toEqual([]);
+    model.select("foo", "bar", true);
+    expect(model.isSelected("foo", "bar")).toBe(true);
+    expect(model.getSelected("foo")).toEqual(["bar"]);
+    model.toggleSelected("foo", "bar");
+    expect(model.isSelected("foo", "bar")).toBe(false);
+    expect(model.getSelected("foo")).toEqual([]);
+    model.setSelected("foo", ["bar", "baz"]);
+    expect(model.isSelected("foo", "bar")).toBe(true);
+    expect(model.isSelected("foo", "baz")).toBe(true);
+    expect(model.getSelected("foo")).toEqual(["bar", "baz"]);
+    model.clear("foo");
+    expect(model.isSelected("foo", "bar")).toBe(false);
+    expect(model.isSelected("foo", "baz")).toBe(false);
+    expect(model.getSelected("foo")).toEqual([]);
+  });
+
+  it("can be observed", () => {
+    const counts = {
+            add: 0,
+            update: 0,
+            delete: 0
+          } as { [name: string]: number };
+    model.observe("foo", change => {
+      ++counts[change.type];
+    });
+    model.select("foo", "baz", true);
+    expect(counts).toEqual({ add: 1, update: 0, delete: 0 });
+    model.observe("baz", change => {
+      ++counts[change.type];
+    });
+    model.select("baz", "roo", true);
+    expect(counts).toEqual({ add: 2, update: 0, delete: 0 });
+  });
+});
---
+++ src/models/stores/selection.ts
@@ -0,0 +1,80 @@
+import { types, Instance } from "mobx-state-tree";
+import { IMapDidChange, ObservableMap } from "mobx";
+
+export const DataSetSelectionModel = types
+  .model("DataSetSelection", {
+    selection: types.map(types.boolean)
+  })
+  .views(self => ({
+    isSelected(id: string) {
+      return !!self.selection.get(id);
+    },
+    getSelected(): string[] {
+      const selection = Array.from(self.selection.entries());
+      return selection
+              .map(entry => entry[1] ? entry[0] : undefined)
+              .filter(id => !!id) as string[];
+    }
+  }))
+  .actions(self => ({
+    clear() {
+      self.selection.clear();
+    },
+    select(id: string, isSelected: boolean) {
+      self.selection.set(id, isSelected);
+    },
+    toggleSelected(id: string) {
+      self.selection.set(id, !self.isSelected(id));
+    },
+    observe(listener: (change: IMapDidChange<string, boolean>) => void) {
+      return self.selection.observe(listener);
+    }
+  }))
+  .actions(self => ({
+    setSelected(ids: string[]) {
+      self.clear();
+      ids.forEach(id => self.select(id, true));
+    }
+  }));
+
+export const SelectionStoreModel = types
+  .model("SelectionStore", {
+    sets: types.map(DataSetSelectionModel)
+  })
+  .views(self => ({
+    isSelected(setId: string, id: string) {
+      const set = self.sets.get(setId);
+      return set ? set.isSelected(id) : false;
+    },
+    getSelected(setId: string) {
+      const set = self.sets.get(setId);
+      return set ? set.getSelected() : [];
+    }
+  }))
+  .actions(self => ({
+    require(setId: string) {
+      const set = self.sets.get(setId);
+      if (set) return set;
+      const newSet = DataSetSelectionModel.create();
+      self.sets.set(setId, newSet);
+      return newSet;
+    }
+  }))
+  .actions(self => ({
+    clear(setId: string) {
+      self.require(setId).clear();
+    },
+    select(setId: string, id: string, isSelected: boolean) {
+      self.require(setId).select(id, isSelected);
+    },
+    toggleSelected(setId: string, id: string) {
+      self.require(setId).toggleSelected(id);
+    },
+    setSelected(setId: string, ids: string[]) {
+      self.require(setId).setSelected(ids);
+    },
+    observe(setId: string, listener: (changes: IMapDidChange<string, boolean>) => void) {
+      return self.require(setId).observe(listener);
+    }
+  }));
+export type SelectionStoreModelType = Instance<typeof SelectionStoreModel>;
