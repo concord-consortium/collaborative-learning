1f75124fa782f31b69850f0b6df8df996f9be9ad
Doug Martin <dmartin@concord.org>
2019-11-08 14:52:54 -0500

    Added teacher sticky note authoring [#169291357]

--- src/clue/components/teacher/teacher-group-six-pack.tsx
+++ src/clue/components/teacher/teacher-group-six-pack.tsx
@@ -7,6 +7,9 @@ import { FourUpComponent } from "../../../components/four-up";
 import { IconButton } from "../../../components/utilities/icon-button";
 import { GroupVirtualDocument } from "../../../models/document/group-virtual-document";
 import { LogEventName, Logger } from "../../../lib/logger";
+import { createTextSupport } from "../../../models/curriculum/support";
+import { AudienceModel, AudienceEnum } from "../../../models/stores/supports";
+import { GroupUserModelType } from "../../../models/stores/groups";
 
 import "./teacher-group-six-pack.sass";
 
@@ -16,14 +19,24 @@ interface IProps extends IBaseProps {
   selectedSectionId: string | null;
 }
 
+interface IState {
+  focusedGroupUser: GroupUserModelType | undefined;
+}
+
 const ROWS = 2;
 const COLUMNS = 3;
 export const GROUPS_PER_PAGE = ROWS * COLUMNS;
 
 @inject("stores")
 @observer
-export class TeacherGroupSixPack extends BaseComponent<IProps, {}> {
+export class TeacherGroupSixPack extends BaseComponent<IProps, IState> {
 
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      focusedGroupUser: undefined
+    };
+  }
   public render() {
     return (
       <div className="teacher-group-six-pack">
@@ -49,6 +62,7 @@ export class TeacherGroupSixPack extends BaseComponent<IProps, {}> {
   }
 
   private renderFourUp(groupIndex: number, r: number, c: number) {
+    const { focusedGroupUser } = this.state;
     const { documentViewMode, selectedSectionId } = this.props;
     const { groups } = this.stores;
     const group = groups.allGroups[groupIndex];
@@ -58,9 +72,27 @@ export class TeacherGroupSixPack extends BaseComponent<IProps, {}> {
     }
     interface IGroupHeaderProps {
       group: IGroupRecord;
+      focusedGroupUser?: GroupUserModelType;
     }
     const TeacherGroupHeader = (props: IGroupHeaderProps) => {
-      const { ui }  = this.stores;
+      const { ui, db }  = this.stores;
+
+      const messageClickHandler = () => {
+        if (focusedGroupUser) {
+          ui.prompt(`Enter your message for ${focusedGroupUser.name}`, "", `Message ${focusedGroupUser.name}`, 5)
+          .then((message) => {
+            const audience = AudienceModel.create({type: AudienceEnum.user, identifier: focusedGroupUser.id});
+            db.createSupport(createTextSupport(message), "", audience);
+          });
+        }
+        else {
+          ui.prompt("Enter your message for the group", "", "Message Group", 5)
+          .then((message) => {
+            const audience = AudienceModel.create({type: AudienceEnum.group, identifier: props.group.id});
+            db.createSupport(createTextSupport(message), "", audience);
+          });
+        }
+      };
 
       const showGroupClickHandler = () => {
         Logger.log(LogEventName.VIEW_GROUP, {group: group.id, via: "dashboard-show-standalone-group"});
@@ -81,6 +113,12 @@ export class TeacherGroupSixPack extends BaseComponent<IProps, {}> {
           <div className="group-label">Group {String(group.id)}</div>
           <div className="actions">
             <IconButton
+              title={`Message ${focusedGroupUser ? focusedGroupUser.name : "Group"}`}
+              className="icon"
+              icon="sticky-note"
+              key={`sticky-note-${focusedGroupUser ? `user-${focusedGroupUser.id}` : "group"}`}
+              onClickButton={messageClickHandler} />
+            <IconButton
               title="Support"
               className="icon"
               icon="support"
@@ -99,7 +137,7 @@ export class TeacherGroupSixPack extends BaseComponent<IProps, {}> {
 
     return (
       <div className={`teacher-group group-${r}-${c}`} key={`group-${r}-${c}`}>
-        <TeacherGroupHeader group={ group } />
+        <TeacherGroupHeader group={ group } focusedGroupUser={focusedGroupUser} />
         <div className="teacher-group-canvas-container">
           <div className="teacher-group-canvas">
             <FourUpComponent
@@ -109,10 +147,15 @@ export class TeacherGroupSixPack extends BaseComponent<IProps, {}> {
               documentViewMode={documentViewMode}
               selectedSectionId={selectedSectionId}
               viaTeacherDashboard={true}
+              setFocusedGroupUser={this.handleSetFocusedGroupUser}
             />
           </div>
         </div>
       </div>
     );
+  }
+
+  private handleSetFocusedGroupUser = (focusedGroupUser?: GroupUserModelType) => {
+    this.setState({focusedGroupUser});
   }
 }
