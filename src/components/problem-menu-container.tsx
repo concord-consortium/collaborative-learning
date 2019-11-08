775c60de5ddccfb6e8364e2b72e792a71a9c3bab
Noah Paessel <knowuh@gmail.com>
2019-11-08 11:43:02 -0500

    Change `IMenuItemProps` to `IDropdownItem`
    [#168988636] Dashboard Header Styling
    
    https://www.pivotaltracker.com/story/show/168988636

--- src/components/problem-menu-container.tsx
+++ src/components/problem-menu-container.tsx
@@ -3,7 +3,7 @@ import { IBaseProps, BaseComponent } from "./base";
 import { inject, observer } from "mobx-react";
 import { LogEventName, LogEventMethod, Logger } from "../lib/logger";
 import { toJS } from "mobx";
-import { DropDown, IMenuItemProps } from "concord-react-components";
+import { DropDown, IDropdownItem } from "concord-react-components";
 
 interface IProps extends IBaseProps {}
 
@@ -16,7 +16,7 @@ export class ProblemMenuContainer extends BaseComponent <IProps, {}> {
     return <DropDown title="Problems" items={problemMenuItems} />;
   }
 
-  private handleMenuItemClick = (item: IMenuItemProps, problemName: string) => {
+  private handleMenuItemClick = (item: IDropdownItem, problemName: string) => {
     const {link, text} = item;
     if (link) {
       window.location.replace(link);
@@ -41,7 +41,7 @@ export class ProblemMenuContainer extends BaseComponent <IProps, {}> {
   private getProblemMenuItems() {
     const { user, unit, problem } = this.stores;
     const investigations = unit.investigations;
-    const links: IMenuItemProps[] = [];
+    const links: IDropdownItem[] = [];
 
     // For each problem in the curriculum assigned in this class...
     investigations.forEach ( (investigation) => {
@@ -56,7 +56,7 @@ export class ProblemMenuContainer extends BaseComponent <IProps, {}> {
             offering.unitCode === unitCode
           );
         });
-        const link: IMenuItemProps = {
+        const link: IDropdownItem = {
           selected: problem.title === invProb.title,
           text: portalOffering ? invProb.title : `${invProb.title} (N/A)`,
           link: portalOffering ? portalOffering.location : undefined,
