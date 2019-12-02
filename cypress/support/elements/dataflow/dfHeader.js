450db87a3de4feff7656adb9c02ff277368362e6
eireland <eireland@concord.org>
2019-12-02 12:44:36 -0800

    Fixed selector for view toggle. Fixed crete new program dialog title

--- cypress/support/elements/dataflow/dfHeader.js
+++ cypress/support/elements/dataflow/dfHeader.js
@@ -1,6 +1,6 @@
 class dfHeader{
     getDataflowWorkspaceSwitch(){
-        return cy.get('.dataflow-app-content .app-header .bp3-button')
+        return cy.get('.dataflow-app-content .app-header .toggle-button')
     }
     switchWorkspace(workspace){
         cy.wait(1000)
