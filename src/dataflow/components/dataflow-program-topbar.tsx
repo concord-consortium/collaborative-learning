2e0afb3511369b9953eeeb50808757c0040e7fd0
mklewandowski <matt.kain.lewandowski@gmail.com>
2019-12-09 16:41:18 -0800

    add program countdown timer

--- src/dataflow/components/dataflow-program-topbar.tsx
+++ src/dataflow/components/dataflow-program-topbar.tsx
@@ -12,6 +12,7 @@ interface TopbarProps {
   onProgramTimeSelectClick: (type: number) => void;
   isRunEnabled: boolean;
   runningProgram: boolean;
+  remainingTime: number;
   readOnly: boolean;
 }
 
@@ -19,36 +20,59 @@ export const DataflowProgramTopbar = (props: TopbarProps) => {
   const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     props.onProgramTimeSelectClick(Number(event.target.value));
   };
+  const runTime = props.programRunTimes.find( (rt: ProgramRunTime) => rt.val === props.programDefaultRunTime );
+  const remainingHours = Math.floor(props.remainingTime / 3600);
+  const remainingMinutes = Math.floor((props.remainingTime - remainingHours * 3600) / 60);
+  const remainingSeconds = (props.remainingTime - (remainingHours * 3600) - (remainingMinutes * 60) ) % 60;
+  const progressWidth = (76 * props.remainingTime / props.programDefaultRunTime).toString() + "px";
+  const hoursString = String(remainingHours).padStart(2, "0");
+  const minutesString = String(remainingMinutes).padStart(2, "0");
+  const secondsString = String(remainingSeconds).padStart(2, "0");
   return (
     <div className="program-editor-topbar">
-      <div className="duration" title="Duration">
-        <div className="label-back">
-          <div className="label">Duration</div>
-        </div>
-        <div className="duration-options-back">
-          <HTMLSelect className="duration-options"
-            onChange={handleSelectChange}
-            disabled={!props.isRunEnabled || props.readOnly}
-            value={props.programDefaultRunTime.toString()}
-          >
-            { props.programRunTimes.map((rt: ProgramRunTime) => (
-                <option key={rt.text} value={rt.val}>
-                  {rt.text}
-                </option>
-              ))
-            }
-          </HTMLSelect>
-        </div>
-      </div>
-      <button
-        className="program-state-button"
-        title="Run"
-        onClick={props.onRunProgramClick}
-        disabled={!props.isRunEnabled || props.readOnly}
-      >
-        <div className="icon run" />
-        <div className="text">Run</div>
-      </button>
+      { props.runningProgram
+        ? <div className="running-container countdown">
+            <div className="total">
+              {`Duration: ${runTime?.text.toString()}`}
+            </div>
+            <div className="remaining">
+              <div className="progress-bar" style={{width: progressWidth}}/>
+              <div className="progress-time">
+              {`${hoursString}:${minutesString}:${secondsString}`}
+              </div>
+            </div>
+          </div>
+        : <div className="running-container">
+            <div className="duration" title="Duration">
+              <div className="label-back">
+                <div className="label">Duration</div>
+              </div>
+              <div className="duration-options-back">
+                <HTMLSelect className="duration-options"
+                  onChange={handleSelectChange}
+                  disabled={!props.isRunEnabled || props.readOnly}
+                  value={props.programDefaultRunTime.toString()}
+                >
+                  { props.programRunTimes.map((rt: ProgramRunTime) => (
+                      <option key={rt.text} value={rt.val}>
+                        {rt.text}
+                      </option>
+                    ))
+                  }
+                </HTMLSelect>
+              </div>
+            </div>
+            <button
+              className="program-state-button"
+              title="Run"
+              onClick={props.onRunProgramClick}
+              disabled={!props.isRunEnabled || props.readOnly}
+            >
+              <div className="icon run" />
+              <div className="text">Run</div>
+            </button>
+          </div>
+      }
       <button
         className="program-state-button"
         title="Stop"
--- src/dataflow/components/dataflow-program.tsx
+++ src/dataflow/components/dataflow-program.tsx
@@ -97,6 +97,7 @@ interface IState {
   programDisplayState: ProgramDisplayStates;
   graphDataSet: DataSet;
   editorContainerWidth: number;
+  remainingTime: number;
 }
 
 const numSocket = new Rete.Socket("Number value");
@@ -127,7 +128,8 @@ export class DataflowProgram extends BaseComponent<IProps, IState> {
       programRunState: ProgramRunStates.Ready,
       graphDataSet: { sequences: [], startTime: 0, endTime: 0 },
       editorContainerWidth: 0,
-      programDisplayState: ProgramDisplayStates.Program
+      programDisplayState: ProgramDisplayStates.Program,
+      remainingTime: 0,
     };
   }
 
@@ -147,6 +149,7 @@ export class DataflowProgram extends BaseComponent<IProps, IState> {
           programDefaultRunTime={this.props.programRunTime || DEFAULT_PROGRAM_TIME}
           isRunEnabled={this.isReady()}
           runningProgram={this.isRunning() && !this.props.readOnly}
+          remainingTime={this.state.remainingTime}
           readOnly={this.props.readOnly || !this.isReady()}
         />}
         <div className={toolbarEditorContainerClass}>
@@ -904,6 +907,9 @@ export class DataflowProgram extends BaseComponent<IProps, IState> {
         this.props.onCheckProgramRunState(this.props.programEndTime);
         this.setState({ programRunState: ProgramRunStates.Complete, programDisplayState });
         clearInterval(this.intervalHandle);
+      } else if (this.props.programEndTime && (Date.now() < this.props.programEndTime)) {
+        const remainingTime = Math.ceil((this.props.programEndTime - Date.now()) / 1000);
+        this.setState({ remainingTime });
       }
     }
   }
