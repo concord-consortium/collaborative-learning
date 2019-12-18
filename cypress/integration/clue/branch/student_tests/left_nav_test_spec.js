a18f903dcca83af8a6aa95f7f6dad5a1d687b836
eireland <eireland@concord.org>
2019-12-18 13:11:11 -0800

    Initial idea of having a folder for branch builds

---
+++ cypress/integration/clue/branch/student_tests/left_nav_test_spec.js
@@ -0,0 +1,38 @@
+import LeftNav from '../../../../support/elements/clue/LeftNav'
+
+let leftNav = new LeftNav;
+
+before(function(){
+    const baseUrl = `${Cypress.config("baseUrl")}`;
+    const queryParams = `${Cypress.config("queryParams")}`;
+
+    cy.visit(baseUrl+queryParams);
+    cy.waitForSpinner()
+    // cy.wait(4000);
+});
+
+describe('Test Left tabs',function(){
+
+    let titles=['Introduction', 'Initial Challenge', 'What If...?', 'Now What Do You Know', 'Extra Workspace']
+
+    it('verify tab names are visible',()=>{
+        let titleArr = [], i=0;
+
+        leftNav.getLeftNavTabs().each(($tab, index, $tabList)=>{
+            expect($tab.text()).to.contain(titles[index])
+        })
+    })    
+    it('verify content when switching tabs', function(){
+        let titleArr = [], i=0;
+
+        leftNav.getLeftNavTabs().each(($tab,index, tabList)=>{
+            var title = $tab.text();
+            cy.wrap($tab).click({force:true});
+            cy.get('#leftNavContainer'+index).find('.left-nav-panel .section-header h1').should("contain",title)
+        })
+    });
+});
+
+after(function(){
+  cy.clearQAData('all');
+});
---
+++ cypress/integration/clue/branch/student_tests/text_tool_spec.js
@@ -0,0 +1,59 @@
+import Canvas from '../../../../support/elements/common/Canvas'
+import ClueCanvas from '../../../../support/elements/clue/cCanvas'
+import TextToolTile from '../../../../support/elements/clue/TextToolTile'
+import RightNav from '../../../../support/elements/common/RightNav';
+
+const canvas = new Canvas;
+const clueCanvas = new ClueCanvas;
+const textToolTile = new TextToolTile;
+const rightNav = new RightNav;
+
+
+before(function(){
+    const baseUrl = `${Cypress.config("baseUrl")}`;
+    const queryParams = `${Cypress.config("queryParams")}`;
+
+    // cy.clearQAData('all');
+    cy.visit(baseUrl+queryParams);
+    cy.waitForSpinner();
+    // cy.wait(4000);
+});
+
+context('Text tool tile functionalities', function(){
+    let title;
+    before(()=>{
+        clueCanvas.getInvestigationCanvasTitle()
+            .then(($canvasTitle)=>{
+                title = $canvasTitle.text().trim();
+                cy.log('title is: '+title)
+            })
+    })
+    it('adds text tool and types Hello World', function(){
+        clueCanvas.addTile('text');
+        textToolTile.enterText('Hello World');
+        textToolTile.getTextTile().last().should('contain', 'Hello World');
+    });
+    it('verifies restore of text field content',()=>{
+        canvas.createNewExtraDocument('text tool test');
+        textToolTile.getTextTile().should('not.exist');
+        //re-open investigation
+        rightNav.openRightNavTab('my-work');
+        rightNav.openSection('my-work','investigations')
+        rightNav.openCanvasItem('my-work','investigations',title)
+        textToolTile.getTextTile().last().should('exist').and('contain', 'Hello World');
+    })
+    it('clicks the same text field and allows user to edit text', function(){
+        textToolTile.getTextTile().last().focus().click();
+        textToolTile.addText('Adding more text to see if it gets added.');
+        textToolTile.addText('Adding more text to delete');
+        textToolTile.deleteText('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}');
+    });
+    it('delete text tile',()=>{
+        clueCanvas.deleteTile('text');
+        textToolTile.getTextTile().should('not.exist');
+    })
+})
+
+after(function(){
+  cy.clearQAData('all');
+});
