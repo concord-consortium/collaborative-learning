554357765b0c2b34e06b42c430b2e6fc02b9d7b5
eireland <eireland@concord.org>
2019-12-22 14:15:46 -0800

    -Added verification of can't run program with no data storage block and no relay -Added test for relay in use

--- cypress/integration/dataflow/full/running-program-spec.js
+++ cypress/integration/dataflow/full/running-program-spec.js
@@ -5,13 +5,15 @@ import dfBlock from "../../../support/elements/dataflow/dfBlock";
 import Canvas from "../../../support/elements/common/Canvas";
 import RightNav from "../../../support/elements/common/RightNav";
 import dfHeader from "../../../support/elements/dataflow/dfHeader";
+import Dialog from "../../../support/elements/common/Dialog";
 
 const dfheader = new dfHeader;
 const dfcanvas = new dfCanvas;
 const dfblock = new dfBlock;
 const canvas = new Canvas;
 const rightNav = new RightNav;
-const dfrightnav = new dfRightNav
+const dfrightnav = new dfRightNav;
+const dialog = new Dialog;
 
 const program1 = 'Program-1'
 const program2 = 'Program-2'
@@ -19,7 +21,6 @@ const dataset1 = 'stopped dataset'
 const dataset2 = '1 minute dataset'
 const dataset3 = '10 minute dataset'
 
-//Add countdown timer tests
 //Add test for when a program is run from a copy of a dataset or a shared dataset/programs
 //Add test for going from dataset to program and rerun the program - verify that a new dataset is created and not added on to a previous test
 //verify that if data storage or relay are not connected or not present, then run button should be disabled
@@ -41,53 +42,100 @@ before(()=>{
 })
 
 context('Program Canvas tests',function(){
-    it('verify data collection stops when Stop button is clicked',()=>{
-        dfcanvas.selectDuration('300')
-        dfcanvas.runProgram(dataset1);
-        cy.wait(15000); //on first run sometimes it takes 10 seconds for graph to start
-        dfcanvas.getProgramGraph().should('be.visible');
-        dfrightnav.getRunningBadge().should('exist');
-        dfcanvas.stopProgram();
-        dfcanvas.getFullGraph().should('be.visible');
-        dfcanvas.getDataFlowProgramEditorTopControl().should('not.exist')
-        canvas.getPersonalDocTitle().should('contain',dataset1)
-        dfrightnav.getRunningBadge().should('not.exist');
-    })
+    describe('Data Storage tests',()=>{
+        it('verify data collection stops when Stop button is clicked',()=>{
+            dfcanvas.selectDuration('300')
+            dfcanvas.runProgram(dataset1);
+            cy.wait(15000); //on first run sometimes it takes 10 seconds for graph to start
+            dfcanvas.getProgramGraph().should('be.visible');
+            dfrightnav.getRunningBadge().should('exist');
+            dfcanvas.stopProgram();
+            dfcanvas.getFullGraph().should('be.visible');
+            dfcanvas.getDataFlowProgramEditorTopControl().should('not.exist')
+            canvas.getPersonalDocTitle().should('contain',dataset1)
+            dfrightnav.getRunningBadge().should('not.exist');
+        })
 
-    it('restore program',()=>{
-        rightNav.openRightNavTab('my-work');
-        rightNav.openSection('my-work','','Programs')
-        rightNav.openCanvasItem('my-work','',program1)
-        dfcanvas.getDurationDropdown().should('contain', '5 mins')
-    })
-    it('verify data collection runs to the end of the duration',()=>{
-        dfcanvas.createNewProgram(program2);
-        dfcanvas.openBlock('Number')
-        dfcanvas.openBlock('Data Storage')
-        dfblock.getNumberInput().type('5');
-        dfblock.connectBlocks('number',0,'data-storage',0)
-        dfcanvas.selectDuration('60')
-        dfcanvas.runProgram(dataset2);
-        dfcanvas.getDurationContainer().find('.total').should('contain', '1 min')
-        cy.wait(10000);
-        rightNav.openSection('my-work','','Data')
-        dfrightnav.getRunningBadge().should('exist');
-        cy.wait(50000);
-        dfcanvas.getFullGraph().should('be.visible');
-        dfcanvas.getDataFlowProgramEditorTopControl().should('not.exist')
-        canvas.getPersonalDocTitle().should('contain',dataset2)
-        dfrightnav.getRunningBadge().should('not.exist');
-    })
-    it('verify data collected from a run program appears in My Work>Data section',()=>{
-        rightNav.getCanvasItemTitle('my-work','','Data').contains(program1).should('not.be.visible')
-    })
-    it('verify data collected from a run program does not appear in My Work>Programs section',()=>{
-        rightNav.openSection('my-work','','Programs')
-        rightNav.getCanvasItemTitle('my-work','','Programs').contains(dataset1).should('not.be.visible')
-    })
-    it('verify re-running a previously ran program creates a new dataset',()=>{
+        it('restore program',()=>{
+            rightNav.openRightNavTab('my-work');
+            rightNav.openSection('my-work','','Programs')
+            rightNav.openCanvasItem('my-work','',program1)
+            dfcanvas.getDurationDropdown().should('contain', '5 mins')
+        })
+        it('verify data collection runs to the end of the duration',()=>{
+            dfcanvas.createNewProgram(program2);
+            dfcanvas.openBlock('Number')
+            dfcanvas.openBlock('Data Storage')
+            dfblock.getNumberInput().type('5');
+            dfblock.connectBlocks('number',0,'data-storage',0)
+            dfcanvas.selectDuration('60')
+            dfcanvas.runProgram(dataset2);
+            dfcanvas.getDurationContainer().find('.total').should('contain', '1 min')
+            cy.wait(10000);
+            rightNav.openSection('my-work','','Data')
+            dfrightnav.getRunningBadge().should('exist');
+            cy.wait(50000);
+            dfcanvas.getFullGraph().should('be.visible');
+            dfcanvas.getDataFlowProgramEditorTopControl().should('not.exist')
+            canvas.getPersonalDocTitle().should('contain',dataset2)
+            dfrightnav.getRunningBadge().should('not.exist');
+        })
+        it('verify data collected from a run program appears in My Work>Data section',()=>{
+            rightNav.getCanvasItemTitle('my-work','','Data').contains(program1).should('not.be.visible')
+        })
+        it('verify data collected from a run program does not appear in My Work>Programs section',()=>{
+            rightNav.openSection('my-work','','Programs')
+            rightNav.getCanvasItemTitle('my-work','','Programs').contains(dataset1).should('not.be.visible')
+        })
+        it('verify re-running a previously ran program creates a new dataset',()=>{
 
-    })
+        })
+        it('verify program run from copy creates a new dataset',()=>{
+
+        })
+        it('verify new data set is created from a shared program',()=>{
+
+        })
+        it('verify program cannot be run if data storage or relay block is not in the canvas',()=>{
+            dfcanvas.createNewProgram("Can't run program")
+            dfcanvas.runProgram()
+            dialog.getDialogTitle().should('contain','No Program Output')
+            dialog.getDialogOKButton.click();
+        })
+        it('verify program cannot be run when data storage has no connections',()=>{
+            dfcanvas.openBlock('Data Storage');
+            dfcanvas.runProgram();
+            dialog.getDialogTitle().should('contain', "Missing Data Storage Inputs");
+            dialog.getDialogOKButton().click();
+        })
+        it('verify program runs after error',()=>{
+            dfcanvas.openBlock('Number');
+            dfcanvas.connectBlocks('number',0,'data-storage',0)
+            dfcanvas.runProgram();
+            dialog.getDialogTitle().should('not.exist')
+            dfcanvas.getDurationContainer().should('be.visible');
+            dfcanvas.stopProgram();
+        })
+    }) 
+    describe('Relay tests',()=>{
+        const relayTestProgram = "Relay test program"
+        it('verify relay that is already in us is not available to another user',()=>{
+            dfcanvas.createNewProgram(relayTestProgram)
+            dfcanvas.openBlock('Relay');
+            dfblock.selectRelayOperator('codap-server-hub-sim');
+            dfcanvas.runProgram();
+            canvas.copyDocument('Second Relay Program');
+            dfcanvas.openBlock('Relay');
+            dfblock.selectRelayOperator('codap-server-hub-sim');
+            dfcanvas.runProgram();
+            dialog.getDialogTitle().should('contain','Relay In Use')
+            dialog.getDialogOKButton().click();
+        })
+        it('verify running relay program is in My Work>Relay Program',()=>{
+            rightNav.openSection('my-work','','Relay Programs');
+            rightNav.getCanvasItemTitle('my-work','','',relayTestProgram).should(exist);
+        })
+    }) 
 })
 context('Data Canvas tests',()=>{   
     it('verify restore of data canvas',()=>{
@@ -100,7 +148,6 @@ context('Data Canvas tests',()=>{
         dfcanvas.getGraphButton('data').should('be.visible')
         dfcanvas.getGraphButton('layout').should('be.visible')
         dfcanvas.getGraphButton('type').should('be.visible')
-
     })
     it('verify program toolbar is not present',()=>{
         dfcanvas.getProgramToolbar().should('not.be.visible')
