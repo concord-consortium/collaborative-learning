6adf53d866944596a7983e5693253c7b29c2e1e7
eireland <eireland@concord.org>
2020-01-03 16:15:47 -0800

    fixed logic for which test suite to run

--- cypress/run_cypress_test.sh
+++ cypress/run_cypress_test.sh
@@ -5,12 +5,9 @@ wait-on http://localhost:8080
 
 if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[dev-build]"* ]]; then 
     npm run test:cypress:smoke
+elif [["$TRAVIS_BRANCH" != "master"]]; then
+    npm run test:cypress:branch   
 else 
     npm run test:cypress
 fi  
-
-if [["$TRAVIS_BRANCH" != "master"]]; then
-    npm run test:cypress:branch
-else 
-    npm run test:cypress
-fi     
+   
