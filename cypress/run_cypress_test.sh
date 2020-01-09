97973a4cd77cca9864c13f3130eed5a5efbe4e96
eireland <eireland@concord.org>
2020-01-08 18:29:18 -0800

    added back running branch folder test

--- cypress/run_cypress_test.sh
+++ cypress/run_cypress_test.sh
@@ -6,8 +6,8 @@ wait-on http://localhost:8080
 if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[dev-build]"* ]]; then 
     npm run test:cypress:smoke
 elif [[ "$TRAVIS_BRANCH" != "master" ]]; then
-    echo "elif TRAVIS_BRANCH=$TRAVIS_BRANCH"
-    # npm run test:cypress:branch   
+    # echo "elif TRAVIS_BRANCH=$TRAVIS_BRANCH"
+    npm run test:cypress:branch
 else 
     echo "else TRAVIS_BRANCH=$TRAVIS_BRANCH"
     # npm run test:cypress
