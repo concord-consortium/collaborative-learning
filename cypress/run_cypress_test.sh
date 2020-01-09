7c2cb971bba845766061448843d3f25ae175f398
eireland <eireland@concord.org>
2020-01-08 20:16:54 -0800

    Added additional conditionals for not master, production or dataflow

--- cypress/run_cypress_test.sh
+++ cypress/run_cypress_test.sh
@@ -5,11 +5,11 @@ wait-on http://localhost:8080
 
 if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[dev-build]"* ]]; then 
     npm run test:cypress:smoke
-elif [[ "$TRAVIS_BRANCH" != "master" ]]; then
+elif [[ "$TRAVIS_BRANCH" != "master" ]] || [[ "$TRAVIS_BRANCH" != "production" ]] || [[ "$TRAVIS_BRANCH" != "dataflow" ]]; then
     # echo "elif TRAVIS_BRANCH=$TRAVIS_BRANCH"
     npm run test:cypress:branch
 else 
-    echo "else TRAVIS_BRANCH=$TRAVIS_BRANCH"
+    # echo "else TRAVIS_BRANCH=$TRAVIS_BRANCH"
     npm run test:cypress
 fi  
    
