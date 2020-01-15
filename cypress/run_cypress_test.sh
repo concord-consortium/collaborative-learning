bdb3d4c6f6d31afc1b97e9a754ac1a71ff1f4aea
eireland <eireland@concord.org>
2020-01-15 10:52:42 -0800

    Changed conditional to test against an array of branch names

--- cypress/run_cypress_test.sh
+++ cypress/run_cypress_test.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+declare -a BRANCH_ARRAY=("master" "production" "dataflow" "dataflow_production")
+
 npm run start &
 wait-on http://localhost:8080
 echo "start TRAVIS_BRANCH=$TRAVIS_BRANCH"
 if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[dev-build]"* ]]; then 
+    echo "dev-build"
     npm run test:cypress:smoke
-elif [[ "$TRAVIS_BRANCH" != "master" ]] || [[ "$TRAVIS_BRANCH" != "production" ]] || [[ "$TRAVIS_BRANCH" != "dataflow" ]] || [[ "$TRAVIS_BRANCH" != "dataflow-production" ]]; then
+elif !(echo $BRANCH_ARRAY | grep -q $TRAVIS_BRANCH); then
     echo "elif TRAVIS_BRANCH=$TRAVIS_BRANCH"
     npm run test:cypress:branch
 else 
     echo "else TRAVIS_BRANCH=$TRAVIS_BRANCH"
     npm run test:cypress
 fi  
+
    
