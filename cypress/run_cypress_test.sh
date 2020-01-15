4f79abaa62e0c05d546a6f8147958c2652a0f438
eireland <eireland@concord.org>
2020-01-15 08:28:12 -0800

    checking TRAVIS_BRANCH var on master

--- cypress/run_cypress_test.sh
+++ cypress/run_cypress_test.sh
@@ -2,14 +2,14 @@
 
 npm run start &
 wait-on http://localhost:8080
-
+echo "start TRAVIS_BRANCH=$TRAVIS_BRANCH"
 if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[dev-build]"* ]]; then 
     npm run test:cypress:smoke
 elif [[ "$TRAVIS_BRANCH" != "master" ]] || [[ "$TRAVIS_BRANCH" != "production" ]] || [[ "$TRAVIS_BRANCH" != "dataflow" ]] || [[ "$TRAVIS_BRANCH" != "dataflow-production" ]]; then
-    # echo "elif TRAVIS_BRANCH=$TRAVIS_BRANCH"
+    echo "elif TRAVIS_BRANCH=$TRAVIS_BRANCH"
     npm run test:cypress:branch
 else 
-    # echo "else TRAVIS_BRANCH=$TRAVIS_BRANCH"
+    echo "else TRAVIS_BRANCH=$TRAVIS_BRANCH"
     npm run test:cypress
 fi  
    
