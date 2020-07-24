80d06552c5abe496ffdf5db249b4b0b82c2aaa1b
Kirk Swenson <kswenson@concord.org>
2020-07-24 14:33:41 -0700

    Update firebase credential: - use new credential with `appId` - add referrer restrictions as well as API restrictions - use `atob()` to lightly obfuscate the key

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -106,13 +106,15 @@ export class DB {
 
       // check for already being initialized for tests
       if (firebase.apps.length === 0) {
+        const key = atob("QUl6YVN5QVV6T2JMblZESURYYTB4ZUxmSVpLV3BiLTJZSWpYSXBJ");
         firebase.initializeApp({
-          apiKey: "AIzaSyBKwTfDSxKRSTnOaAzI-mUBN78LiI2gM78",
+          apiKey: key,
           authDomain: "collaborative-learning-ec215.firebaseapp.com",
           databaseURL: "https://collaborative-learning-ec215.firebaseio.com",
           projectId: "collaborative-learning-ec215",
           storageBucket: "collaborative-learning-ec215.appspot.com",
-          messagingSenderId: "112537088884"
+          messagingSenderId: "112537088884",
+          appId: "1:112537088884:web:c51b1b8432fff36faff221"
         });
       }
 
