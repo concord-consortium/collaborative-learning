3c33cf9486770e7d7a079cd2a30c6883bb6eb23b
Kirk Swenson <kswenson@concord.org>
2020-10-23 11:14:31 -0700

    Dispose MobX reaction on unmount

--- src/components/tools/hooks/use-mobx-on-change.ts
+++ src/components/tools/hooks/use-mobx-on-change.ts
@@ -4,13 +4,14 @@ import { useEffect, useRef } from "react";
 export function useMobXOnChange<T>(getValue: () => T, onChange: (value: T) => void) {
   const valueRef = useRef(getValue());
   useEffect(() => {
-    reaction(
-      getValue,
-      value => {
-        if (value !== valueRef.current) {
-          valueRef.current = value;
-          onChange(value);
-        }
-      });
+    const dispose = reaction(
+                      getValue,
+                      value => {
+                        if (value !== valueRef.current) {
+                          valueRef.current = value;
+                          onChange(value);
+                        }
+                      });
+    return () => dispose();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 }
