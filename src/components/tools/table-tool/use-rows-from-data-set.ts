20769f59e068fc0fbf14e0776f38b0f082f62669
Kirk Swenson <kswenson@concord.org>
2020-11-21 14:56:33 -0800

    Write changes back to content

--- src/components/tools/table-tool/use-rows-from-data-set.ts
+++ src/components/tools/table-tool/use-rows-from-data-set.ts
@@ -9,16 +9,30 @@ interface IUseRowsFromDataSet {
   rowChanges: number;
   context: IGridContext;
 }
+
+const canonicalize = (dataSet: IDataSet, row: TRow) => {
+  // ReactDataGrid currently doesn't like editing undefined values,
+  // so we convert them to empty strings when building rows.
+  if (row) {
+    dataSet.attributes.forEach(attr => {
+      if (row[attr.id] == null) {
+        row[attr.id] = "";
+      }
+    });
+  }
+  return row;
+};
+
 export const useRowsFromDataSet = ({ dataSet, readOnly, inputRowId, rowChanges, context }: IUseRowsFromDataSet) => {
   return useMemo(() => {
     const rowKeyGetter = (row: TRow) => row.__id__;
     const rowClass = (row: TRow) => row.__id__ === inputRowId ? "input-row" : undefined;
     const rows = dataSet.getCanonicalCasesAtIndices()
-                        .map((row, i) => ({
+                        .map((_case, i) => canonicalize(dataSet, {
                           __index__: i + 1,
                           __context__: context,
-                          ...row })) as TRow[];
-    !readOnly && rows.push({ __id__: inputRowId, __context__: context });
+                          ..._case } as TRow));
+    !readOnly && rows.push(canonicalize(dataSet, { __id__: inputRowId, __context__: context }));
     rowChanges; // eslint-disable-line no-unused-expressions
     return { rows, rowKeyGetter, rowClass };
   }, [context, dataSet, inputRowId, readOnly, rowChanges]);
