a799579662fe386585a667966b006c7e53f72937
eireland <eireland@concord.org>
2021-02-18 17:45:50 -0800

    Adds Student Work tab content and student work tab tests

--- cypress/integration/clue/branch/teacher_tests/teacher_workspace_spec.js
+++ cypress/integration/clue/branch/teacher_tests/teacher_workspace_spec.js
@@ -11,20 +11,23 @@ import DrawToolTile from "../../../../support/elements/clue/DrawToolTile";
     let drawToolTile = new DrawToolTile;
 
     let teacherDoc = "Teacher Investigation Copy";
-    const queryParams = `${Cypress.config("teacherQueryParams")}`;
+    const queryParams = "/?appMode=demo&demoName=CLUE-Test&fakeClass=5&fakeOffering=5&problem=2.1&fakeUser=teacher:6&unit=msa";
 
-before(function() {
-  cy.fixture("teacher-dash-data-CLUE-test.json").as("clueData");
-
-  cy.visit(queryParams+`&unit=msa`);
+before(() => {
+  cy.visit(queryParams);
   cy.waitForSpinner();
   dashboard.switchView("Workspace");
   cy.wait(2000);
   clueCanvas.getInvestigationCanvasTitle().text().as('investigationTitle');
 });
 
-describe('teacher specific navigation tabs', function() {
-    it('verify problem tab solution switch', function() {
+beforeEach(() => {
+  cy.fixture("teacher-dash-data-msa-test.json").as("clueData");
+});
+
+describe('teacher specific navigation tabs', () =>  {
+
+    it('verify problem tab solution switch', () => {
       cy.get('.nav-tab.tab-problems').should('exist').click();
       cy.get('.prob-tab').contains('What If...?').click();
       cy.get('[data-test=solutions-button]').should('have.class',"toggled");
@@ -35,23 +38,38 @@ describe('teacher specific navigation tabs', function() {
 
       cy.get('.close-button').click();
     });
-    it('verify teacher guide', function() {
+    it('verify teacher guide', () => {
       cy.get('.nav-tab.tab-teacher-guide').should('exist').click();
       cy.get('.prob-tab.teacher-guide').should('exist').and('have.length', 4).each(function(subTab, index, subTabList) {
-        const teacherGuideSubTabs = ["Overview", "Launch", "Explore", "Summerize"];
+        const teacherGuideSubTabs = ["Overview", "Launch", "Explore", "Summarize"];
         cy.wrap(subTab).text().should('contain',teacherGuideSubTabs[index]);
       });
 
       cy.get('.close-button').click();
     });
-    it('verify student workspace tab', function() {
-      cy.get('.nav-tab.tab-student-work').should('exist').click();
+    it('verify student workspace tab', () => {
+      cy.get('@clueData').then((clueData)=>{
+        const groups = clueData.classes[0].problems[0].groups;
 
+        cy.get('.nav-tab.tab-student-work').should('exist').click();
+        cy.get('.nav-tab-buttons').should('have.class', 'hidden');
+        cy.get('.student-group-view').should('be.visible');
+        cy.get('.student-group .group-number').should('be.visible').and('have.length', groups.length);
+        cy.get('.student-group .group-number').eq(0).should('have.class', 'active');
+        cy.get('.group-title').should('contain', 'Group 1');
+        cy.get('.canvas-area .four-up .member').should('have.length', 4);
+        cy.get('.canvas-area .four-up .member').eq(0).should('contain', 'S1');
+        cy.get('.student-group .group-number').contains('G3').click();
+        cy.get('.student-group .group-number').eq(2).should('have.class', 'active');
+        cy.get('.group-title').should('contain', 'Group 3');
+        cy.get('.canvas-area .four-up .member').eq(0).should('contain', 'S9');
+      });
+      cy.get('.close-button').click();
     });
 });
 
 
-describe.skip('teacher document functionality',function(){
+describe('teacher document functionality',function(){
      before(function(){
         clueCanvas.addTile('table');
         clueCanvas.addTile('drawing');
