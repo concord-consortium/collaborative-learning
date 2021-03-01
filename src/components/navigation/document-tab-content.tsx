b4a551e6a9fdfa339b27f6b707c3f2f4f78558dc
eireland <eireland@concord.org>
2021-03-01 10:37:38 -0800

    PR fixes

--- src/components/navigation/document-tab-content.tsx
+++ src/components/navigation/document-tab-content.tsx
@@ -47,18 +47,19 @@ export const DocumentTabContent: React.FC<IProps> = ({ tabSpec }) => {
 
   const editButton = (type: string, sClass: string, document: DocumentModelType) => {
     return (
-      (type === "my-work") || (type === "learningLog") ?
-        <div className={`edit-button ${sClass}`} onClick={() => handleEditClick(document)}>
-          <EditIcon className={`edit-icon ${sClass}`} />
-          <div>Edit</div>
-        </div>
+      (type === "my-work") || (type === "learningLog")
+        ?
+          <div className={`edit-button ${sClass}`} onClick={() => handleEditClick(document)}>
+            <EditIcon className={`edit-icon ${sClass}`} />
+            <div>Edit</div>
+          </div>
         : null
     );
   };
 
   const sectionClass = referenceDocument?.type === "learningLog" ? "learning-log" : "";
   const selectedSection = tabSpec.tab === "supports" ? ENavTabSectionType.kTeacherSupports : undefined;
-  const documentView = (referenceDocument && !referenceDocument?.getProperty("isDeleted")) &&
+  const documentView = referenceDocument && !referenceDocument?.getProperty("isDeleted") &&
     <div>
       <div className={`document-header ${tabSpec.tab} ${sectionClass}`}>
         <div className={`document-title`}>
