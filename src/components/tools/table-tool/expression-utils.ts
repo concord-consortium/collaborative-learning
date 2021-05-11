ee9fa49f1b0ffcb6128737c6c1d80060327b25e8
Kirk Swenson <kswenson@users.noreply.github.com>
2021-05-10 19:08:22 -0700

    Fix parse error in `prettifyExpression()` with column names with spaces (#959)
    The `prettifyExpression()` function formats a table column expression for display to the user. Under the hood it was using the `substitute()` function of the `expr-eval` library we're using for expression evaluation to insert the name of any columns referenced. The `substitute()` function expects the result of the expression to be parseable/evaluable, which is not the case when the referenced column names contain spaces. But since `prettifyExpression()` is only intended to format expressions for display to the user, a simple string replacement is sufficient for our needs.

--- src/components/tools/table-tool/expression-utils.ts
+++ src/components/tools/table-tool/expression-utils.ts
@@ -1,5 +1,6 @@
+import escapeStringRegexp from 'escape-string-regexp';
 import { Parser } from "expr-eval";
-import { kSerializedXKey } from "../../../models/tools/table/table-model-types";
+import { kSerializedXKey, kSerializedXKeyRegEx } from "../../../models/tools/table/table-model-types";
 
 export const getEditableExpression = (
   rawExpression: string | undefined, canonicalExpression: string, xName: string
@@ -10,41 +11,36 @@ export const getEditableExpression = (
 };
 
 export const canonicalizeExpression = (displayExpression: string, xName: string) => {
-  if (xName && displayExpression) {
+  if (!displayExpression || !xName) return displayExpression;
+  let canonicalExpression = displayExpression.replace(new RegExp(escapeStringRegexp(xName), "g"), kSerializedXKey);
+  try {
     const parser = new Parser();
-    const canonicalExpression = parser.parse(displayExpression).substitute(xName, kSerializedXKey);
-    return canonicalExpression.toString();
-  } else {
-    return displayExpression;
+    canonicalExpression = parser.parse(canonicalExpression).toString();
   }
+  catch(e) {
+    // nop
+  }
+  return canonicalExpression;
 };
 
 export const prettifyExpression = (canonicalExpression: string | undefined, xName: string) => {
-  if (xName && canonicalExpression) {
-    const parser = new Parser();
-    let expression = parser.parse(canonicalExpression).substitute(kSerializedXKey, xName).toString();
-    if (expression.charAt(0) === "(" && expression.charAt(expression.length - 1) === ")") {
-      expression = expression.substring(1, expression.length - 1);
-    }
-    return expression;
-  } else {
-    return canonicalExpression;
-  }
+  return canonicalExpression && xName
+          ? canonicalExpression.replace(kSerializedXKeyRegEx, xName)
+          : canonicalExpression;
 };
 
-export const validateExpression = (expressionStr: string, xName: string) => {
-  if (!expressionStr || !xName) return;
+export const validateDisplayExpression = (displayExpression: string, xName: string) => {
+  if (!displayExpression || !xName) return;
+  const canonicalExpression = canonicalizeExpression(displayExpression, xName);
   const parser = new Parser();
   try {
-    const expression = parser.parse(expressionStr);
-    const unknownVar = expression.variables().find(variable => variable !== xName);
+    const parsed = parser.parse(canonicalExpression);
+    const unknownVar = parsed.variables().find(variable => variable !== kSerializedXKey);
     if (unknownVar) {
       return `Unrecognized variable "${unknownVar}" in expression.`;
     }
-    if (xName) {
-      // Attempt an evaluation to check for errors e.g. invalid function names
-      expression.evaluate({[xName]: 1});
-    }
+    // Attempt an evaluation to check for errors e.g. invalid function names
+    parsed.evaluate({[kSerializedXKey]: 1});
   } catch {
     return "Could not understand expression. Make sure you supply all operands " +
     "and use a multiplication sign where necessary, e.g. 3 * x + 4 instead of 3x + 4.";
