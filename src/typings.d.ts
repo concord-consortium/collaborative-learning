dd0560a2c2242f305f845ca7792e65b5f40ff8c9
Sam Fentress <sfentress@concord.org>
2021-09-07 11:41:07 -0400

    Add rete and chart.js packages and typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,4 +1,7 @@
 // declare modules that don't have TypeScript bindings here
+declare module "rete-connection-plugin";
+declare module "rete-area-plugin";
+declare module "rete-react-render-plugin";
 
 declare module "slate-md-serializer";
 
