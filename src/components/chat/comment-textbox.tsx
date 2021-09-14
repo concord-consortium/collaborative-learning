ac9fc0f4a973c70c72a45cdd760a2d222188981e
eireland <eireland@concord.org>
2021-09-13 20:04:41 -0700

    Text area resizes if user partially deletes text

--- src/components/chat/comment-textbox.tsx
+++ src/components/chat/comment-textbox.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import SendIcon from "../../assets/send-icon.svg";
 import "../themes.scss";
 
@@ -12,12 +12,21 @@ interface IProps {
 const minTextAreaHeight = 35;
 
 export const CommentTextBox: React.FC<IProps> = ({ activeNavTab, numPostedComments, onPostComment }) => {
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const [commentTextAreaHeight, setCommentTextAreaHeight] = useState(minTextAreaHeight);
   const [commentAdded, setCommentAdded] = useState(false);
   const [commentText, setCommentText] = useState("");
   const textareaStyle = {height: commentTextAreaHeight};
   const postButtonClass = classNames("comment-footer-button", "themed-negative", activeNavTab,
                                      { disabled: !commentAdded, "no-action": !commentAdded });
+  // resize textarea when user deletes some text
+  useEffect(() => {
+    if (textareaRef && textareaRef.current) {
+      textareaRef.current.style.height = minTextAreaHeight + "px";
+      const scrollHeight = textareaRef.current.scrollHeight;
+      textareaRef.current.style.height = scrollHeight + "px";
+    }
+  });
 
   const handleCommentTextAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const target = event.target;
@@ -40,13 +49,18 @@ export const CommentTextBox: React.FC<IProps> = ({ activeNavTab, numPostedCommen
   };
 
   const handlePostComment = () => {
-    onPostComment?.(commentText);
-    setCommentTextAreaHeight(minTextAreaHeight);
-    setCommentAdded(false);
-    setCommentText("");
+    // do not send post if text area is empty, only has spaces or new lines
+    const content = commentText.trim().replace(/\s\s+/g, " ");
+    if (!(content === "" || content === "\n" || content === " ")) {
+      onPostComment?.(commentText);
+      setCommentTextAreaHeight(minTextAreaHeight);
+      setCommentAdded(false);
+      setCommentText("");
+    }
   };
 
   const handleCommentTextboxKeyDown = (event: React.KeyboardEvent) => {
+    const content = commentText.trim().replace(/\s\s+/g, " ");
     switch(event.key) {
       case "Escape":
         setCommentTextAreaHeight(minTextAreaHeight);
@@ -55,9 +69,12 @@ export const CommentTextBox: React.FC<IProps> = ({ activeNavTab, numPostedCommen
         break;
       case "Enter":
         if(event.shiftKey) {
-          const content = commentText;
           event.preventDefault();
           setCommentText(content+"\n");
+        } else if (content === "" || content === "\n" || content === " ") {
+          // do not send post if text area is empty, only has spaces or new lines
+          event.preventDefault();
+          break;
         } else {
           event.preventDefault();
           handlePostComment();
@@ -69,6 +86,7 @@ export const CommentTextBox: React.FC<IProps> = ({ activeNavTab, numPostedCommen
   return (
     <div className="comment-textbox">
       <textarea
+        ref={textareaRef}
         style={textareaStyle}
         placeholder={numPostedComments < 1 ? "Comment on this document...": "Reply..."}
         value={commentText}
