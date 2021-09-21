90074731dbf2b75ad9cff36c2187edf3ba051688
Kirk Swenson <kswenson@concord.org>
2021-09-20 20:26:38 -0700

    Networks for authenticated teachers pulled from firestore

--- src/hooks/use-user-context.ts
+++ src/hooks/use-user-context.ts
@@ -6,7 +6,7 @@ export const useUserContext = (): IUserContext => {
   const appMode = useAppMode();
   const { name: demoName } = useDemoStore();
   const classInfo = useClassStore();
-  const { id: uid, portal, type, name, teacherNetwork: network, classHash } = useUserStore();
+  const { id: uid, portal, type, name, network, classHash } = useUserStore();
   return useMemo(() => {
     const teachers: string[] = [];
     classInfo.users.forEach(user => (user.type === "teacher") && teachers.push(user.id));
