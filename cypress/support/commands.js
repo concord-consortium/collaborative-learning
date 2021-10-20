8a9f4891938c31321d8c150baccc5647a930ebbc
Scott Cytacki <scytacki@concord.org>
2021-10-20 10:18:07 -0400

    Try adding a wait after qa cleared

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -76,6 +76,13 @@ Cypress.Commands.add("clearQAData", (data)=>{ //clears data from Firebase (curre
     if (data==='all') {
         cy.visit('?appMode=qa&qaClear=' + data + '&fakeClass=5&fakeUser=student:5');
         cy.get('span', {timeout: 60000}).should('contain','QA Cleared: OK');
+        // According to the firebase documentation and our implementation we shouldn't be showing
+        // QA Cleared: OK until the removal of the node in firebase is complete.
+        // However there are some randoming failing tests which could be explained if this
+        // removal on the firebase server happens later and triggers an event after the next
+        // visit command.
+        // So to be safe there is a wait here to give firebase more of a chance to process.
+        cy.wait(1000);
     }
 });
 
