8ec9fdbb8f0255fddef51dffd7b3100d7b232897
Scott Cytacki <scytacki@concord.org>
2021-11-03 14:28:38 -0400

    a test failed because text was not saved before the page navigation
    This adds a wait after typing the text.
    There doesn't seem to be any debouncing of the text so it should be saved quickly.
    In most cases this seemed to be working, hopefully the wait will decrease the failures.
    As the comment says, ideally there would be a wait make sure the data was actually
    saved.

--- cypress/support/elements/clue/TextToolTile.js
+++ cypress/support/elements/clue/TextToolTile.js
@@ -8,6 +8,11 @@ class TextToolTile {
     enterText(text){
         this.getTextTile().last().focus();
         this.getTextEditor().last().type(text);
+        // This doesn't guarantee the text has been saved to firebase. It would be best
+        // if there was a way to tell if it has been saved perhaps by some saving indicator
+        // in the UI. Or reaching into app to find some saving state.
+        // In the meantime a short wait is added to decrease the chances this might happen
+        cy.wait(300);
     }
 
     deleteText(text){
