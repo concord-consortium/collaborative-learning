ee853e89a6620e51a3e33e5556f690a3c7bf67b1
Scott Cytacki <scytacki@concord.org>
2021-11-08 15:51:15 -0500

    remove undefined toolbar prop support
    the DocumentToolbar is only rendered if the toolbar prop is truthy.
    Also fix a typo.

--- src/components/document/editable-document-content.tsx
+++ src/components/document/editable-document-content.tsx
@@ -18,7 +18,7 @@ import "./editable-document-content.scss";
 
 interface IToolbarProps {
   document: DocumentModelType;
-  toolbar?: ToolbarModelType;
+  toolbar: ToolbarModelType;
 }
 
 const DocumentToolbar: React.FC<IToolbarProps> = ({ toolbar, ...others }) => {
@@ -26,17 +26,15 @@ const DocumentToolbar: React.FC<IToolbarProps> = ({ toolbar, ...others }) => {
 
   // The toolbar prop represents the app's configuration of the toolbar
   // It is cloned here in the document so changes to one document's toolbar
-  // to not effect another document's toolbar.
+  // do not effect another document's toolbar.
   // Currently the toolbar model is not modified, but it seems safer to do this.
   // The cloned model is stored in state so it isn't recreated on each render
-  const [toolbarModel] = useState<ToolbarModelType | undefined>(() => {
+  const [toolbarModel] = useState<ToolbarModelType>(() => {
       // The new model is passed the appIcons as its environment, so the model
       // can lookup an app level Icon if needed.
       return clone(toolbar, { appIcons: appConfig.appIcons });
   });
-  return toolbarModel
-          ? <ToolbarComponent key="toolbar" toolbarModel={toolbarModel} {...others} />
-          : null;
+  return <ToolbarComponent key="toolbar" toolbarModel={toolbarModel} {...others} />;
 };
 
 interface IOneUpCanvasProps {
