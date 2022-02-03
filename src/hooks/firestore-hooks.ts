488ad4931725a7e3fe786d2e5fadf8a9cc7c4efa
Kirk Swenson <kswenson@concord.org>
2022-02-03 08:33:32 -0800

    Show teacher names in network document captions

--- src/hooks/firestore-hooks.ts
+++ src/hooks/firestore-hooks.ts
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useMutation, useQuery, useQueryClient, UseQueryOptions } from 'react-query';
+import { UserDocument } from "../lib/firestore-schema";
 import { useDBStore } from './use-stores';
 
 export type WithId<T> = T & { id: string };
@@ -13,6 +14,32 @@ export function useFirestore() {
     root = root.slice(0, root.length - 1);
   }
   return [db.firestore, root] as const;
+}
+
+const firestoreTeachers: Record<string, UserDocument> = {};
+
+export function useFirestoreTeacher(uid: string, network: string) {
+  const [firestore] = useFirestore();
+  const [ , setCount] = useState(0);
+  if (firestoreTeachers[uid]) return firestoreTeachers[uid];
+
+  // default response until promise resolves
+  firestoreTeachers[uid] = { uid, name: "Network User", type: "teacher", network, networks: [network] };
+
+  firestore.doc(`users/${uid}`).get()
+    .then(snap => {
+      const teacher = snap.data() as UserDocument | undefined;
+      if (teacher) {
+        firestoreTeachers[uid] = teacher;
+        // trigger re-render when we get the results
+        setCount(count => ++count);
+      }
+    })
+    .catch(() => {
+      // ignore errors
+    });
+
+  return firestoreTeachers[uid];
 }
 
 // https://medium.com/swlh/using-firestore-with-typescript-65bd2a602945
