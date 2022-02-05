37d1aa3a9c541681b9c7b994567850de01ba4ebf
Kirk Swenson <kswenson@concord.org>
2022-02-05 13:25:28 -0800

    Update jest configuration and functions documentation

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -1,5 +1,8 @@
 // cf. https://medium.com/@leejh3224/testing-firebase-cloud-functions-with-jest-4156e65c7d29
 module.exports = {
+  "setupFilesAfterEnv": [
+    "<rootDir>/../src/setupTests.ts"
+  ],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
