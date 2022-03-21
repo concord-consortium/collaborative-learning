f76795f0f8f00524089742d97c7cfbce7588f1b5
Scott Cytacki <scytacki@concord.org>
2022-03-21 17:47:35 -0400

    bump the version number shown in diagram tile

--- src/plugins/diagram-viewer/diagram-types.ts
+++ src/plugins/diagram-viewer/diagram-types.ts
@@ -1,7 +1,7 @@
 export const kDiagramToolID = "Diagram";
 
 // taken from most recent tag when code was last synced from quantity-playground repo
-export const kQPVersion = "demo-8+";
+export const kQPVersion = "demo-9+";
 
 export const kDiagramDefaultWidth = 480;
 export const kDiagramDefaultHeight = 320;
