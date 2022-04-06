4cc665e1788b767b25955460fe4cdfe3e3bee942
Scott Cytacki <scytacki@concord.org>
2022-04-06 16:57:41 -0400

    update the displayed version

--- src/plugins/diagram-viewer/diagram-types.ts
+++ src/plugins/diagram-viewer/diagram-types.ts
@@ -1,7 +1,9 @@
 export const kDiagramToolID = "Diagram";
 
-// taken from most recent tag when code was last synced from quantity-playground repo
-export const kQPVersion = "0.0.10-beta.1";
+// Taken from most recent tag when code was last synced from quantity-playground repo
+// TODO: include a version in the diagram-view export so we can just reference that
+// instead
+export const kQPVersion = "0.0.10";
 
 export const kDiagramDefaultWidth = 480;
 export const kDiagramDefaultHeight = 320;
