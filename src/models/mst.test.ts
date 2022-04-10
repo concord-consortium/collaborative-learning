35c7964913bb1cfed5e322f0f90358b6b46b56fa
Scott Cytacki <scytacki@concord.org>
2022-04-10 00:32:31 -0400

    improve comment in test

--- src/models/mst.test.ts
+++ src/models/mst.test.ts
@@ -15,7 +15,10 @@ describe("mst", () => {
     expect(todo2.text).toBe("todo2 text");
 
     const todo1b = Todo1.create();
-    // Is this really expected??
+    // When the type created by the snapshotProcessor (Todo2) is instantiated,
+    // the base type's (Todo1's) create method is modified so it actually goes 
+    // through the processor. This is bug in MST and has been reported here:
+    // https://github.com/mobxjs/mobx-state-tree/issues/1897
     expect(todo1b.text).toBe("todo2 text");
   });
 });
