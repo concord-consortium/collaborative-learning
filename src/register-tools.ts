d805e986b029821ddec90fa81273a90d680aa6e2
Scott Cytacki <scytacki@concord.org>
2022-04-13 17:37:16 -0400

    handle dynamic loading of the shared variables model

--- src/register-tools.ts
+++ src/register-tools.ts
@@ -1,14 +1,14 @@
-// FIXME: switch to dynamic loading here
-// Try importing shared models first since they will be referenced
-// by the some of the models
-import "./plugins/shared-variables/shared-variables-registration";
-
 // import all tools so they are registered
 import "./models/tools/unknown-content";
 import "./models/tools/placeholder/placeholder-registration";
 
 const gToolRegistration: Record<string, () => void> = {
-  "Diagram": () => import(/* webpackChunkName: "Diagram" */"./plugins/diagram-viewer/diagram-registration"),
+  "Diagram": () => {
+    // Currently the only time the SharedVariables, so we register both of them whenever there is 
+    // a Diagram tool in the configuration
+    import(/* webpackChunkName: "Diagram" */"./plugins/diagram-viewer/diagram-registration");
+    import(/* webpackChunkName: "SharedVariables" */"./plugins/shared-variables/shared-variables-registration");
+  },
   "Drawing": () => import(/* webpackChunkName: "Drawing" */"./models/tools/drawing/drawing-registration"),
   "Geometry": () => import(/* webpackChunkName: "Geometry" */"./models/tools/geometry/geometry-registration"),
   "Image": () => import(/* webpackChunkName: "Image" */"./models/tools/image/image-registration"),
