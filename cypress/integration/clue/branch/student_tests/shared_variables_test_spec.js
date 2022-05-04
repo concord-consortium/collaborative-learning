8befb8570aabd8345f8f304825e75e021bc30c38
Scott Cytacki <scytacki@concord.org>
2022-05-04 15:28:33 -0400

    more tests to cover the variables text plugin

--- cypress/integration/clue/branch/student_tests/shared_variables_test_spec.js
+++ cypress/integration/clue/branch/student_tests/shared_variables_test_spec.js
@@ -32,4 +32,43 @@ context('Shared Variables', function () {
       .findByDisplayValue('Var A')
       .should('exist');
   });
+
+  it('can add a duplicate variable chip to the text tool', function() {
+    // Note that when the chip is focused you can't type so we have to use 
+    // rightArrow to move off of the chip
+    textToolTile.enterText('{rightArrow}Second Chip:');
+    textToolTile.getTextTile().last().should('contain', 'Second Chip');
+    textToolTile.clickToolbarTool("Variables");
+    cy.get(".ReactModalPortal").within(() => {
+      cy.findByRole("combobox").type("Var{enter}");
+      cy.findByRole("button", {name: "OK"}).click();
+    });
+    // Make sure the text tile now has 2 chips with Var A.
+    textToolTile.getTextTile().last().find('.ccrte-variable:contains("Var A")').should('have.length', 2);
+  });
+
+  it('can edit a variable by double clicking', function() {
+    textToolTile.getTextTile().last().find('.ccrte-variable').first().dblclick();
+    cy.get(".ReactModalPortal").within(() => {
+      cy.findByLabelText(/Name/).clear().type("Var B");
+      cy.findByRole("button", {name: "OK"}).click();
+    });
+    // Make sure the text tile now has 2 chips with Var B.
+    textToolTile.getTextTile().last().find('.ccrte-variable:contains("Var B")').should('have.length', 2);
+    // Make sure the diagram tile now has a card with Var B.
+    cy.get('.primary-workspace .canvas-area .diagram-tool [data-testid="quantity-node"]')
+      .findByDisplayValue('Var B')
+      .should('exist');
+  });
+
+  it('can set the value of a variable', function() {
+    textToolTile.getTextTile().last().find('.ccrte-variable').first().dblclick();
+    cy.get(".ReactModalPortal").within(() => {
+      cy.findByLabelText(/Value/).clear().type("1.234");
+      cy.findByRole("button", {name: "OK"}).click();
+    });
+    // Make sure the text tile now has 2 chips with Var B.
+    textToolTile.getTextTile().last().find('.ccrte-variable:contains("Var B=1.234")').should('have.length', 2);
+  });
+
 });
