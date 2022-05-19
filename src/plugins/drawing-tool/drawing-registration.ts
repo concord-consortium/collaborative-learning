685d91e894bb536391a5e19b320e6fad4bef8922
Scott Cytacki <scytacki@concord.org>
2022-05-19 16:51:36 -0400

    fix some broken asset paths

--- src/plugins/drawing-tool/drawing-registration.ts
+++ src/plugins/drawing-tool/drawing-registration.ts
@@ -2,7 +2,7 @@ import { registerToolContentInfo } from "../../models/tools/tool-content-info";
 import { DrawingContentModel, DrawingToolMetadataModel, defaultDrawingContent } from "./model/drawing-content";
 import { kDrawingToolID, kDrawingDefaultHeight } from "./model/drawing-types";
 import DrawingToolComponent from "./components/drawing-tool";
-import DrawingToolIcon from "../../../clue/assets/icons/draw-tool.svg";
+import DrawingToolIcon from "../../clue/assets/icons/draw-tool.svg";
 
 registerToolContentInfo({
   id: kDrawingToolID,
