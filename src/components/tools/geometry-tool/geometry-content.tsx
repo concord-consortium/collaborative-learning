97196741ea771f625db100829d811c0ec96e1942
Teale Fristoe <fristoe@gmail.com>
2022-06-01 11:57:07 -0700

    Move line dialog away from blueprint

--- src/components/tools/geometry-tool/geometry-content.tsx
+++ src/components/tools/geometry-tool/geometry-content.tsx
@@ -41,6 +41,7 @@ import AxisSettingsDialog from "./axis-settings-dialog";
 import { EditableGeometryTitle } from "./editable-geometry-title";
 import LabelSegmentDialog from "./label-segment-dialog";
 import MovableLineDialog from "./movable-line-dialog";
+// import { useMovableLineDialog } from "./use-movable-line-dialog";
 import placeholderImage from "../../../assets/image_placeholder.png";
 import { LinkTableButton } from "./link-table-button";
 import ErrorAlert from "../../utilities/error-alert";
@@ -48,6 +49,7 @@ import SingleStringDialog from "../../utilities/single-string-dialog";
 import { autorun, observe } from "mobx";
 
 import "./geometry-tool.sass";
+import { string } from "mobx-state-tree/dist/internal";
 
 export interface IGeometryContentProps extends IGeometryProps {
   onSetBoard: (board: JXG.Board) => void;
@@ -457,7 +459,6 @@ export class GeometryContentComponent extends BaseComponent<IProps, IState> {
       return (
         <MovableLineDialog
           key="editor"
-          isOpen={line != null}
           onAccept={this.handleUpdateLine}
           onClose={this.closeLineDialog}
           line={line}
---
+++ src/components/tools/geometry-tool/use-movable-line-dialog.tsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import LinkGraphIcon from "../../../clue/assets/icons/table/link-graph-icon.svg";
+import { useCustomModal } from "../../../hooks/use-custom-modal";
+import { getBoundingBoxIntersections, solveForY } from "../../../models/tools/geometry/jxg-movable-line";
+import { round } from "lodash";
+
+interface IContentProps {
+  slope: string;
+  setSlope: React.Dispatch<React.SetStateAction<string>>;
+  intercept: string;
+  setIntercept: React.Dispatch<React.SetStateAction<string>>;
+  errorMessage: string;
+}
+const Content: React.FC<IContentProps>
+              = ({ slope, setSlope, intercept, setIntercept, errorMessage })=> {
+
+    return (
+      <>
+        <div className="nc-attribute-name-prompt">Slope:</div>
+        <input
+          className="nc-attribute-name-input pt-input"
+          type="text"
+          maxLength={5}
+          defaultValue={slope}
+          onChange={e => setSlope(e.target.value)}
+          // onKeyDown={handleKeyDown}
+          dir="auto"
+        />
+        <div className="nc-attribute-name-prompt">y-intercept:</div>
+        <input
+          className="nc-attribute-name-input pt-input"
+          type="text"
+          maxLength={5}
+          defaultValue={intercept}
+          onChange={e => setIntercept(e.target.value)}
+          // onKeyDown={handleKeyDown}
+          dir="auto"
+        />
+        <div className="nc-dialog-error">
+          {errorMessage}
+        </div>
+      </>
+    );
+};
+
+interface IProps {
+  line: JXG.Line;
+  onAccept: (line: JXG.Line, point1: [number, number], point2: [number, number]) => void;
+  onClose: () => void;
+}
+export const useMovableLineDialog = ({ line, onAccept, onClose }: IProps) => {
+  const [slope, setSlope] = useState(`${round(line.getSlope(), 1)}`);
+  const [intercept, setIntercept] = useState(`${round(line.getRise(), 1)}`);
+
+  const fSlope = parseFloat(slope);
+  const fIntercept = parseFloat(intercept);
+  const errorMessage =
+    (!isFinite(fSlope) || !isFinite(fIntercept))
+    ? "Please enter a valid number for slope and intercept."
+    : getBoundingBoxIntersections(fSlope, fIntercept, line.board).length < 2
+    ? "Please change the graph axes scale to make the line visible, and try again."
+    : "";
+
+  const getLineControlPoints = () => {
+    if (isFinite(fSlope) && isFinite(fIntercept)) {
+      const board = line.board;
+      const intersections = getBoundingBoxIntersections(fSlope, fIntercept, board);
+      if (!intersections.length) return undefined;
+      let point1: [number, number];
+      let point2: [number, number];
+      if (board.hasPoint(0, fIntercept)) {
+        point1 = [0, fIntercept];
+        const maxX = intersections[1][0];
+        const middleX = maxX / 2;
+        point2 = [middleX, solveForY(fSlope, fIntercept, middleX)];
+      } else {
+        const minX = intersections[0][0];
+        const maxX = intersections[1][0];
+        const lineWidth = maxX - minX;
+        const thirdLineWidth = lineWidth / 3;
+        const x1 = minX + thirdLineWidth;
+        const x2 = minX + thirdLineWidth * 2;
+        point1 = [x1, solveForY(fSlope, fIntercept, x1)];
+        point2 = [x2, solveForY(fSlope, fIntercept, x2)];
+      }
+      return [point1, point2];
+    } else {
+      return undefined;
+    }
+  };
+
+  const handleClick = () => {
+    const points = getLineControlPoints();
+    if (points) {
+      onAccept(line, points[0], points[1]);
+    } else {
+      onClose();
+    }
+  };
+
+  // const handleKeyDown = (evt: React.KeyboardEvent<HTMLInputElement>) => {
+  //   evt.stopPropagation();
+  //   if (evt.keyCode === 13) {
+  //     this.handleAccept();
+  //   } else if (evt.keyCode === 27) {
+  //     this.handleCancel();
+  //   }
+  // };
+
+  const [showModal, hideModal] = useCustomModal({
+    Icon: LinkGraphIcon,
+    title: "Edit Line",
+    Content,
+    contentProps: { slope, setSlope, intercept, setIntercept, errorMessage },
+    buttons: [
+      { label: "Cancel" },
+      { label: "OK",
+        isDefault: true,
+        isDisabled: errorMessage.length > 0,
+        onClick: handleClick
+      }
+    ],
+    onClose
+  }, [slope, intercept, errorMessage]);
+
+  return [showModal, hideModal];
+};
