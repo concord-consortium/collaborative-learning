6e498758c4651f1041a5878859d624d5f3bbc4b4
eireland <eireland@concord.org>
2022-06-08 07:53:38 -0700

    PR fixes

--- src/components/undo-redo-button.tsx
+++ src/components/undo-redo-button.tsx
@@ -2,58 +2,49 @@ import classNames from "classnames";
 import React from "react";
 import { IButtonProps } from "./tool-button";
 
-interface IProps extends IButtonProps {
-  // onSetShowDeleteTilesConfirmationAlert: (showAlert: () => void) => void;
-  // onDeleteSelectedTiles: () => void;
-}
 
-export const UndoButton: React.FC<IProps> =
+
+export const UndoButton: React.FC<IButtonProps> =
   ({ toolButton, isActive, isDisabled, onSetToolActive, onClick }) => {
 
   const { id, title, Icon } = toolButton;
 
-  const handleMouseDown = () => {
-    !isDisabled && onSetToolActive(toolButton, true);
-  };
-
-  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    !isDisabled && onClick(e, toolButton);
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    if(!isDisabled) {
+      onSetToolActive(toolButton, true);
+      onClick(e, toolButton);
+    }
   };
 
   const classes = classNames("tool", "undo-button", id,
                             { active: isActive }, isDisabled ? "disabled" : "enabled");
   return (
-    <div className={classes} data-testid="undo-button"
-        key={id}
-        title={title}
-        onMouseDown={handleMouseDown}
-        onClick={handleClick}>
+    <div className={classes} data-testid="undo-button" key={id} title={title}
+          onMouseDown={handleMouseDown}>
       {Icon && <Icon />}
     </div>
   );
 };
 
-export const RedoButton: React.FC<IProps> =
+export const RedoButton: React.FC<IButtonProps> =
   ({ toolButton, isActive, isDisabled, onSetToolActive, onClick }) => {
 
   const { id, title, Icon } = toolButton;
 
-  const handleMouseDown = () => {
-    !isDisabled && onSetToolActive(toolButton, true);
-  };
-
-  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    !isDisabled && onClick(e, toolButton);
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    if(!isDisabled) {
+      onSetToolActive(toolButton, true);
+      onClick(e, toolButton);
+    }
   };
 
   const classes = classNames("tool", "redo-button", id,
                             { active: isActive }, isDisabled ? "disabled" : "enabled");
   return (
-    <div className={classes} data-testid="redo-button"
-        key={id}
-        title={title}
-        onMouseDown={handleMouseDown}
-        onClick={handleClick}>
+    <div className={classes} data-testid="redo-button" key={id} title={title}
+          onMouseDown={handleMouseDown}>
       {Icon && <Icon />}
     </div>
   );
