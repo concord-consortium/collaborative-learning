2f5f3f135c900d8a3b84d21a65785fffc58cb417
eireland <eireland@concord.org>
2022-06-15 13:43:07 -0700

    Fixes slider styling

--- src/components/playback/slider.tsx
+++ src/components/playback/slider.tsx
@@ -1,29 +1,25 @@
 import React from "react";
 import Slider from "rc-slider";
+import classNames from "classnames";
 import { useUIStore } from "../../hooks/use-stores";
 
 import "./slider.scss";
-import classNames from "classnames";
 
 export const SliderComponent: React.FC = () => {
   const { activeNavTab } = useUIStore();
-  const sliderComponentClass = (`slider-component themed ${activeNavTab}`);
+  const sliderComponentClass = classNames(`slider-component ${activeNavTab}`);
+
   const renderMarkerRail = () => {
     return (
-      <div className={`marker-rail themed ${activeNavTab}`}>
+      <div className={`marker-rail ${activeNavTab}`}>
       </div>
     );
   };
-  // const timeHandle:SliderProps["handleRender"] = (node, props) => {
-  //   const timeHandleStyle = classNames("slider-handle");
-  //   return(
-  //     <TimeHandle className={timeHandleStyle}/>
-  //   );
-  // };
+
   const renderSliderRail = () => {
     return (
-      <div className={`slider-rail themed ${activeNavTab}`}>
-        <Slider min={0} max={100} />
+      <div className="slider-rail">
+        <Slider min={0} max={100} className={`${activeNavTab}`} />
       </div>
     );
   };
