306173cd80b511a0e571ece3b76a765f02bfd460
Teale Fristoe <fristoe@gmail.com>
2022-06-17 10:27:36 -0700

    Get minigraphs working in editable mode

--- src/plugins/dataflow-tool/nodes/dataflow-node-plot.tsx
+++ src/plugins/dataflow-tool/nodes/dataflow-node-plot.tsx
@@ -30,44 +30,28 @@ function lineData(node: any) {
   const data = node.data.recentValues;
   const chartDataSets: ChartDataSets[] = [];
 
-  let graphKeys: string[] = [];
-  if (data && data.length) {
-    graphKeys = Object.keys(data[data.length - 1]);
-  }
-
   let dsMax = 0;
   let dsMin = 0;
-  if (graphKeys && graphKeys.length) {
-    graphKeys.forEach((graphKey, index: number) => {
-      // set up a new dataset to be graphed
-      const dataset: ChartDataSets = {
-        backgroundColor: NodePlotColor,
-        borderColor: NodePlotColor,
-        borderWidth: 2,
-        pointRadius: 2,
-        data: [0],
-        fill: false,
-      };
-      chartDataSets.push(dataset);
+  const dataset: ChartDataSets = {
+    backgroundColor: NodePlotColor,
+    borderColor: NodePlotColor,
+    borderWidth: 2,
+    pointRadius: 2,
+    data: [0],
+    fill: false,
+  };
+  chartDataSets.push(dataset);
 
-      // get all values for this key
-      const chdata: any[] = [];
-      data.forEach((recVal: any) => {
-        if (recVal[graphKey]) {
-          chdata.push(recVal[graphKey].val);
-        } else {
-          chdata.push(Number.NaN);
-        }
-        if (chdata && chdata.length && isFinite(chdata[chdata.length - 1])) {
-          dsMax = Math.max(dsMax, chdata[chdata.length - 1]);
-          dsMin = Math.min(dsMin, chdata[chdata.length - 1]);
-        }
-      });
-      // put values in dataset
-      chartDataSets[index].data = chdata;
-    });
-    stepY = (dsMax - dsMin) / 2;
-  }
+  const chData: any[] = [];
+  data.forEach((val: any) => {
+    if (isFinite(val)) {
+      chData.push(val);
+      dsMax = Math.max(dsMax, val);
+      dsMin = Math.min(dsMin, val);
+    }
+  });
+  chartDataSets[0].data = chData;
+  stepY = (dsMax - dsMin) / 2;
 
   const chartData: ChartData = {
     labels: new Array(MAX_NODE_VALUES).fill(undefined).map((val,idx) => idx),
