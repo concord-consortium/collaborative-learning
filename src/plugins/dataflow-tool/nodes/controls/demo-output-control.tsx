2687ee5779176873eb7592a47e88a8be9fc0669e
Teale Fristoe <fristoe@gmail.com>
2022-07-15 09:37:07 -0700

    Start adding tilt to animation.

--- src/plugins/dataflow-tool/nodes/controls/demo-output-control.tsx
+++ src/plugins/dataflow-tool/nodes/controls/demo-output-control.tsx
@@ -16,12 +16,9 @@ export class DemoOutputControl extends Rete.Control {
     this.key = key;
     this.node = node;
 
-    this.component = (compProps: {value: number, type: string}) => {
+    this.component = (compProps: {value: number, percentOpen: number, percentTilt: number, type: string}) => {
       const controlClassName = compProps.type === "Light Bulb" ? "lightbulb-control" : compProps.type === "Backyard Claw" ? "backyard-claw-control" : "grabber-control";
-      let percentOpen = compProps.value;
-      percentOpen = Math.min(1, percentOpen);
-      percentOpen = Math.max(0, percentOpen);
-      const frame = percentOpen < 1 ? Math.floor(grabberClawFrames.length * percentOpen) :  grabberClawFrames.length - 1;
+      const frame = compProps.percentOpen < 1 ? Math.floor(grabberClawFrames.length * compProps.percentOpen) :  grabberClawFrames.length - 1;
       return (
         <div className={`demo-output-control ${controlClassName}`}>
           {compProps.type === "Light Bulb"
@@ -37,20 +34,45 @@ export class DemoOutputControl extends Rete.Control {
       );
     };
 
-    const initial = node.data[key] || 0;
+    const initial = 0;
+    const initialTilt = 0;
     node.data[key] = initial;
 
     const initialType = "Light Bulb";
 
     this.props = {
       value: initial,
+      percentOpen: this.getPercentOpen(initial),
+      tilt: initialTilt,
+      percentTilt: this.getPercentTilt(initialTilt),
       type: initialType
     };
   }
 
+  // Converts a number to [0,1]
+  private getPercentOpen = (val: number) => {
+    let percentOpen = Math.min(1, val);
+    percentOpen = Math.max(0, percentOpen);
+    return percentOpen
+  };
+
   public setValue = (val: number) => {
     this.props.value = val;
+    this.props.percentOpen = this.getPercentOpen(val);
     this.putData(this.key, val);
+    (this as any).update();
+  };
+
+  // Converts [-1,1] to [0,1]. Caps at 0 and 1.
+  private getPercentTilt = (tilt: number) => {
+    let percentTilt = (tilt + 1) / 2;
+    percentTilt = Math.min(1, percentTilt);
+    percentTilt = Math.max(0, percentTilt);
+    return percentTilt;
+  };
+
+  public setTilt = (tilt: number) => {
+    this.props.tilt = tilt;
     (this as any).update();
   };
 
