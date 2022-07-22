cd3fe11d030784f5ee317aa274491e0e5990604d
Joe Bacal <bacalj@gmail.com>
2022-07-22 15:28:44 -0400

    clean up sketch

--- src/plugins/dataflow-tool/arduino/ccemgfsr/ccemgfsr.ino
+++ src/plugins/dataflow-tool/arduino/ccemgfsr/ccemgfsr.ino
@@ -25,7 +25,7 @@
 
   //EMG saturation values (when EMG reaches this value the gripper will be fully opened/closed)
   int sensitivities[] = {200, 350, 520, 680, 840, 1000};
-  int lastSensitivitiesIndex = 2;             //set initial sensitivity index
+  int lastSensitivitiesIndex = 3;             //set initial sensitivity index
 
   int emgSaturationValue = 0;                 //selected sensitivity/EMG saturation value
   int emgReading;                             //measured value for EMG
@@ -46,22 +46,14 @@
   String emgId = "emg";                       //key for Dataflow to know what sensor this came from
   String fsrId = "fsr";                       //key for Dataflow to know what sensor this came from
   String kvSeparator = ":";                   //separator for key and value
-  String emgStringOut = "";              // init empty string for default output
-  String fsrStringOut = "";              // init empty string for default output
+  String emgStringOut = "";                   // init empty string for default output
+  String fsrStringOut = "";                   // init empty string for default output
 
-  String fromComputer;
-  int readingFromDataflow;
+  const int BUFFER_SIZE = 4;                  // Accomodate size of new angles coming in which will be 120 - 180, plus delimitter
+  char buf[BUFFER_SIZE];                      // a char array buffer
 
-  int inB = 0;
+  const unsigned int MAX_ANGLE_BYTE_LENGTH = 4;
 
-  char buffer[13];
-
-  const int BUFFER_SIZE = 4;
-  char buf[BUFFER_SIZE];
-
-  String myChar;
-
-  const unsigned int MAX_MESSAGE_LENGTH = 4;
   //-----------------------------------------------------------------------------------
   //   Setup servo, inputs and outputs
   // ----------------------------------------------------------------------------------
@@ -83,9 +75,6 @@
 
     //get current sensitivity
     emgSaturationValue = sensitivities[lastSensitivitiesIndex];
-
-
-
   }
 
 
@@ -99,160 +88,131 @@
   //   - Shows EMG strength on LED bar
   //   - Sets angle of servo based on EMG strength and current mode (open/closed)
   // ----------------------------------------------------------------------------------
+
   void loop()
   {
+    // Thanks, https://www.programmingelectronics.com/serial-read/
+    // Thanks, https://docs.arduino.cc/built-in-examples/strings/StringToInt
 
-    // mite also try this: https://docs.arduino.cc/built-in-examples/strings/StringToInt
+    // Check to see if anything is in the serial receive buffer
+    while (Serial.available() > 0){
+      //Create a place to hold the incoming angle |message|
+      static char message[MAX_ANGLE_BYTE_LENGTH];
+      static unsigned int message_pos = 0;
 
-    //Check to see if anything is available in the serial receive buffer
- while (Serial.available() > 0)
- {
-   //Create a place to hold the incoming message
-   static char message[MAX_MESSAGE_LENGTH];
-   static unsigned int message_pos = 0;
+      //Read the next available byte in the serial receive buffer
+      char inByte = Serial.read();
 
-   //Read the next available byte in the serial receive buffer
-   char inByte = Serial.read();
+      //Message coming in (check not terminating character) and guard for over message size
+      if ( inByte != '\n' && (message_pos < MAX_ANGLE_BYTE_LENGTH - 1) ){
+        //Add the incoming byte to our message
+        message[message_pos] = inByte;
+        message_pos++;
+      }
+      //Full message received...
+      else {
+        //Add null character
+        message[message_pos] = '\0';
+        // we need to go Char -> String -> Int
+        String asString = String(message);
+        Gripper.write(asString.toInt());
 
-   //Message coming in (check not terminating character) and guard for over message size
-   if ( inByte != '\n' && (message_pos < MAX_MESSAGE_LENGTH - 1) )
-   {
-     //Add the incoming byte to our message
-     message[message_pos] = inByte;
-     message_pos++;
-   }
-   //Full message received...
-   else
-   {
-     //Add null character to string
-     message[message_pos] = '\0';
-
-     //Print the message (or do other things)
-     Serial.println(message);
-     String myStuff = String(message);
-     Gripper.write(myStuff.toInt());
-
-     //Reset for the next message
-     message_pos = 0;
-   }
- }
+        message_pos = 0;
+      }
+    }
 
 
-        //-----------------------  Switch sensitivity ------------------------------------
+    //-----------------------  Button to change sensitivity with blink feedback ------------------------------------
 
-        //check if button is pressed (HIGH)
-        if (digitalRead(SENSITIVITY_BUTTON_PIN))
-        {
-            //turn off all the LEDs in LED bar
-            for(int j = 0; j < NUM_LED; j++)
-            {
-              digitalWrite(ledPins[j], LOW);
+    //check if button is pressed (HIGH)
+    if (digitalRead(SENSITIVITY_BUTTON_PIN)){
+      //turn off all the LEDs in LED bar
+      for(int j = 0; j < NUM_LED; j++){
+        digitalWrite(ledPins[j], LOW);
+      }
+
+      //increment sensitivity index
+      lastSensitivitiesIndex++;
+      if(lastSensitivitiesIndex==NUM_LED){
+        lastSensitivitiesIndex = 0;
+      }
+
+      //get current sensitivity value
+      emgSaturationValue = sensitivities[lastSensitivitiesIndex];
+
+      //light up LED at lastSensitivitiesIndex position for visual feedback
+      digitalWrite(ledPins[lastSensitivitiesIndex], HIGH);
+
+      //wait user to release button
+      while (digitalRead(SENSITIVITY_BUTTON_PIN)){
+        delay(10);
+      }
+      //whait a bit more so that LED feedback is visible
+      delay(100);
+    }
+
+
+    //---------------  Switch gripper default position open/close --------------------
+
+    // TODO (CLAW): Remove this as it is not compatible with "percent closed" paradigm in Dataflow control block
+
+    //check if enough time has passed for button contact to settle down
+    if((millis() - debouncerTimer) > 50){
+        gripperStateButtonValue = digitalRead(GRIPPER_STATE_BUTTON_PIN);
+        //if button is pressed
+        if(gripperStateButtonValue == HIGH){
+          //if last time we checked button was not pressed
+          if(userReleasedButton){
+            debouncerTimer = millis();
+            //block button events until user releases it
+            userReleasedButton = 0;
+
+            //toggle operation mode
+            if(currentFunctionality == OPEN_MODE){
+              currentFunctionality = CLOSED_MODE;
             }
-
-            //increment sensitivity index
-            lastSensitivitiesIndex++;
-            if(lastSensitivitiesIndex==NUM_LED)
-            {
-              lastSensitivitiesIndex = 0;
+            else {
+              currentFunctionality = OPEN_MODE;
             }
-
-            //get current sensitivity value
-            emgSaturationValue = sensitivities[lastSensitivitiesIndex];
-
-            //light up LED at lastSensitivitiesIndex position for visual feedback
-            digitalWrite(ledPins[lastSensitivitiesIndex], HIGH);
-
-            //wait user to release button
-            while (digitalRead(SENSITIVITY_BUTTON_PIN))
-            {
-              delay(10);
-            }
-            //whait a bit more so that LED light feedback is always visible
-            delay(100);
+          }
         }
-
-
-        //----------------------------  Switch gripper default position open/close --------------------
-
-        //check if enough time has passed for button contact to settle down
-        if((millis() - debouncerTimer) > 50)
-        {
-            gripperStateButtonValue = digitalRead(GRIPPER_STATE_BUTTON_PIN);
-            //if button is pressed
-            if(gripperStateButtonValue == HIGH)
-            {
-                //if last time we checked button was not pressed
-                if(userReleasedButton)
-                {
-                    debouncerTimer = millis();
-                    //block button events until user releases it
-                    userReleasedButton = 0;
-
-                    //toggle operation mode
-                    if(currentFunctionality == OPEN_MODE)
-                    {
-                      currentFunctionality = CLOSED_MODE;
-                    }
-                    else
-                    {
-                      currentFunctionality = OPEN_MODE;
-                    }
-                }
-             }
-             else
-             {
-                userReleasedButton = 1;
-             }
+        else {
+          userReleasedButton = 1;
         }
+    }
 
+    //-----------------------------  Measure EMG -----------------------------------------------
 
-        //-----------------------------  Measure EMG -----------------------------------------------
+    emgReading = analogRead(A0);                //read EMG value from analog input A0
+    fsrReading = analogRead(A1);                //read FSR value from analog input A1
 
-        emgReading = analogRead(A0);                //read EMG value from analog input A0
-        fsrReading = analogRead(A1);                //read FSR value from analog input A1
+    //---------------- Show EMG strength on LED and Send Sensor Data ---------------------------
 
-        //---------------------- Show EMG strength on LED ------------------------------------------
+    //turn OFF all LEDs on LED bar
+    for(int j = 0; j < NUM_LED; j++){
+      digitalWrite(ledPins[j], LOW);
+    }
 
-        //turn OFF all LEDs on LED bar
-        for(int j = 0; j < NUM_LED; j++)
-        {
-          digitalWrite(ledPins[j], LOW);
-        }
+    //calculate what LEDs should be turned ON on the LED bar
+    emgReading= constrain(emgReading, 30, emgSaturationValue);
+    ledbarHeight = map(emgReading, 30, emgSaturationValue, 0, NUM_LED);
 
-        //calculate what LEDs should be turned ON on the LED bar
-        emgReading= constrain(emgReading, 30, emgSaturationValue);
-        ledbarHeight = map(emgReading, 30, emgSaturationValue, 0, NUM_LED);
+    //turn ON LEDs on the LED bar
+    for(int k = 0; k < ledbarHeight; k++){
+      digitalWrite(ledPins[k], HIGH);
+    }
 
-        //turn ON LEDs on the LED bar
-        for(int k = 0; k < ledbarHeight; k++)
-        {
-          digitalWrite(ledPins[k], HIGH);
-        }
+    // use existing timing code to wait a little before writing sensor data out
+    if (millis() - oldTime > MINIMUM_SERVO_UPDATE_TIME){
+      oldTime = millis();
+      oldDegrees = newDegree;
 
+      // assemble keyed strings so Dataflow knows what is what
+      emgStringOut = String(emgId + kvSeparator + emgReading);
+      //fsrStringOut = String(fsrId + kvSeparator + fsrReading);
 
-
-
-
-        //set new angle if enough time passed
-        if (millis() - oldTime > MINIMUM_SERVO_UPDATE_TIME)
-        {
-
-
-              //Gripper.write(inB);
-
-              oldTime = millis();
-
-              oldDegrees = newDegree;
-
-              emgStringOut = String(emgId + kvSeparator + emgReading);
-              //fsrStringOut = String(fsrId + kvSeparator + fsrReading);
-
-
-
-
-
-              // seems like the right shape at this point
-             Serial.println(emgStringOut);
-              // Serial.println(fsrStringOut);
-        }
+      // send data to serial where Dataflow will find it
+      Serial.println(emgStringOut);
+      // Serial.println(fsrStringOut);
+    }
 }
