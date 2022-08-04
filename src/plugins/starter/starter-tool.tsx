17d71c96b9c80784bdea14af09bca64034ff13b7
Kirk Swenson <kswenson@concord.org>
2022-08-04 12:04:51 -0700

    Add comment about stale closure concerns

--- src/plugins/starter/starter-tool.tsx
+++ src/plugins/starter/starter-tool.tsx
@@ -5,6 +5,11 @@ import { StarterContentModelType } from "./starter-content";
 import "./starter-tool.scss";
 
 export const StarterToolComponent: React.FC<IToolTileProps> = observer((props) => {
+  // Note: capturing the content here and using it in handleChange() below may run the risk
+  // of encountering a stale closure issue depending on the order in which content changes,
+  // component renders, and calls to handleChange() occur. See the PR discussion at
+  // (https://github.com/concord-consortium/collaborative-learning/pull/1222/files#r824873678
+  // and following comments) for details. We should be on the lookout for such issues.
   const content = props.model.content as StarterContentModelType;
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
