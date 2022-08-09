02775b8ef4e72a1c85825866510cd9bcd4f00a1a
eireland <eireland@concord.org>
2022-08-09 10:31:46 -0700

    Refactors play/pause button to be in the same component as the slider component Adds history tree to time travel UI. Does not update document state as playback is happening.

--- src/components/playback/playback.tsx
+++ src/components/playback/playback.tsx
@@ -1,24 +1,30 @@
 import React, { useState } from "react";
+import { Instance } from "mobx-state-tree";
+import { observer } from "mobx-react";
 import classNames from "classnames";
-import { useUIStore } from "../../hooks/use-stores";
-import { SliderComponent } from "./slider";
+import { useDocumentFromStore, useUIStore } from "../../hooks/use-stores";
+import { TreeManager } from "../../models/history//tree-manager";
+import { PlaybackControlComponent } from "./playback-control";
 import PlaybackIcon from "../../clue/assets/icons/playback/playback-icon.svg";
-import PlayButton from "../../clue/assets/icons/playback/play-button.svg";
-import PauseButton from "../../clue/assets/icons/playback/pause-button.svg";
 
 import "./playback.scss";
 
-export const PlaybackComponent: React.FC = () => {
+interface IProps {
+  documentId: string | undefined;
+}
+
+export const PlaybackComponent: React.FC<IProps> = observer((props: IProps) => {
+  const { documentId } = props;
   const { activeNavTab } = useUIStore();
   const [showPlaybackControls, setShowPlaybackControls] = useState(false);
-  const [sliderPlaying, setSliderPlaying] = useState(false);
+  const document = useDocumentFromStore(documentId);
+  const treeManager = document?.treeManagerAPI as Instance<typeof TreeManager>;
+  const history = treeManager?.document.history;
 
   const handleTogglePlaybackControlComponent = () => {
     setShowPlaybackControls(!showPlaybackControls);
   };
-  const handlePlayPauseToggle = () => {
-    setSliderPlaying(!sliderPlaying);
-  };
+
   const renderPlaybackToolbarButton = () => {
     const playbackToolbarButtonComponentStyle =
       classNames("playback-toolbar-button-component", {"disabled" : false},
@@ -33,47 +39,16 @@ export const PlaybackComponent: React.FC = () => {
     );
   };
 
-  const renderPlayPauseButton = () => {
-    const playButtonStyle = classNames("play-button", "themed", activeNavTab, {"disabled" : false});
-    const pauseButtonStyle = classNames("pause-button", "themed", activeNavTab, {"playing" : sliderPlaying});
-    if (sliderPlaying) {
-      return <PauseButton className={pauseButtonStyle} onClick={handlePlayPauseToggle}/>;
-    } else {
-      return <PlayButton className={playButtonStyle} onClick={handlePlayPauseToggle}/>;
-    }
-  };
-
-  const renderTimeInfo = () => {
-    return (
-      <div className={"time-info"}>
-        <div className={"date-info"}>Nov. 25, 2022</div>
-        <div className={"time-info"}>12:55 AM</div>
-      </div>
-    );
-  };
-
-  const renderPlaybackControls = () => {
-    const playbackControlsClass = classNames("playback-controls", activeNavTab);
-// TODO: play button is disabled because user is already at the end
-//       moving time handle enables play button
-
-    return (
-      <div className={playbackControlsClass}>
-        <div className={`control-separator ${activeNavTab}`}/>
-        {renderPlayPauseButton()}
-        <SliderComponent sliderPlaying={sliderPlaying} onTogglePlayPause={handlePlayPauseToggle}/>
-        {renderTimeInfo()}
-      </div>
-    );
-  };
-
+  const disablePlayback = history.length < 1;
   const playbackComponentClass = classNames("playback-component", activeNavTab,
-                                            {"show-control" : showPlaybackControls});
-
+                                            {"show-control" : showPlaybackControls,
+                                              "disabled" : disablePlayback});
   return (
     <div className={playbackComponentClass}>
       {renderPlaybackToolbarButton()}
-      {showPlaybackControls && renderPlaybackControls()}
+      {showPlaybackControls && <PlaybackControlComponent treeManager={treeManager} />}
     </div>
   );
-};
+});
+PlaybackComponent.displayName = "PlaybackComponent";
+
