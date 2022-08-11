5edba8f13e5c636da19d7ea19b07a9c1edd890bb
Joe Bacal <bacalj@gmail.com>
2022-08-11 11:05:08 -0400

    tile title is editable again

--- src/plugins/deck-tool/deck-tool.tsx
+++ src/plugins/deck-tool/deck-tool.tsx
@@ -2,11 +2,14 @@ import { observer } from "mobx-react";
 import React, { useEffect } from "react";
 import { IToolTileProps } from "../../components/tools/tool-tile";
 import { DeckContentModelType } from "./deck-content";
+import { EditableTileTitle } from "../../components/tools/editable-tile-title";
+import { measureText } from "../../components/tools/hooks/use-measure-text";
+import { defaultTileTitleFont } from "../../components/constants";
 
 import "./deck-tool.scss";
 
 export const DeckToolComponent: React.FC<IToolTileProps> = observer((props) => {
-  const { onRegisterToolApi, model } = props;
+  const { onRegisterToolApi, model, scale, readOnly } = props;
 
   const content = model.content as DeckContentModelType;
 
@@ -21,18 +24,23 @@ export const DeckToolComponent: React.FC<IToolTileProps> = observer((props) => {
   }
 
   useEffect(() => {
-    onRegisterToolApi({
-      getTitle: ()=>{
-        return theTitle();
-      }
-    })
-  },[])
+    onRegisterToolApi({ getTitle: () => theTitle() });
+  }, [content.metadata.title]);
+
+  const updateDeckTitle = (event: any) => {
+    content.setTitle(event);
+  };
 
   return (
-    <div className="deck-tool">
-      <input
-        value={theTitle()}
-        onChange={e => content.setTitle(e.target.value)}
+    <div className="deck-tool" style={{ border: "3px dashed silver", padding: "6px" }}>
+      <EditableTileTitle
+        key="deck-title"
+        size={{width: null, height: null}}
+        scale={scale}
+        getTitle={theTitle}
+        readOnly={readOnly}
+        measureText={(text) => measureText(text, defaultTileTitleFont)}
+        onEndEdit={updateDeckTitle}
       />
     </div>
   );
