e801b711bd85103131d04aac6f730425b77ba916
Joe Bacal <bacalj@gmail.com>
2022-08-16 11:41:53 -0400

    wip componentizing

--- src/plugins/deck-tool/components/deck-card-data.tsx
+++ src/plugins/deck-tool/components/deck-card-data.tsx
@@ -11,68 +11,26 @@ export const DeckCardData: React.FC<IProps> = ({ caseIndex, model }) => {
   const content = model.content as DeckContentModelType;
   const thisCase = content.caseByIndex(caseIndex);
 
-  if (thisCase){
-    const keysHere = Object.keys(thisCase).filter(k => k !== "__id__");
-    const caseData = keysHere.map((k) => {
-      const attrName = content.attrById(k).name;
-      return thisCase ? { a: attrName, v: thisCase[k]} : undefined;
-    });
-
-    console.log(caseData);
+  // TODO what else FC<IProps> will accept as a return if I am to make this check
+  if (!thisCase){
+    return null;
   }
 
+  const keysHere = Object.keys(thisCase).filter(k => k !== "__id__");
+  const caseData = keysHere.map((k) => {
+    const attrName = content.attrById(k).name;
+    return thisCase ? { a: attrName, v: thisCase[k]} : undefined;
+  });
 
+  const caseHtml = caseData.map((caseDataPoint, i) => {
+    return (
+      <div className="case-item" key={i}>
+        <div className="attribute"><b>{caseDataPoint?.a}</b></div>
+        <div className="value">{caseDataPoint?.v}</div>
+      </div>
+    )
+  })
 
-  return (
-    <div>
-      caseIndex: { caseIndex }
-    </div>
-  );
+  return (<>{caseHtml}</>)
 };
 
-
-
-// import React from "react";
-// import { IBaseProps } from "../../../app-config";
-// import { DeckContentModelType } from "../deck-content"
-// import { ToolTileModelType } from "../../../models/tools/tool-tile"
-
-
-// interface IProps  {
-//   caseIndex: number;
-//   model: ToolTileModelType;
-//   //onSomeEvent: () => void;
-// }
-
-// export const DeckCardData: React.FC = ({model, caseIndex}) => {
-//   const content = model.content as DeckContentModelType;
-//   const thisCase = content.caseByIndex(caseIndex);
-
-
-
-//   }
-
-
-//   return (
-//     <div className="data-deck-card-data" key="data-area">
-//         { children }
-//     </div>
-//   );
-// };
-
-// interface IProps {
-//   stamp: StampModelType;
-//   isSelected: boolean;
-//   onSelectStamp: () => void;
-// }
-// export const StampButton: React.FC<IProps> = ({ stamp, isSelected, onSelectStamp }) => {
-//   return (
-//     <div className={classNames("stamp-button", { select: isSelected })} onClick={() => onSelectStamp()}>
-//       <img src={stamp.url} draggable="false" />
-//       <svg className={`highlight ${isSelected ? "select" : ""}`}
-//             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="30">
-//         <rect x="1" y="1" width="28" height="28" strokeWidth="2" fill="none"/>
-//       </svg>
-//     </div>
-//   );
-// };
