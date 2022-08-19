ded25c69858744dd62d5240d933b696477384538
Joe Bacal <bacalj@gmail.com>
2022-08-19 00:03:20 -0400

    saving a value change

--- src/plugins/deck-tool/components/deck-card-data.tsx
+++ src/plugins/deck-tool/components/deck-card-data.tsx
@@ -21,15 +21,45 @@ export const DeckCardData: React.FC<IProps> = observer(({ caseIndex, model, tota
 
   useEffect(()=>{
     setCurrentCaseId(() => {
-      return content.caseByIndex(caseIndex)?.__id__ || "no_id"
+      return content.caseByIndex(caseIndex)?.__id__ || "no_id";
     })
-  }, [caseIndex])
+  }, [caseIndex]);
 
   useEffect(()=>{
     const raw = content.caseByIndex(caseIndex);
     const filteredKeys = raw ? Object.keys(raw).filter(k => k !== "__id__") : ["label1"];
     setAttrKeys(filteredKeys);
-  },[model])
+  },[model]);
+
+  function activateInput(e:any){
+    const attrToEdit = (e.target.classList[1]);
+    const facetToEdit = (e.target.classList[0]);
+
+    if (facetToEdit === "name" || "value"){
+      setActiveFacet(facetToEdit);
+      setActiveAttrId(attrToEdit);
+
+      if (facetToEdit === "name"){
+        setCandidate(content.attrById(attrToEdit).name)
+      }
+
+      if (facetToEdit === "value"){
+        const currentVal = content.dataSet.getValue(currentCaseId, attrToEdit);
+        setCandidate(currentVal as string);
+      }
+    }
+  };
+
+  function saveClear(){
+    if (activeFacet === "value"){
+      content.setAttValue(currentCaseId, activeAttrId, candidate);
+    } else if (activeFacet === "name") {
+      content.setAttName(activeAttrId, candidate);
+    }
+    setActiveFacet("");
+    setActiveAttrId("");
+    setCandidate("");
+  };
 
   return (
     <>
@@ -37,7 +67,19 @@ export const DeckCardData: React.FC<IProps> = observer(({ caseIndex, model, tota
         return (
           <div key={a} className={`attribute-name-value-pair ${currentCaseId}`}>
             <div className={`name ${a}`}>{ content.dataSet.attrFromID(a).name}</div>
-            <div className={`value ${a}`}>{content.dataSet.getValue(currentCaseId, a ) }</div>
+
+            <div className={`value ${a}`} onDoubleClick={activateInput}>
+              { activeAttrId === a && activeFacet === "value"
+                ? <input
+                    key={`${a}_value`}
+                    type="text"
+                    value={candidate || ""}
+                    onChange={(e:any) => setCandidate(e.target.value)}
+                    onBlur={() => {saveClear()}}
+                  />
+                : content.dataSet.getValue(currentCaseId, a)
+              }
+            </div>
           </div>
         )
       })}
