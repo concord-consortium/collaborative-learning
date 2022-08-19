859195ae47659ffea43bb576e6584c9d5972871e
Joe Bacal <bacalj@gmail.com>
2022-08-19 15:59:40 -0400

    rewind time

--- src/plugins/deck-tool/components/new-card-attribute.tsx
+++ src/plugins/deck-tool/components/new-card-attribute.tsx
@@ -19,20 +19,22 @@ export const NewCardAttribute: React.FC<IProps> = observer(({ caseIndex, model, 
   const attrId = useRef("");
 
   useEffect(()=>{
-    if (attrId.current === ""){
-      console.log("no attr in play, lets set it.");
-      setUpNewAttr();
-    } else {
-      console.log("existing attr being worked on");
-    }
-
+    // if (attrId.current === ""){
+    //   console.log("no attr in play, lets set it.");
+    //   setUpNewAttr();
+    //   attrId.current = "bob123"
+    // } else {
+    //   console.log("existing attr being worked on");
+    // }
+    // console.log(attrId.current);
   });
 
   function setUpNewAttr(){
     const nextLabel = `Label ${attrsCount + 1}`;
     console.log("ok, here is nextLabel: ", nextLabel);
 
-    // addAttributeToDataSet(content.dataSet, { name: nextLabel });
+    addAttributeToDataSet(content.dataSet, { name: nextLabel });
+
     //setAttrsCount(content.existingAttributes().length);
   }
 
