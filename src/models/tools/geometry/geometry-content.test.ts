e53a2512b08cdebe5a4349c615a1ad854d9a6d7f
Teale Fristoe <fristoe@gmail.com>
2022-08-22 07:17:00 -0700

    Clean up test

--- src/models/tools/geometry/geometry-content.test.ts
+++ src/models/tools/geometry/geometry-content.test.ts
@@ -138,13 +138,8 @@ describe("GeometryContent", () => {
   function createTileAndBoard(
       configContent?: (content: GeometryContentModelType) => void):
       { tile: ToolTileModelType, board: JXG.Board } {
-    // ignore warning about not being able to find parent in getToolTileModel()
-    // I believe this warning is occurring because we're passing a content model, rather than
-    // a snapshot, to ToolTileModel.create(), and the content model has a title getter
-    const spy = jest.spyOn(console, "warn").mockImplementation(() => null);
     const { content, board } = createContentAndBoard(configContent);
-    const tile = ToolTileModel.create({ content });
-    spy.mockRestore();
+    const tile = ToolTileModel.create({ content: getSnapshot(content) });
     return { tile, board };
   }
 
