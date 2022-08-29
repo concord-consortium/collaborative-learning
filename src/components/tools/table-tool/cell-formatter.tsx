a38ae89fec00b5a4b697728cd18d6de9e0a52cbc
Teale Fristoe <fristoe@gmail.com>
2022-08-29 07:50:29 -0700

    Groundwork for better cell formatting

--- src/components/tools/table-tool/cell-formatter.tsx
+++ src/components/tools/table-tool/cell-formatter.tsx
@@ -1,11 +1,19 @@
+import React from "react";
 import { TFormatterProps } from "./table-types";
 import { useNumberFormat } from "./use-number-format";
 
 export const formatValue = (formatter: (n: number | { valueOf(): number }) => string, value: any) => {
-  if ((value == null) || (value === "")) return "";
+  if ((value == null) || (value === "")) return <span></span>;
   const num = Number(value);
-  if (!isFinite(num)) return value;
-  return formatter(num);
+  if (!isFinite(num)) {
+    return (
+      <div style={{height: 240, width: 80, whiteSpace: 'normal',
+      overflowWrap: 'break-word', textAlign: 'left'}}>
+        {value}
+      </div>
+    );
+  }
+  return <span>{formatter(num)}</span>;
 };
 
 export const CellFormatter: React.FC<TFormatterProps> = ({ column, row }) => {
