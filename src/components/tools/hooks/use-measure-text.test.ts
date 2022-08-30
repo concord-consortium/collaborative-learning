dfb428f3c2f11fa1baa09889314227348ab50603
Teale Fristoe <fristoe@gmail.com>
2022-08-30 12:28:11 -0700

    Add tests for measureTextLines()

--- src/components/tools/hooks/use-measure-text.test.ts
+++ src/components/tools/hooks/use-measure-text.test.ts
@@ -1,7 +1,8 @@
 // mock the measureText function
 const mockMeasureText = jest.fn((text: string, fontSize: number) => {
   // assume every character is half the width of the font's height
-  return { width: text.length * fontSize / 2 };
+  const width = text.length * fontSize / 2;
+  return { width };
 });
 
 // mock the 2D canvas context
@@ -15,7 +16,7 @@ class MockCanvas2DContext {
   }
 
   measureText(text: string) {
-    return mockMeasureText(text, this.fontSize / 2);
+    return mockMeasureText(text, this.fontSize);
   }
 }
 
@@ -25,7 +26,7 @@ const mockCreateElement = jest.spyOn(document, "createElement").mockImplementati
 } as any as HTMLCanvasElement));
 
 import { defaultFont } from "../../constants";
-import { measureText } from "./use-measure-text";
+import { measureText, measureTextLines } from "./use-measure-text";
 
 describe("measureText", () => {
   const testText = "test text";
@@ -60,4 +61,39 @@ describe("measureText", () => {
     // both results were cached by previous tests
     expect(mockMeasureText).toHaveBeenCalledTimes(0);
   });
+});
+
+// Each character is 7px wide
+// Each line is 70px wide, so 10 characters
+const lineWidth = 70;
+const mtl = (text: string) => measureTextLines(text, lineWidth);
+describe("measureTextLines", () => {
+  beforeEach(() => {
+    mockMeasureText.mockClear();
+  });
+
+  afterAll(() => {
+    mockCreateElement.mockRestore();
+  });
+
+  it("handles empty strings", () => {
+    expect(mtl("")).toEqual(1);
+  });
+  it("handles short words", () => {
+    expect(mtl("abcd")).toEqual(1);
+  });
+  it("handles words of the exact line length", () => {
+    expect(mtl("abcdefghij")).toEqual(1);
+  });
+  it("handles long words", () => {
+    expect(mtl("abcdefghijk")).toEqual(2);
+  });
+  it("handles multiple words", () => {
+    expect(mtl("abcde fghij")).toEqual(2);
+    expect(mtl("abcde fghij klmn")).toEqual(2);
+  });
+  it("handles multiple long words", () => {
+    expect(mtl("a reallongword")).toEqual(3);
+  });
+  // With the mock measureText(), it's not possible to test strings with extra whitespace
 });
