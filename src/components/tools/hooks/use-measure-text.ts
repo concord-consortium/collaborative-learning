d723e36968c373c553937e368e5ae4bec119202d
Teale Fristoe <fristoe@gmail.com>
2022-09-02 12:23:15 -0700

    Add caching for measureTextLines()

--- src/components/tools/hooks/use-measure-text.ts
+++ src/components/tools/hooks/use-measure-text.ts
@@ -18,58 +18,69 @@ export const useMeasureText = (font = defaultFont) => {
   }, [font]);
 };
 
+// Removes leading and trailing whitespace and replaces all other white space with a single space
 const trimWhitespace = (text:string) => {
   return text.trim().replace(/\s\s+/g, ' ');
 };
 
+// Caches values for measuring lines. First key is font, second is text, third is width.
+const lineCache: Record<string, Record<string, Record<number, number>>> = {};
+
 // Approximates the number of lines a given string will take when rendered in a div of the given width
 export const measureTextLines = (text: string, width: number, font = defaultFont, maxLines = 100) => {
-  const trimmedText = trimWhitespace(text);
-  const context = canvas.getContext("2d");
-  if (!context) { return 1; }
-  context && font && (context.font = font);
-  let lines = 1;
-  let startOfLine = 0;
-  let startOfWord = 0;
-  let currentIndex = 0;
-  while (currentIndex < trimmedText.length) {
-    // Lines should never start with a space
-    if (trimmedText[startOfLine] === " ") {
-      startOfLine++;
-      currentIndex++;
-      startOfWord++;
-    }
-
-    if (context.measureText(trimmedText.slice(startOfLine, currentIndex)).width < width) {
-      // We haven't hit a line break yet
-      if (trimmedText[currentIndex] === " ") {
-        startOfWord = currentIndex + 1;
+  lineCache[font] = lineCache[font] || {};
+  lineCache[font][text] = lineCache[font][text] || {};
+  
+  if (!(width in lineCache[font][text])) {
+    const trimmedText = trimWhitespace(text);
+    let lines = 1;
+    let startOfLine = 0;
+    let startOfWord = 0;
+    let currentIndex = 0;
+    while (currentIndex < trimmedText.length) {
+      // Lines should never start with a space
+      if (trimmedText[startOfLine] === " ") {
+        startOfLine++;
+        currentIndex++;
+        startOfWord++;
       }
-      currentIndex++;
-    } else {
-      // We hit the end of the line
-      lines++;
-      if (lines >= maxLines) return maxLines;
 
-      if (startOfWord === startOfLine) {
-        // The line is one big word so we have to break in the middle of it
-        startOfLine = startOfWord = currentIndex;
-        // This commented code keeps oversized words on a single line
-        // while (currentIndex < text.length) {
-        //   // We found a new word, so start a new line with it
-        //   if (text[currentIndex] === " ") {
-        //     startOfLine = startOfWord = ++currentIndex;
-        //     break;
-        //   // We haven't found the end of the long word, keep looking
-        //   } else {
-        //     currentIndex++;
-        //   }
-        // }
+      if (measureText(trimmedText.slice(startOfLine, currentIndex), font) < width) {
+        // We haven't hit a line break yet
+        if (trimmedText[currentIndex] === " ") {
+          startOfWord = currentIndex + 1;
+        }
+        currentIndex++;
       } else {
-        // We've encountered other words on this line, so start the next line at the beginning of the last word
-        startOfLine = startOfWord;
+        // We hit the end of the line
+        lines++;
+        if (lines >= maxLines) {
+          lineCache[font][text][width] = maxLines;
+          return maxLines;
+        }
+
+        if (startOfWord === startOfLine) {
+          // The line is one big word so we have to break in the middle of it
+          startOfLine = startOfWord = currentIndex;
+          // This commented code keeps oversized words on a single line
+          // while (currentIndex < text.length) {
+          //   // We found a new word, so start a new line with it
+          //   if (text[currentIndex] === " ") {
+          //     startOfLine = startOfWord = ++currentIndex;
+          //     break;
+          //   // We haven't found the end of the long word, keep looking
+          //   } else {
+          //     currentIndex++;
+          //   }
+          // }
+        } else {
+          // We've encountered other words on this line, so start the next line at the beginning of the last word
+          startOfLine = startOfWord;
+        }
       }
     }
+    lineCache[font][text][width] = lines;
   }
-  return lines;
+
+  return lineCache[font][text][width];
 };
