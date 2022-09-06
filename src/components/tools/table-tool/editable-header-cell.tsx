1cee99007dc7b6e3970571ac95df29e2c712a4f3
Teale Fristoe <fristoe@gmail.com>
2022-09-06 15:25:07 -0700

    Fix cell input size

--- src/components/tools/table-tool/editable-header-cell.tsx
+++ src/components/tools/table-tool/editable-header-cell.tsx
@@ -56,11 +56,11 @@ export const EditableHeaderCell: React.FC<IProps> = ({ column: _column, height }
   // ¯\_(ツ)_/¯ The fix is to force the <input> to be the height of the row with an inline style.
   const inputStyle: React.CSSProperties = { ...ehcStyle };
   return (
-    <div className={"editable-header-cell"} onClick={handleClick} style={ehcStyle}>
+    <div className="editable-header-cell" onClick={handleClick} style={ehcStyle}>
       {isEditing
         ? <HeaderCellInput style={style} inputStyle={inputStyle} value={nameValue}
             onKeyDown={handleKeyDown} onChange={handleChange} onClose={handleClose} />
-        : name}
+        : <div className="header-name">{name}</div>}
     </div>
   );
 };
