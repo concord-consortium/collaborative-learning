dcffd627d74a88707a5013217b19cbb53f356439
Tejal Shah <tejal.shah@tscube.com>
2022-09-28 00:39:57 -0400

    Fixing cypress code coverage that was broken when upgrading to cypress 10.7

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,22 +1,2 @@
-const fs = require("fs-extra");
-const path = require("path");
-
-const fetchConfigurationByFile = file => {
-  const pathOfConfigurationFile = `config/cypress.${file}.json`;
-
-  return (
-    file && fs.readJson(path.join(__dirname, "../", pathOfConfigurationFile))
-  );
-};
-
 module.exports = (on, config) => {
-    require('cypress-terminal-report/src/installLogsPrinter')(on);
-
-    const environment = config.env.testEnv || "dev";
-    const configurationForEnvironment = fetchConfigurationByFile(environment);
-
-    return configurationForEnvironment || config
-        .then(envConfig => {
-            return require('@cypress/code-coverage/task')(on, { ...config, ...envConfig });
-        });
 }
