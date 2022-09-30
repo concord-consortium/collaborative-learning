65362770bc6cef17c9281a19fe558c066730a0ac
picklesrus <picklesrus@users.noreply.github.com>
2022-09-30 16:41:18 -0400

    fix indentation, removed unused stuff.

--- src/components/chat/chat-thread-toggle.test.tsx
+++ src/components/chat/chat-thread-toggle.test.tsx
@@ -12,7 +12,6 @@ describe("Comment Textbox", () => {
   });
   
   it("Chat toggle collapsed", () => {
-    const activeNavTab = "problems";
     render((
       <ChatThreadToggle  isThreadExpanded={false} />
     ));
