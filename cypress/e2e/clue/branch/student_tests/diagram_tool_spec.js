6bfca4e1b5dd636003c4da78814b60020f02a54b
Teale Fristoe <fristoe@gmail.com>
2022-10-27 08:19:55 -0700

    Basic Diagram Dialog (#1449)
    * Make dialog button show dialog
    
    * Connect dialog with QP component and selected variable

--- cypress/e2e/clue/branch/student_tests/diagram_tool_spec.js
+++ cypress/e2e/clue/branch/student_tests/diagram_tool_spec.js
@@ -21,5 +21,13 @@ context('Diagram Tool Tile', function () {
       diagramToolTile.getDiagramToolbarButton("button-dialog").should("exist");
       diagramToolTile.getDiagramToolbarButton("button-delete").should("exist");
     });
+    it("renders dialogs", () => {
+      diagramToolTile.getDiagramToolbarButton("button-dialog").should("be.disabled");
+      // TODO Add these tests back in after we have a convenient way of adding cards to the tile
+      // diagramToolTile.getDiagramToolbarButton("button-dialog").click();
+      // diagramToolTile.getDiagramDialog().should("exist");
+      // diagramToolTile.getDiagramDialogCloseButton().click();
+      // diagramToolTile.getDiagramDialog().should("not.exist");
+    });
   });
 });
--- src/plugins/diagram-viewer/diagram-tool.tsx
+++ src/plugins/diagram-viewer/diagram-tool.tsx
@@ -1,18 +1,24 @@
 import React from "react";
+import { observer } from "mobx-react";
 import { IToolTileProps } from "../../components/tools/tool-tile";
 import { DiagramToolbar } from "./diagram-toolbar";
 import { DiagramContentModelType } from "./diagram-content";
 import { kQPVersion } from "./diagram-types";
+import { useEditVariableDialog } from "./use-edit-variable-dialog";
 import { useToolbarToolApi } from "../../components/tools/hooks/use-toolbar-tool-api";
 import { Diagram } from "@concord-consortium/diagram-view";
 import "@concord-consortium/diagram-view/dist/index.css";
 
 import "./diagram-tool.scss";
 
-export const DiagramToolComponent: React.FC<IToolTileProps> = (
+export const DiagramToolComponent: React.FC<IToolTileProps> = observer((
   { documentContent, model, onRegisterToolApi, onUnregisterToolApi, readOnly, scale, toolTile }
 ) => {
   const content = model.content as DiagramContentModelType;
+
+  const [showEditVariableDialog] = useEditVariableDialog({
+    variable: content.root.selectedNode?.variable
+  });
 
   const toolbarProps = useToolbarToolApi({ id: model.id, enabled: !readOnly, onRegisterToolApi, onUnregisterToolApi });
   
@@ -20,13 +26,15 @@ export const DiagramToolComponent: React.FC<IToolTileProps> = (
     <div className="diagram-tool">
       <DiagramToolbar
         documentContent={documentContent}
+        handleDialogClick={() => showEditVariableDialog()}
         toolTile={toolTile}
         scale={scale}
+        selectedVariable={content.root.selectedNode?.variable}
         { ...toolbarProps }
       />
       <Diagram dqRoot={content.root} />
       <div className="qp-version">{`version: ${kQPVersion}`}</div>
     </div>
   );
-};
+});
 DiagramToolComponent.displayName = "DiagramToolComponent";
