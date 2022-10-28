0a3f5a110c1b1b44a0e0480e805dbc1d94354032
dennisrcao <dcao@concord.org>
2022-10-27 19:11:50 -0700

    -used promise.all to speed up queries. -stuck at figuring out how to retreive titles for publication, learninglogpublication, personalPublication, supportPublication

--- src/components/chat/commented-documents.tsx
+++ src/components/chat/commented-documents.tsx
@@ -5,37 +5,27 @@ import { CurriculumDocument, DocumentDocument } from "../../lib/firestore-schema
 import { getSectionTitle } from "../../models/curriculum/section";
 import { UserModelType } from "../../models/stores/user";
 import "./commented-documents.scss";
-import { string } from "@concord-consortium/mobx-state-tree/dist/internal";
-
 interface IProps {
   documentObj: CurriculumDocument,
   user?: UserModelType
   handleDocView: (() => void) | undefined;
 }
-
-// Not sure this is the best way to do this.  The issue, I think
-// Is that the promises Firebase returns are not visible to TypeScript compiler
-// which can't see that the docs will eventually have the fields that match a CurriculumDocument
 interface PromisedCurriculumDocument extends CurriculumDocument {
   id?: string,
   title?: string,
   numComments?: number
 }
-
 interface PromisedDocumentDocument extends DocumentDocument {
   id?: string,
   numComments?: number,
   title: string
 }
 
-
 export const CommentedDocuments: React.FC<IProps> = ({documentObj, user, handleDocView}) => {
   console.log("----- < CommentedDocuments > -----------");
-  console.log("documentObj:", documentObj);
-  console.log("user:", user);
-  console.log("handleDocView:", handleDocView);
-
-
+  // console.log("documentObj:", documentObj);
+  // console.log("user:", user);
+  // console.log("handleDocView:", handleDocView);
   const [docsCommentedOn, setDocsCommentedOn] = useState<PromisedCurriculumDocument[]>();
   const [db] = useFirestore();
   const cDocsRef = db.collection("curriculum");
@@ -49,20 +39,18 @@ export const CommentedDocuments: React.FC<IProps> = ({documentObj, user, handleD
   //MyWork/ClassWork
   const [myWorkDocuments, setMyWorkDocuments] = useState<PromisedDocumentDocument[]>();
   const mDocsRef = db.collection("documents");
-  const mDocsInScopeRef = mDocsRef.where("network", "==", user?.network);
+  const mDocsInScopeRef = mDocsRef
+    .where("network", "==", user?.network); //do we want to filter by demo class 1?
+    // .where("context_id", "==", "democlass1");
   const ui = useUIStore();
   const store = useStores();
 
-  //Curriculum Documents
+  //------Curriculum Documents--------
   useEffect(() => {
-    console.log("---in useEffect 1-----");
-    const t0 = performance.now();
-    const unsubscribeFromDocs = cDocsInScopeRef.onSnapshot(async querySnapshot => {
-      // console.log("querySnapshot1: ", querySnapshot);
-      // console.log("querySnapshot1 docs", querySnapshot.docs);
-      // console.log("size of querySnapshots.docs:", querySnapshot.docs.length);
+    // console.log("---in useEffect 1-----");
+    // const t0 = performance.now();
+    const unsubscribeFromDocs = cDocsInScopeRef.onSnapshot(querySnapshot => {
       const docs = querySnapshot.docs.map(doc => {
-        console.log("doc line 60:", doc);
         return (
           {
             id: doc.id,
@@ -73,77 +61,92 @@ export const CommentedDocuments: React.FC<IProps> = ({documentObj, user, handleD
         );
       });
       const commentedDocs: PromisedCurriculumDocument[] = [];
+      const promiseArr: Promise<void>[] = [];
       for (let doc of docs){
-        // console.log("doc line 73:", doc);
         const docCommentsRef = cDocsRef.doc(doc.id).collection("comments");
-        // console.log("docCommentsRef:", docCommentsRef);
-        await docCommentsRef.get().then((qs) => {
-          // console.log("qs1:", qs);
+        promiseArr.push(docCommentsRef.get().then((qs) => {
           if (qs.empty === false) {
             const firstCharPosition = doc.id.split("_", 4).join("_").length + 1; //first char after 4th _
             const sectionType =  doc.id.substring(firstCharPosition, doc.id.length);
             doc = {...doc, title: getSectionTitle(sectionType), numComments: qs.size};
             commentedDocs.push(doc as PromisedCurriculumDocument);
           }
-        });
+        }));
       }
-      setDocsCommentedOn(commentedDocs);
-      // console.log("size of commentedDocs", commentedDocs.length);
-      const t1 = performance.now();
-      console.log(`Call to useEffect1 took ${t1 - t0} milliseconds.`);
-      console.log("-------end useEffect 1----------");
-
+      Promise.all(promiseArr).then((results)=>{
+        setDocsCommentedOn(commentedDocs);
+      }).then(()=>{
+        // const t1 = performance.now();
+        // console.log(`Call to useEffect1 took ${t1 - t0} milliseconds.`);
+      });
+      // console.log("-------end useEffect 1----------");
     });
     return () => unsubscribeFromDocs?.();
-
   },[]);
 
-  //MyWork/ClassWork
+  // ------MyWork/ClassWork--------
   useEffect(() => {
-    console.log("-----in useEffect 2----------");
+    //-------promise.all -----------
     const t0 = performance.now();
-    console.log("t0 useEffect 2:", t0);
-    const unsubscribeFromDocs = mDocsInScopeRef.onSnapshot(async querySnapshot=>{
+    const unsubscribeFromDocs = mDocsInScopeRef.onSnapshot(querySnapshot=>{
       const docs = querySnapshot.docs.map(doc =>{ //convert each element of docs to an object
+        console.log("line 103 doc:", doc.data());
         return (
           {
             id: doc.id,
             type: doc.data().type,
             numComments: 0,
             title: "temp",
-            ...doc.data()//this isn't properly getting added to the object
+            ...doc.data()
           }
         );
       });
       const commentedDocs: PromisedDocumentDocument[]= [];
-
+      const promiseArr: Promise<void>[]=[];
       for (let doc of docs){
         let title: string;
+        console.log("line 118 doc.type:", doc.type);
         switch (doc.type){
           case "problem":
             title = store.problem.title;
             break;
           case "planning":
             title = `${store.problem.title}: Planning`;
-            // console.log("line 119", store.problem.title, store.problemPath, store.problem.subtitle);
             break;
           case "learningLog":
           case "personal":
             title = doc.title;
             break;
+          case "publication":
+            console.log("----case publication------");
+            console.log("publication doc", doc);
+            console.log(store.documents.getDocument);
+            console.log(store.documents.getNextLearningLogTitle);
+            console.log(store.documents.getNextOtherDocumentTitle);
+            console.log(store.documents.getNextPersonalDocumentTitle);
+            console.log("----end case publication------");
+
+            break;
+          // types I have not accounted for :
+          //publication, learningLogPublication, personalPublication, supportPublication
         }
         const docCommentsRef = mDocsRef.doc(doc.id).collection("comments");
-        await docCommentsRef.get().then((qs)=>{
+        promiseArr.push(docCommentsRef.get().then((qs)=>{
           if (qs.empty === false){
             doc = {...doc, title, numComments: qs.size};
             commentedDocs.push(doc as PromisedDocumentDocument);
           }
-        });
+        }));
       }
-      setMyWorkDocuments(commentedDocs);
-      const t1 = performance.now();
-      console.log("useEffect 2 t1:", t1);
-      console.log(`Call to useEffect2 took ${t1 - t0} milliseconds.`);
+
+      Promise.all(promiseArr).then(()=>{
+        setMyWorkDocuments(commentedDocs);
+      }).then(()=>{
+        const t1 = performance.now();
+        console.log("useEffect 2: commentedDocs", commentedDocs);
+        console.log("useEffect 2 t1:", t1);
+        console.log(`Call to useEffect2 took ${t1 - t0} milliseconds.`);
+      });
       console.log("-------end useEffect 2---------");
     });
     return () => unsubscribeFromDocs?.();
@@ -159,6 +162,7 @@ export const CommentedDocuments: React.FC<IProps> = ({documentObj, user, handleD
         docsCommentedOn &&
         (docsCommentedOn).map((doc: PromisedCurriculumDocument, index:number) => {
           let navTab: string;
+          console.log("doc.id line 163", doc.id);
           if (doc.id?.includes("guide")){
             navTab = "teacher-guide";
           }
@@ -191,12 +195,18 @@ export const CommentedDocuments: React.FC<IProps> = ({documentObj, user, handleD
       {
         myWorkDocuments &&
         (myWorkDocuments).map((doc: PromisedDocumentDocument, index: number) =>{
+          console.log("doc.id line 196:", doc.id);
+          console.log("doc.title line 197:", doc.title);
+          console.log("doc:", doc);
           return (
             <div
               className={"document-box"}
               key={index}
               onClick={()=>{
                 console.log("clicked a mywork/classwork doc");
+                console.log("line 200", doc.title);
+                //is it true that mywork tab only holds Problem, Planning, Personal (you make), Learning?
+                //class work holds .... publication, learningLogPublication, personalPublication, supportPublication?
               }}
             >
               <div className={"title"}>
