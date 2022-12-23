163cb426b2e140fd39b8d8cc57c953f7e858e159
picklesrus <picklesrus@users.noreply.github.com>
2022-12-23 09:36:15 -0500

    Add back dataflow cypress test. I had just disabled it to make deployment more consistent.

--- cypress/e2e/clue/branch/student_tests/dataflow_tool_spec.js
+++ cypress/e2e/clue/branch/student_tests/dataflow_tool_spec.js
@@ -3,125 +3,129 @@ import DataflowToolTile from '../../../../support/elements/clue/DataflowToolTile
 
 let clueCanvas = new ClueCanvas,
   dataflowToolTile = new DataflowToolTile;
-  describe('adding points and polygons to a graph', function(){
-    it('dummy test', function () {
-        expect(true).to.equal(true);
+  context('Dataflow Tool Tile', function () {	
+    before(function () {
+      const queryParams = "?appMode=qa&fakeClass=5&fakeUser=student:5&qaGroup=5&unit=dfe";	
+      cy.clearQAData('all');	
+  
+      cy.visit(queryParams);	
+      cy.waitForLoad();	
+      cy.closeResourceTabs();	
     });
-});
-//   describe("Dataflow Tool", () => {
-//     it("renders dataflow tool tile", () => {
-//       clueCanvas.addTile("dataflow");
-//       dataflowToolTile.getDrawTile().should("exist");
-//       dataflowToolTile.getTileTitle().should("exist");
-//     });
-//     describe("Number Node", () => {
-//       const nodeType = "number";
-//       it("can create number node", () => {
-//         dataflowToolTile.getCreateNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("exist");
-//       });
-//       it("can toggle minigraph", () => {
-//         dataflowToolTile.getShowGraphButton(nodeType).click();
-//         dataflowToolTile.getMinigraph(nodeType).should("exist");
-//         dataflowToolTile.getShowGraphButton(nodeType).click();
-//         dataflowToolTile.getMinigraph(nodeType).should("not.exist");
-//       });
-//       it("can change the number", () => {
-//         dataflowToolTile.getNumberField().type("3{enter}");
-//         dataflowToolTile.getNumberField().should("have.value", "3");
-//       });
-//       it("can delete number node", () => {
-//         dataflowToolTile.getDeleteNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("not.exist");
-//       });
-//     });
-//     describe("Generator Node", () => {
-//       const nodeType = "generator";
-//       it("can create generator node", () => {
-//         dataflowToolTile.getCreateNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("exist");
-//       });
-//       it("can delete generator node", () => {
-//         dataflowToolTile.getDeleteNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("not.exist");
-//       });
-//     });
-//     describe("Timer Node", () => {
-//       const nodeType = "timer";
-//       it("can create timer node", () => {
-//         dataflowToolTile.getCreateNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("exist");
-//       });
-//       it("can delete timer node", () => {
-//         dataflowToolTile.getDeleteNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("not.exist");
-//       });
-//     });
-//     describe("Math Node", () => {
-//       const nodeType = "math";
-//       it("can create math node", () => {
-//         dataflowToolTile.getCreateNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("exist");
-//       });
-//       it("can delete math node", () => {
-//         dataflowToolTile.getDeleteNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("not.exist");
-//       });
-//     });
-//     describe("Logic Node", () => {
-//       const nodeType = "logic";
-//       it("can create logic node", () => {
-//         dataflowToolTile.getCreateNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("exist");
-//       });
-//       it("can delete logic node", () => {
-//         dataflowToolTile.getDeleteNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("not.exist");
-//       });
-//     });
-//     describe("Transform Node", () => {
-//       const nodeType = "transform";
-//       it("can create transform node", () => {
-//         dataflowToolTile.getCreateNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("exist");
-//       });
-//       it("can delete transform node", () => {
-//         dataflowToolTile.getDeleteNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("not.exist");
-//       });
-//     });
-//     describe("Demo Output Node", () => {
-//       const nodeType = "demo-output";
-//       it("can create demo output node", () => {
-//         dataflowToolTile.getCreateNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("exist");
-//       });
-//       it("can change output type", () => {
-//         const dropdown = "outputType";
-//         dataflowToolTile.getDropdown(nodeType, dropdown).click();
-//         dataflowToolTile.getDropdownOptions(nodeType, dropdown).should("have.length", 3);
-//         dataflowToolTile.getDropdownOptions(nodeType, dropdown).last().click();
-//         dataflowToolTile.getDropdownOptions(nodeType, dropdown).should("have.length", 0);
-//         dataflowToolTile.getDropdown(nodeType, dropdown).contains("Grabber").should("exist");
-//       });
-//       it("can delete demo output node", () => {
-//         dataflowToolTile.getDeleteNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("not.exist");
-//       });
-//     });
-//     describe("Live Output Node", () => {
-//       const nodeType = "live-output";
-//       it("can create live output node", () => {
-//         dataflowToolTile.getCreateNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("exist");
-//       });
-//     });
-//     describe("Control Node", () => {
-//       const nodeType = "control";
-//       it("can create control node", () => {
-//         dataflowToolTile.getCreateNodeButton(nodeType).click();
-//         dataflowToolTile.getNode(nodeType).should("exist");
-//       });
-//     });
-//   });
-// });
+    describe("Dataflow Tool", () => {
+      it("renders dataflow tool tile", () => {
+        clueCanvas.addTile("dataflow");
+        dataflowToolTile.getDrawTile().should("exist");
+        dataflowToolTile.getTileTitle().should("exist");
+      });
+      describe("Number Node", () => {
+        const nodeType = "number";
+        it("can create number node", () => {
+          dataflowToolTile.getCreateNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("exist");
+        });
+        it("can toggle minigraph", () => {
+          dataflowToolTile.getShowGraphButton(nodeType).click();
+          dataflowToolTile.getMinigraph(nodeType).should("exist");
+          dataflowToolTile.getShowGraphButton(nodeType).click();
+          dataflowToolTile.getMinigraph(nodeType).should("not.exist");
+        });
+        it("can change the number", () => {
+          dataflowToolTile.getNumberField().type("3{enter}");
+          dataflowToolTile.getNumberField().should("have.value", "3");
+        });
+        it("can delete number node", () => {
+          dataflowToolTile.getDeleteNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("not.exist");
+        });
+      });
+      describe("Generator Node", () => {
+        const nodeType = "generator";
+        it("can create generator node", () => {
+          dataflowToolTile.getCreateNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("exist");
+        });
+        it("can delete generator node", () => {
+          dataflowToolTile.getDeleteNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("not.exist");
+        });
+      });
+      describe("Timer Node", () => {
+        const nodeType = "timer";
+        it("can create timer node", () => {
+          dataflowToolTile.getCreateNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("exist");
+        });
+        it("can delete timer node", () => {
+          dataflowToolTile.getDeleteNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("not.exist");
+        });
+      });
+      describe("Math Node", () => {
+        const nodeType = "math";
+        it("can create math node", () => {
+          dataflowToolTile.getCreateNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("exist");
+        });
+        it("can delete math node", () => {
+          dataflowToolTile.getDeleteNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("not.exist");
+        });
+      });
+      describe("Logic Node", () => {
+        const nodeType = "logic";
+        it("can create logic node", () => {
+          dataflowToolTile.getCreateNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("exist");
+        });
+        it("can delete logic node", () => {
+          dataflowToolTile.getDeleteNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("not.exist");
+        });
+      });
+      describe("Transform Node", () => {
+        const nodeType = "transform";
+        it("can create transform node", () => {
+          dataflowToolTile.getCreateNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("exist");
+        });
+        it("can delete transform node", () => {
+          dataflowToolTile.getDeleteNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("not.exist");
+        });
+      });
+      describe("Demo Output Node", () => {
+        const nodeType = "demo-output";
+        it("can create demo output node", () => {
+          dataflowToolTile.getCreateNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("exist");
+        });
+        it("can change output type", () => {
+          const dropdown = "outputType";
+          dataflowToolTile.getDropdown(nodeType, dropdown).click();
+          dataflowToolTile.getDropdownOptions(nodeType, dropdown).should("have.length", 3);
+          dataflowToolTile.getDropdownOptions(nodeType, dropdown).last().click();
+          dataflowToolTile.getDropdownOptions(nodeType, dropdown).should("have.length", 0);
+          dataflowToolTile.getDropdown(nodeType, dropdown).contains("Grabber").should("exist");
+        });
+        it("can delete demo output node", () => {
+          dataflowToolTile.getDeleteNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("not.exist");
+        });
+      });
+      describe("Live Output Node", () => {
+        const nodeType = "live-output";
+        it("can create live output node", () => {
+          dataflowToolTile.getCreateNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("exist");
+        });
+      });
+      describe("Control Node", () => {
+        const nodeType = "control";
+        it("can create control node", () => {
+          dataflowToolTile.getCreateNodeButton(nodeType).click();
+          dataflowToolTile.getNode(nodeType).should("exist");
+        });
+      });
+    });
+  });
