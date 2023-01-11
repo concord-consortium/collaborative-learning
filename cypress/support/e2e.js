208530bb264a6e1264ea4e72192bc13df33efab2
Scott Cytacki <scytacki@concord.org>
2023-01-11 16:15:58 -0500

    hide the cypress fetch requests

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -40,6 +40,25 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     return false;
 });
 
+// Hide fetch/XHR requests from command log
+// if (Cypress.config('hideXHRInCommandLog')) {
+// eslint-disable-next-line no-constant-condition
+if (true) {
+  const app = window.top;
+
+  if (
+    app &&
+    !app.document.head.querySelector('[data-hide-command-log-request]')
+  ) {
+    const style = app.document.createElement('style');
+    style.innerHTML =
+      '.command-name-request, .command-name-xhr { display: none }';
+    style.setAttribute('data-hide-command-log-request', '');
+
+    app.document.head.appendChild(style);
+  }
+}
+
 after(function(){
   cy.clearQAData('all');
 });
