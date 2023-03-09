1afc634a48b0adcc8170ac91b4264a69c7144cbb
Ethan McElroy <emcelroy@concord.org>
2023-03-08 22:18:57 -0500

    refactor: Move unit problem sections to external files (PT-184605688)
    PT Story: https://www.pivotaltracker.com/story/show/184605688

---
+++ scripts/update-curriculum-to-use-external-problem-sections.ts
@@ -0,0 +1,71 @@
+#!/usr/bin/node
+
+// to run this script type the following in the terminal
+// cf. https://stackoverflow.com/a/66626333/16328462
+// $ cd scripts
+// $ node --loader ts-node/esm update-curriculum-to-use-external-problem-sections.ts
+
+import fs from "fs";
+import stringify from "json-stringify-pretty-compact";
+
+/* 
+ * Returns a date string in yyyy-mm-dd format.
+ */
+const getTimestamp = () => {
+  const todaysDate = new Date();
+  const offset = todaysDate.getTimezoneOffset();
+  const timestamp = new Date(todaysDate.getTime() - (offset*60*1000));
+  return timestamp.toISOString().split('T')[0];
+};
+
+/* 
+ * Iterates over unit content and updates any inline problem sections by
+ * copying their data to a new external file and replacing the inline
+ * data with a reference the external file.
+ */
+const processProblemSections = (content: any, subdir: string) => {
+  const curriculumCode = content.code;
+  let sectionFileCount = 0;
+  for (const investigation of content.investigations) {
+    const investigationOrdinal = investigation.ordinal;
+    for (const problem of investigation.problems) {
+      const problemOrdinal = problem.ordinal;
+      for (let i = 0; i < problem.sections.length; i++) {
+        const section = problem.sections[i];
+        const sectionFileName =
+          `${curriculumCode}-investigation-${investigationOrdinal}-problem-${problemOrdinal}-section-${i+1}.json`;
+        const sectionFilePath = `${subdir}/${sectionFileName}`;
+        if (typeof section !== "string") { // don't update if it's already a reference to an external file
+          const sectionData = section;
+          fs.writeFileSync(sectionFilePath, stringify(sectionData, {maxLength: 300}));
+          sectionFileCount++;
+          problem.sections[i] = `${sectionFileName}`;
+        }
+      }
+    }
+  }
+  console.log(`${sectionFileCount} section files added to ${subdir}`);
+  return content;
+};
+
+const curriculumDir = "../src/public/curriculum";
+const curriculumSubdirs = fs.readdirSync(curriculumDir).filter(name => !name.endsWith(".json"));
+
+for (const subdirName of curriculumSubdirs) {
+  const subdir = `${curriculumDir}/${subdirName}`;
+  const files = fs.readdirSync(subdir).filter(name => name.toLowerCase().endsWith(".json"));
+
+  for (const filename of files) {
+    if (subdirName !== "stretching-and-shrinking") {
+      const fileContent = fs.readFileSync(`${subdir}/${filename}`, "utf8");
+      const fileData = JSON.parse(fileContent);
+      // copy content to back-up file
+      // const backupFilename = `${filename.split(".")[0]}--back-up-${getTimestamp()}.json`;
+      // const backupFilePath = `${subdir}/${backupFilename}`;
+      // fs.writeFileSync(backupFilePath, fileContent);
+      // write data to original file
+      const updatedFileData = processProblemSections(fileData, subdir);
+      fs.writeFileSync(`${subdir}/${filename}`, stringify(updatedFileData, {maxLength: 300}));
+    }
+  }
+}
