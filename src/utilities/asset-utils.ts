5fa88e36b65f13f5bff26d41ec2c5351d3843220
Ethan McElroy <emcelroy@concord.org>
2023-03-22 15:47:01 -0400

    Re-uninstategetAssetUrl

--- src/utilities/asset-utils.ts
+++
@@ -1,9 +0,0 @@
-declare const __webpack_public_path__: string;
-
-export function getAssetUrl(url: string) {
-  if (typeof __webpack_public_path__ === "undefined") {
-    return url;
-  }
-  const urlObj = new URL(url, __webpack_public_path__);
-  return urlObj.href;  
-}
