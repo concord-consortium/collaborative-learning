c181e1094c70f3073ac38479b7afd3ced0c90902
dennisrcao <dcao@concord.org>
2023-03-24 14:29:30 -0700

    break up topbar into seperate components

--- src/plugins/dataflow/components/ui/dataflow-program-topbar.tsx
+++ src/plugins/dataflow/components/ui/dataflow-program-topbar.tsx
@@ -1,19 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
-import Slider from "rc-slider";
-import classNames from "classnames";
+import React from "react";
+import { RateSelectorOrPlayBack } from "./dataflow-rateselector-playback";
 import { ProgramDataRate } from "../../model/utilities/node";
-import { IconButton } from "../../../../components/utilities/icon-button";
 import { SerialDevice } from "../../../../models/stores/serial";
 import RecordIcon from "../../assets/topbar-buttons/record-icon.svg";
 import StopIcon from "../../assets/topbar-buttons/stop-icon.svg";
 import PlayIcon from "../../assets/topbar-buttons/play-icon.svg";
 import ClearIcon from "../../assets/topbar-buttons/clear-icon.svg";
-import { DataflowSerialConnectButton } from "./dataflow-serial-connect";
-
+import { DataflowSerialConnectButton } from "./dataflow-serial-connect-button";
 
 import "./dataflow-program-topbar.scss";
-
-const totalSamples = 10000;
 
 interface TopbarProps {
   programDataRates: ProgramDataRate[];
@@ -72,126 +67,6 @@ export const DataflowProgramTopbar = (props: TopbarProps) => {
   );
 };
 
-/* ==[ Sampling Rate Selector ] == */
-interface IRateSelectorProps {
-  rateOptions: ProgramDataRate[];
-  dataRate: number;
-  onRateSelectClick: (rate: number) => void;
-  readOnly: boolean;
-  programRecordState: number;
-  numNodes: number;
-  onRecordDataChange: () => void;
-}
-
-function formatTime(seconds: number) {
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds % 60;
-  const formattedMinutes = minutes.toString().padStart(3, '0');
-  const formattedSeconds = remainingSeconds.toString().padStart(2, '0');
-  return `${formattedMinutes}:${formattedSeconds}`;
-}
-
-const RateSelectorOrPlayBack = (props: IRateSelectorProps) => {
-  const { onRateSelectClick, readOnly, dataRate, rateOptions, programRecordState,
-          numNodes, onRecordDataChange} = props;
-
-  /* ==[ Total Recording Time  - Calculate] format as "MMM:SS" */
-  const totalTimeSec = Math.floor((dataRate / 1000) * (totalSamples/numNodes));
-  const totalTimeFormatted = formatTime(totalTimeSec);
-
-  /* ==[ Timer Recording Time  - Calculate] format as "MMM:SS" */
-  const timerMin = useRef(0);
-  const sliderSec = useRef(0); //this does not wrap around 60 and is used for the slider/total seconds
-  const timerSec = useRef(0); //seconds that have passed after hitting Record
-  const formattedMin = timerMin.current.toString().padStart(3, "0");
-  const formattedSec = timerSec.current.toString().padStart(2, "0");
-  const formattedTime = `${formattedMin}:${formattedSec}`;
-
-  /* ==[ Timer - Enable ] == */
-  const startTimer = numNodes > 0 && sliderSec.current < totalTimeSec && (programRecordState === 1);
-
-  useEffect(()=>{
-    if (startTimer){
-      const timer = setInterval(() => {
-        startTimer && timerSec.current++;
-        startTimer && sliderSec.current++;
-        console.log("timerSec:", timerSec.current);
-        console.log("sliderSec:", sliderSec.current);
-        if (timerSec.current === 60){
-          timerMin.current++;
-          timerSec.current = 0;
-        }
-      }, 1000);
-      return () => clearInterval(timer);
-    }
-  }, [timerSec, startTimer, sliderSec]);
-
-  /* ==[ Timer - Reset ] == */
-  if (programRecordState === 0) {
-    timerSec.current = 0;
-    timerMin.current = 0;
-    sliderSec.current = 0;
-  }
-
-  if(sliderSec.current === totalTimeSec && programRecordState === 1){
-    onRecordDataChange();
-  }
-
-  const railRef = useRef<HTMLDivElement>(null);
-
-  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    onRateSelectClick(Number(event.target.value));
-  };
-
-  return (
-    <>
-      <div className="topbar-sampleratetext-or-timeslider">
-        {
-          programRecordState ?
-          <div className="slider-container">
-
-            <Slider
-              min={0}
-              max={totalTimeSec}
-              step={1}
-              value={sliderSec.current}
-              ref={railRef}
-            />
-          </div>
-          :
-          <label className="samplerate-label" htmlFor="rate-select">Sampling Rate</label>
-        }
-      </div>
-
-      <div className="topbar-datarate-or-timer">
-        <div className="datarate-options">
-          {
-            !programRecordState ?
-            <select onChange={handleSelectChange}
-              disabled={readOnly}
-              value={dataRate.toString()}
-              id="rate-select" // TODO: The id needs to be unique to the particular DF tile
-            >
-              {
-                rateOptions.map((rate: ProgramDataRate) => (
-                <option key={rate.text} value={rate.val} disabled={rate.disabled}>
-                  {rate.text}
-                </option>
-                ))
-              }
-            </select> :
-            numNodes > 0 &&
-            <div className="countdown-timer">
-             {`${formattedTime} / ${totalTimeFormatted}`}
-            </div>
-          }
-
-        </div>
-      </div>
-    </>
-  );
-};
-
 
 /* ==[ Record Data Button ] == */
 
@@ -212,9 +87,7 @@ interface IRecordStopOrClearProps {
 }
 
 const RecordStopOrClearButton = (props: IRecordStopOrClearProps) => {
-  //if mode is in clear, click handler should clear data
   const { buttonClickHandler, programRecordState } = props;
-
   return (
     <div className="record-btn-container">
       <button
@@ -228,14 +101,12 @@ const RecordStopOrClearButton = (props: IRecordStopOrClearProps) => {
           {Mode[programRecordState]}
         </div>
       </button>
-
     </div>
   );
 };
 
 
 /* ==[ Playback Button ] == */
-
 interface IPlaybackProps {
   programRecordState: number;
 }
