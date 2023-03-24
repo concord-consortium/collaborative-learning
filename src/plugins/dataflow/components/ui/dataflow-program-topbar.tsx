f44cb325bd3291eaaf5c96326c94c57d88a9db08
Joe Bacal <bacalj@gmail.com>
2023-03-24 14:51:57 -0400

    moves serial button to its own component

--- src/plugins/dataflow/components/ui/dataflow-program-topbar.tsx
+++ src/plugins/dataflow/components/ui/dataflow-program-topbar.tsx
@@ -8,6 +8,8 @@ import RecordIcon from "../../assets/topbar-buttons/record-icon.svg";
 import StopIcon from "../../assets/topbar-buttons/stop-icon.svg";
 import PlayIcon from "../../assets/topbar-buttons/play-icon.svg";
 import ClearIcon from "../../assets/topbar-buttons/clear-icon.svg";
+import { DataflowSerialConnectButton } from "./dataflow-serial-connect";
+
 
 import "./dataflow-program-topbar.scss";
 
@@ -31,52 +33,14 @@ export const DataflowProgramTopbar = (props: TopbarProps) => {
   const { onSerialRefreshDevices, readOnly, serialDevice, programDataRates,
     dataRate, onRateSelectClick, onRecordDataChange, programRecordState, numNodes } = props;
 
-
-  // Of the boards tested, only authentic Arduinos (usbProductId === 67) raise the browser `connect` event
-  // Which we use to track physical connection independently of port state
-  // So we only warn of a lack of physical connection when using an known board
-  const knownBoard = serialDevice.deviceInfo?.usbProductId === 67;
-  const lastMsg = localStorage.getItem("last-connect-message");
-  const classes = classNames(
-    "icon-serial",
-    { "physical-connection": lastMsg === "connect"},
-    { "no-physical-connection": lastMsg === "disconnect" && knownBoard},
-    serialDevice.serialNodesCount > 0 ? "nodes-in-need" : "no-serial-needed",
-    serialDevice.hasPort() ? "has-port" : "no-port"
-  );
-  function serialMessage(){
-    // nodes that use serial, but no device physically connected
-    if (lastMsg !== "connect" && serialDevice.serialNodesCount > 0){
-      return knownBoard ? "connect a device" : "";
-    }
-    // physical connection has been made but user action needed
-    if (lastMsg === "connect"
-        && !serialDevice.hasPort()
-        && serialDevice.serialNodesCount > 0
-    ){
-      return "click to finish connecting";
-    }
-    else {
-      return "";
-    }
-  }
-
   return (
     <div className="program-editor-topbar">
 
-        <div className="topbar-icon">
-          {<IconButton
-            icon="serial"
-            key="serial"
-            onClickButton={onSerialRefreshDevices}
-            title="Refresh Serial Connection"
-            disabled={readOnly}
-            className={classes}
-          />}
-          <div className="serial-message">
-            { serialMessage() }
-          </div>
-        </div>
+        <DataflowSerialConnectButton
+          onSerialRefreshDevices={onSerialRefreshDevices}
+          readOnly={readOnly}
+          serialDevice={serialDevice}
+        />
 
         <div className="topbar-center-container">
           <div className="topbar-blank-or-play">
