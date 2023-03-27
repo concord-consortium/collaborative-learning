619440ff2ecdd45a661c2c5466e5d8e5e5f6d51a
Teale Fristoe <fristoe@gmail.com>
2023-03-27 16:26:33 -0700

    Correct name of function.

--- src/utilities/string-utils.ts
+++ src/utilities/string-utils.ts
@@ -1,4 +1,4 @@
 export const escapeBackslashes = (text: string) => text.replaceAll(`\\`, `\\\\`);
 export const escapeDoubleQuotes = (text: string) => text.replaceAll(`"`, `\\"`);
 // export const removeNewlines = (text: string) => text.replaceAll(`\\n`, ``).replaceAll(`\\r`, ``);
-export const removeNewLines = (text: string) => text.replace(/\r?\n|\r/g, "");
+export const removeNewlines = (text: string) => text.replace(/\r?\n|\r/g, "");
