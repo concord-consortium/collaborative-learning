41c9a2fe1d8d47c460e056a4355dee45bf43ad41
Teale Fristoe <fristoe@gmail.com>
2023-03-29 14:12:52 -0700

    Add basic doc editor and solution button cypress tests.

---
+++ cypress/e2e/clue/branch/author_tests/doc-editor_spec.js
@@ -0,0 +1,32 @@
+
+const url = "/doc-editor.html";
+
+context('History Playback', () => {
+
+  beforeEach(() => {
+    cy.clearQAData('all');
+
+    cy.visit(url);
+    cy.get('.editable-document-content', {timeout: 60000});
+  });
+
+  const documentContent = () => cy.get(".canvas-area .canvas .document-content");
+  it('verify doc editor loads', function() {
+    cy.get(".toolbar").should("exist");
+    documentContent().should("exist");
+  });
+
+  it('solution button works', function() {
+    const toolbarButton = tool => cy.get(`.toolbar .${tool}`);
+    const solutionButton = () => cy.get(".toolbar .solution");
+    const imageTile = () => documentContent().find(".image-tool-tile").last();
+    solutionButton().should("exist").should("have.class", "disabled");
+    toolbarButton("image").click();
+    imageTile().should("exist").click();
+    solutionButton().should("have.class", "enabled");
+    solutionButton().click();
+    imageTile().should("have.class", "teacher");
+    solutionButton().click();
+    imageTile().should("not.have.class", "teacher");
+  });
+});
