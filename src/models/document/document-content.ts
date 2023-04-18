7f43be7ce3479df07d94b9e3df9c9b799e876bdb
Scott Cytacki <scytacki@concord.org>
2023-04-18 12:55:36 -0400

    fix DocumentContentModel name
    The MST model has to be named DocumentContent

--- src/models/document/document-content.ts
+++ src/models/document/document-content.ts
@@ -5,7 +5,9 @@ import { DocumentContentModelWithTileDragging } from "./drag-tiles";
 // I'm not sure but it might be possible to do composition so multiple
 // concerns each using properties of BaseDocumentContentModel
 // Are then combined in the DocumentContentModel
-export const DocumentContentModel = DocumentContentModelWithTileDragging.named("DocumentContentModel");
+// Note: the name "DocumentContent" is important because it is used in other parts
+// of the code.
+export const DocumentContentModel = DocumentContentModelWithTileDragging.named("DocumentContent");
 
 export type DocumentContentModelType = Instance<typeof DocumentContentModel>;
 export type DocumentContentSnapshotType = SnapshotIn<typeof DocumentContentModel>;
