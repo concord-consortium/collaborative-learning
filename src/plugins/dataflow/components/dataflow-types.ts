d84e78c557c3ab90c96b255f62a12cc22d9d0465
Teale Fristoe <fristoe@gmail.com>
2023-04-27 06:30:55 -0700

    Improve draggable id test functions.

--- src/plugins/dataflow/components/dataflow-types.ts
+++ src/plugins/dataflow/components/dataflow-types.ts
@@ -4,10 +4,10 @@ export const kDataflowDroppableId = `dataflow-droppable`;
 
 export const nodeDraggableId = (nodeType: string, tileId: string) =>
   `${kNewNodeButtonDraggableId}_${tileId}_${nodeType}`;
-export const isNodeDraggableId = (id: string) => id.split("_")[0] === kNewNodeButtonDraggableId;
+export const isNodeDraggableId = (id: string) => id.startsWith(kNewNodeButtonDraggableId);
 export const nodeDroppableId = (nodeType: string, tileId: string) =>
   `${kNewNodeButtonDroppableId}_${tileId}_${nodeType}`;
-export const isNodeDroppableId = (id: string) => id.split("_")[0] === kNewNodeButtonDroppableId;
+export const isNodeDroppableId = (id: string) => id.startsWith(kNewNodeButtonDroppableId);
 export const getNodeType = (id: string) => {
   if (isNodeDraggableId(id) || isNodeDroppableId(id)) {
     const parts = id.split("_");
