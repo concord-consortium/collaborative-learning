40e8f00f81a135e5c9cc9434172e3fae84da1069
Joe Bacal <bacalj@gmail.com>
2023-04-26 21:24:02 -0400

    these are correct

--- src/plugins/dataflow/nodes/controls/humidifier-animation.tsx
+++ src/plugins/dataflow/nodes/controls/humidifier-animation.tsx
@@ -4,52 +4,57 @@ interface IProps {
   nodeValue: number;
 }
 
-function playThrough(total: number, f: any){
-  for (let i = 0; i < total; i++){
-    setTimeout(() => {
-      f(i)
-    }, i * 100)
-  }
-}
+const rampUpCount = humidAnimationPhases.rampUp.frames.length;
+const rampDownCount = humidAnimationPhases.rampDown.frames.length;
+const stayOnCount = humidAnimationPhases.stayOn.frames.length;
+const { rampDown, rampUp, stayOn, stayOff } = humidAnimationPhases;
 
-
+console.log("| humidAnimationPhases", humidAnimationPhases)
 export const HumidiferAnimation: React.FC<IProps> = ({nodeValue}) => {
-  const [currentPhase, setCurrentPhase] = useState<AnimationPhase>(humidAnimationPhases.stayOff);
+  const [currentPhase, setCurrentPhase] = useState<AnimationPhase | null>(null);
   const [intervalId, setIntervalId] = useState(null);
   const [currentFrameIndex, setCurrentFrameIndex] = useState(0);
+  const [playing, setPlaying] = useState(false);
   const priorValue = useRef<number | undefined>();
+
+  const niceLog = (msg: string) => {
+    console.log(msg, `\n    | prior: ${priorValue.current} | value: ${nodeValue} | phase: ${currentPhase?.name}[${currentFrameIndex}] | interval: ${intervalId}`)
+  }
 
   const disposeInterval = () => {
     setIntervalId(null)
   }
 
   useEffect(() => {
-    const existingValue = priorValue.current;
-    const rampUp = existingValue === 0 && nodeValue === 1;
-    const rampDown = existingValue === 1 && nodeValue === 0;
+    const justLoaded = priorValue.current === undefined;
+    const shouldRampUp = priorValue.current === 0 && nodeValue === 1;
+    const shouldRampDown = priorValue.current === 1 && nodeValue === 0;
+    const shouldJustLoop = justLoaded && nodeValue === 1;
+    const shouldJustRest = justLoaded && nodeValue === 0;
 
-    if (rampUp){
-      setCurrentPhase(humidAnimationPhases.rampUp)
-      playThrough(humidAnimationPhases.rampUp.frames.length, setCurrentFrameIndex)
-      // const interval = setInterval(() => {
-      //   setCurrentFrameIndex((currentFrameIndex) => currentFrameIndex + 1 % humidAnimationPhases.rampUp.frames.length);
-      // }, 100);
-      // setIntervalId(interval as any);
+    setCurrentFrameIndex(0);
+
+    if (shouldRampUp) {
+      console.log("|> shouldRampUp")
     }
 
-    if (rampDown){
-      setCurrentPhase(humidAnimationPhases.rampDown)
-      playThrough(humidAnimationPhases.rampDown.frames.length, setCurrentFrameIndex)
+    if (shouldRampDown) {
+      console.log("|> shouldRampDown")
+    }
+
+    if (shouldJustLoop) {
+      console.log("|> shouldJustLoop")
+    }
+
+    if (shouldJustRest) {
+      console.log("|> shouldJustRest")
     }
 
     priorValue.current = nodeValue;
-
-    console.log("loop?", {nodeValue}, {existingValue}, {rampUp}, {rampDown})
-
     return disposeInterval();
   },[nodeValue])
 
   return (<>
-    <img className="mist" src={currentPhase.frames[currentFrameIndex]} />
+    <img className="mist" src={humidAnimationPhases.stayOn.frames[1]} />
   </>)
 }
