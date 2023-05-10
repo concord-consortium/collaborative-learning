4b21fc7da01fac131c61c384f97666c5772b58aa
dennisrcao <dcao@concord.org>
2023-05-10 10:18:06 -0700

    address Scott's algorithm comment

--- src/models/data/data-set-utils.ts
+++ src/models/data/data-set-utils.ts
@@ -1,58 +1,20 @@
-import { kDefaultLabel } from "../../plugins/data-card/data-card-types";
-import { IDataSet, addAttributeToDataSet, addCasesToDataSet } from "../../models/data/data-set";
+import { addAttributeToDataSet, IDataSet, addCasesToDataSet } from "./data-set";
 
-
-//used by data-card-tile.tsx when dragging one tile onto another
-export const mergeTwoDataSets = ( dataSetOfDraggedTile: IDataSet, dataSetOfDroppedTile: IDataSet ) => {
-
-  // search for duplicates between attr of draggedTile & droppedTile - only add names of unique attr
-  const attrNamesDraggedTile = dataSetOfDraggedTile.attributes.map((attrObj: any) => attrObj.name);
-  const attrNamesDroppedTile = Object.keys(dataSetOfDroppedTile.attrNameMap);
-
-  attrNamesDraggedTile.forEach((attrNameDrag: any) => {
-    let foundAttrFlag = false;
-    attrNamesDroppedTile.forEach((attrNameDrop: any) => {
-      if (attrNameDrag === attrNameDrop){
-        foundAttrFlag = true;
+export function mergeTwoDataSets(source: IDataSet, target: IDataSet) {
+    const sourceAttrNames = source.attributes.map((attrObj: any) => attrObj.name);
+    sourceAttrNames.forEach((name) => {
+      if (!target.attrNameMap[name]) { // only add unique attributes in source not in target
+        addAttributeToDataSet(target, { name });
       }
     });
-    if (!foundAttrFlag){
-      addAttributeToDataSet(dataSetOfDroppedTile, { name: `${attrNameDrag}` });
-    } //else don't add
-  });
-
-
-  //Add data cards (case) from draggedTile
-  dataSetOfDraggedTile.cases.forEach((card: any) => {
-    addCasesToDataSet(dataSetOfDroppedTile, [{ [kDefaultLabel]: "" }]);
-  });
-
-  //iterate through the cards added and insert values attribute names match
-  const numCasesDraggedTile = dataSetOfDraggedTile.cases.length;
-  const attrIdsDroppedTile = Object.keys(dataSetOfDroppedTile.caseIDMap);
-  const allCasesDroppedTile = dataSetOfDroppedTile.getCases(attrIdsDroppedTile);
-  const startIndexDroppedTile = allCasesDroppedTile.length - numCasesDraggedTile;
-  let startIndexDraggedTile = 0;
-  const attrOfDroppedTile = dataSetOfDroppedTile.attributes;
-  const attrOfDraggedTile = dataSetOfDraggedTile.attributes;
-
-  // //insert all values
-  for (let i = startIndexDroppedTile; i < allCasesDroppedTile.length; i++){ //start at cards that were added
-    const caseId = allCasesDroppedTile[i]?.__id__;
-    attrOfDroppedTile.forEach((attrDrop) => {
-      attrOfDraggedTile.forEach((attrDrag: any, idx: number) => {
-        if (attrDrop.name === attrDrag.name){
-          const val = attrDrag.values[startIndexDraggedTile];
-          if (caseId){
-            dataSetOfDroppedTile.setCanonicalCaseValues([
-              { __id__: caseId, [attrDrop.id]: val }
-            ]);
-          }
-        }
+    const sourceCases: any[] = [];
+    source.cases.forEach((aCase, idx) => {
+      const newCase: any = {};
+      source.attributes.forEach((attr) => {
+        const attrName = attr.name as keyof typeof newCase;
+        newCase[attrName] = attr.values[idx];
       });
+      sourceCases.push(newCase);
     });
-    startIndexDraggedTile ++;
-  }
-};
-
-
+    addCasesToDataSet(target, sourceCases);
+}
