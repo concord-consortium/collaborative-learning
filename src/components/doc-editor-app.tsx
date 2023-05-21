5eb18e6d6555fc6d5efe1c30745281ec70f56361
Scott Cytacki <scytacki@concord.org>
2023-05-20 23:59:01 -0400

    improve standalone doc editor
    - allow passing a document url as a param
    - allow saving a document if the document wasn't opened
    - allow loading basic documents, not section documents
    - show the file name of the opened or saved document
    - add new hot key for copying raw document content: non export document
      raw document content works with shared models better

--- src/components/doc-editor-app.tsx
+++ src/components/doc-editor-app.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import stringify from "json-stringify-pretty-compact";
+import { getSnapshot } from "mobx-state-tree";
 
 import { defaultDocumentModel, defaultDocumentModelParts } from "./doc-editor-app-defaults";
 import { EditableDocumentContent } from "./document/editable-document-content";
 import { createDocumentModel } from "../models/document/document";
 import { AppConfigModelType } from "../models/stores/app-config-model";
 import { DocumentContentSnapshotType } from "../models/document/document-content";
+import { urlParams } from "../utilities/url-params";
 
 export interface IDocEditorAppProps {
   appConfig: AppConfigModelType;
@@ -16,51 +19,99 @@ export const DocEditorApp = ({ appConfig }: IDocEditorAppProps) => {
   });
   const [fileHandle, setFileHandle] = useState<FileSystemHandle|undefined>();
   const [sectionSnapshot, setSectionSnapshot] = useState<any>();
+  const [fileName, setFileName] = useState<string>("");
 
-  // The most useful files to edit like this are currently sections
-  // so lets work with those
-  async function handleOpen() {
-    const [_fileHandle] = await (window as any).showOpenFilePicker();
-    setFileHandle(_fileHandle);
-    const file = await _fileHandle.getFile();
-    const text = await file.text();
-    const _sectionSnapshot = JSON.parse(text);
-    setSectionSnapshot(_sectionSnapshot);
-    const documentContentSnapshot = _sectionSnapshot.content as DocumentContentSnapshotType;
+  function loadDocument(text: string) {
+    const _parsedText = JSON.parse(text);
+    let documentContentSnapshot;
+    if (_parsedText.content ) {
+      setSectionSnapshot(_parsedText);
+      documentContentSnapshot = _parsedText.content as DocumentContentSnapshotType;
+    } else {
+      documentContentSnapshot = _parsedText;
+    }
     setDocument(createDocumentModel({
       ...defaultDocumentModelParts,
       content: documentContentSnapshot
     }));
   }
 
+  // Handle opening both section documents with a content field
+  // and basic documents which are just the content itself
+  async function handleOpen() {
+    const [_fileHandle] = await (window as any).showOpenFilePicker();
+    setFileHandle(_fileHandle);
+    const file = await _fileHandle.getFile();
+    setFileName(file.name);
+    const text = await file.text();
+    loadDocument(text);
+  }
+
   async function handleSave() {
-    if (!fileHandle || !document.content) {
-      console.error("Can't save without a fileHandle or document.content");
+    if (!document.content) {
+      console.error("Can't save without document.content");
       return;
     }
 
-    if (!sectionSnapshot) {
-      console.error("Currently can't save without a section snapshot");
-      return;
+    let _fileHandle = fileHandle;
+    if (!_fileHandle) {
+      _fileHandle = await (window as any).showSaveFilePicker();
+      setFileHandle(_fileHandle);
+      if (_fileHandle) {
+        const file = await (_fileHandle as any).getFile();
+        setFileName(file.name);
+      }
+
     }
 
-    // construct the contents of the section
-    const docContentString = document.content.exportAsJson({ includeTileIds: true });
-    const docContentJSON = JSON.parse(docContentString);
-    sectionSnapshot.content = docContentJSON;
-    const contents = JSON.stringify(sectionSnapshot, undefined, 2);
+    let contents;
+    if (sectionSnapshot) {
+      // construct the contents of the section
+      const docContentString = document.content.exportAsJson({ includeTileIds: true });
+      const docContentJSON = JSON.parse(docContentString);
+      sectionSnapshot.content = docContentJSON;
+      contents = JSON.stringify(sectionSnapshot, undefined, 2);
+    } else {
+      // TODO: we probably want to keep track if we loaded an exported or raw file
+      // and then save it the same way
+      const contentJson = getSnapshot(document.content);
+      contents = stringify(contentJson, {maxLength: 100});
+    }
 
-    const writable = await (fileHandle as any).createWritable();
+    const writable = await (_fileHandle as any).createWritable();
     // Write the contents of the file to the stream.
     await writable.write(contents);
     // Close the file and write the contents to disk.
     await writable.close();
   }
 
+  useEffect(() => {
+    const {document: documentURL} = urlParams;
+    if (!documentURL) {
+      return;
+    }
+
+    fetch(documentURL)
+    .then(async response => {
+      if (response.ok) {
+        const documentText = await response.text();
+        loadDocument(documentText);
+      } else {
+        // If the document is not found, return the response so that the caller can
+        // handle it appropriately.
+        throw Error(`Request rejected with status ${response.status}`);
+      }
+    })
+    .catch(error => {
+      throw Error(`Request rejected with exception ${error}`);
+    });
+  }, []);
+
   return (
     <>
       <button onClick={handleOpen}>open</button>
       <button onClick={handleSave}>save</button>
+      <span>{fileName}</span>
       <EditableDocumentContent
         contained={false}
         mode="1-up"
