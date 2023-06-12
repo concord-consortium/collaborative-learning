4aab3589a80e69e06be49599908bc0c06ff3e2f0
Joe Bacal <bacalj@gmail.com>
2023-06-12 15:51:31 -0400

    move to external function

--- src/plugins/expression/expression-tile-utils.ts
+++ src/plugins/expression/expression-tile-utils.ts
@@ -1,6 +1,55 @@
+import { MathfieldElement } from 'mathlive';
+
 export function replaceKeyBinding(bindings: any[], keyPress: string, command: string) {
   const index = bindings.findIndex(binding => binding.key === keyPress);
   if (index >= 0) {
     bindings[index].command = command;
   }
+}
+
+function getEditableStatus(mf: MathfieldElement) {
+  let editableStatus: "empty" | "allSelected" | "someSelected"| "cursorInContent" | undefined;
+
+  const exp = mf.value;
+  const selStart = mf.selection.ranges[0][0];
+  const selEnd = mf.selection.ranges[0][1];
+  const pos = mf.position;
+
+  const locale = pos === 0 ? "beginning"
+    : (pos === exp.length || (pos === selEnd && pos === selStart) ? "end"
+    : "middle");
+
+  if (exp.length === 0 || exp === "\\placeholder" || exp === " ") editableStatus = "empty";
+  else if (selStart === 0 && selEnd === exp.length) editableStatus = "allSelected";
+  else if (selStart === 0 && selEnd === pos && pos !== selStart) editableStatus = "allSelected";
+  else if (selStart !== selEnd) editableStatus = "someSelected";
+  else if (selStart === selEnd && selStart === pos) editableStatus = "cursorInContent";
+  else editableStatus = undefined;
+
+  return editableStatus;
+}
+
+export function getMixedFractionCommandArray(mf: MathfieldElement ) {
+  const editableStatus = getEditableStatus(mf);
+  const ph = "\\placeholder{}";
+  const emptyFrac = `\\frac{${ph}}{${ph}}}`;
+
+  const replaceAll = editableStatus === "empty" || editableStatus === "allSelected";
+
+  if (replaceAll){
+    return ["insert", ph + emptyFrac, {insertionMode: "replaceAll"}]
+  }
+
+  else if (editableStatus === "cursorInContent") {
+    return ["insert", `#@${emptyFrac}`, {insertionMode: "insertAfter"}]
+  }
+
+  else if (editableStatus === "someSelected") {
+    return ["insert", `#@${emptyFrac}`, {insertionMode: "insertAfter"}]
+  }
+
+  else {
+    return ["insert", `#@${emptyFrac}`, {insertionMode: "insertAfter"}]
+  }
+
 }
--- src/plugins/expression/expression-toolbar.tsx
+++ src/plugins/expression/expression-toolbar.tsx
@@ -1,5 +1,4 @@
 import { observer } from "mobx-react";
-import { ComputeEngine } from "@cortex-js/compute-engine";
 import classNames from "classnames";
 import React from "react";
 import ReactDOM from "react-dom";
@@ -7,6 +6,7 @@ import {
   IFloatingToolbarProps, useFloatingToolbarLocation
 } from "../../components/tiles/hooks/use-floating-toolbar-location";
 import { ExpressionContentModelType } from "./expression-content";
+import { getMixedFractionCommandArray } from "./expression-tile-utils";
 import { ITileModel } from "../../models/tiles/tile-model";
 
 import "./expression-toolbar.scss";
@@ -18,10 +18,6 @@ interface IProps extends IFloatingToolbarProps {
   mf: React.RefObject<MathfieldElement> | undefined;
   trackedCursorPos: React.MutableRefObject<number>;
 }
-
-const ce = new ComputeEngine();
-// find the code using the "power" button
-
 
 export const ExpressionToolbar: React.FC<IProps> = observer((
   { model, documentContent, mf, tileElt, onIsEnabled,
@@ -60,58 +56,9 @@ export const ExpressionToolbar: React.FC<IProps> = observer((
   };
 
   const addMixedFraction = () => {
-    if (!mf) return;
-
-    // collect info on state of the mathfield vis=à-vis selection and curspr
-    const exp = content.latexStr;
-    const selStart = mf.current?.selection.ranges[0][0];
-    const selEnd = mf.current?.selection.ranges[0][1];
-    const pos = mf.current?.position;
-
-    let editableStatus: "empty" | "allSelected" | "someSelected"| "cursorInContent" | undefined;
-
-    // this needs to be reworked, but the logic now works
-    if (exp.length === 0 || exp === "\\placeholder" || exp === " ") editableStatus = "empty";
-    else if (selStart === 0 && selEnd === exp.length) editableStatus = "allSelected";
-    else if (selStart === 0 && selEnd === pos && pos !== selStart) editableStatus = "allSelected";
-    else if (selStart !== selEnd) editableStatus = "someSelected";
-    else if (selStart === selEnd && selStart === pos) editableStatus = "cursorInContent";
-    else editableStatus = undefined;
-
-    // locale is not always correct
-    const locale = pos === 0 ? "beginning" : (pos === exp.length || (pos === selEnd && pos === selStart) ? "end" : "middle");
-    const parsedJson = JSON.stringify(ce.parse(exp).json);
-    const parsedLatex = ce.parse(exp).latex;
-
-    const ph = "\\placeholder{}";
-    const emptyFrac = `\\frac{${ph}}{${ph}}}`;
-
-    if (editableStatus === "empty" || editableStatus === "allSelected"){
-      mf.current?.executeCommand(
-        ["insert", ph + emptyFrac, {insertionMode: "replaceAll"}]
-      );
-    } else if (editableStatus === "cursorInContent") {
-      mf.current?.executeCommand(
-        ["insert", `#@${emptyFrac}`, {insertionMode: "insertAfter"}]
-      );
-    } else if (editableStatus === "someSelected") {
-      const currentSelection = mf.current?.selection;
-      console.log("| handle someSelected |",
-      "\n CONTENT:",
-      "\n   initial exp:      ", exp,
-      "\n   parsedJson:       ", parsedJson,
-      "\n   parsedLatex:      ", parsedLatex,
-      "\n SELECTION: ",
-      "\n   selStart:         ", selStart,
-      "\n   selEnd:           ", selEnd,
-      "\n   pos:              ", pos,
-      "\n   currentSelection: ", currentSelection,
-      "\n DERIVED: ",
-      "\n   editableStatus:   ", editableStatus,
-      "\n   cLocale:          ", locale
-      )
-    }
-
+    if (!(mf && mf.current)) return;
+    const c = getMixedFractionCommandArray(mf.current)
+    mf.current.executeCommand(c as any);
     mf.current?.focus();
   };
 
