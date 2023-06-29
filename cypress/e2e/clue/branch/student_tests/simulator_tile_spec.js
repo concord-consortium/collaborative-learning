2d8a9c424172b9c0d1a7eaf6d2c5d124b1434e64
Teale Fristoe <fristoe@gmail.com>
2023-06-29 11:49:57 -0700

    Finish cypress tests.

--- cypress/e2e/clue/branch/student_tests/simulator_tile_spec.js
+++ cypress/e2e/clue/branch/student_tests/simulator_tile_spec.js
@@ -42,30 +42,51 @@ context('Simulator Tile', function () {
     it("links to dataflow tile", () => {
       clueCanvas.addTile("dataflow");
 
+      // Simulation options are not present in the sensor before the simulation has been added to the document
       const sensor = "sensor";
       dataflowTile.getCreateNodeButton(sensor).click();
       dataflowTile.getDropdown(sensor, "sensor-type").click();
       dataflowTile.getSensorDropdownOptions(sensor).eq(7).find(".label").click(); // EMG
       dataflowTile.getDropdown(sensor, "sensor-select").click();
       dataflowTile.getSensorDropdownOptions(sensor).should("have.length", 4);
-      dataflowTile.getSensorDropdownOptions(sensor).eq(0).click();
+      // Click the background to not select any option
+      cy.get(".flow-tool").click();
       dataflowTile.getNodeValueContainer(sensor).invoke('text').then(parseFloat).should("equal", 0);
 
+      // Simulation options are not present in the live output before the simulation has been added to the document
       const lo = "live-output";
       dataflowTile.getCreateNodeButton(lo).click();
       dataflowTile.getDropdown(lo, "liveOutputType").click();
       dataflowTile.getDropdownOptions(lo, "liveOutputType").eq(1).click(); // Gripper
       dataflowTile.getDropdown(lo, "hubSelect").click();
       dataflowTile.getDropdownOptions(lo, "hubSelect").should("have.length", 4);
-      dataflowTile.getDropdownOptions(lo, "hubSelect").eq(0).click();
+      // Click the background to not select any option
+      cy.get(".flow-tool").click();
 
+      // Sensor options are correct after adding the simulation to the document
       clueCanvas.addTile("simulator");
       dataflowTile.getDropdown(sensor, "sensor-select").click();
       dataflowTile.getSensorDropdownOptions(sensor).should("have.length", 5);
       dataflowTile.getSensorDropdownOptions(sensor).eq(3).click();
       dataflowTile.getNodeValueContainer(sensor).invoke('text').then(parseFloat).should("be.gt", 0);
 
+      // Live output options are correct after adding the simulation to the document
+      dataflowTile.getDropdown(lo, "hubSelect").click();
+      dataflowTile.getDropdownOptions(lo, "hubSelect").should("have.length", 5);
+      dataflowTile.getDropdownOptions(lo, "hubSelect").eq(4).click();
 
+      // Simulator tile's output variable updates when dataflow sets it
+      dataflowTile.getCreateNodeButton("number").click();
+      dataflowTile.getNumberField().type("1{enter}");
+      const output = () => dataflowTile.getNode("number").find(".socket.output");
+      const input = () => dataflowTile.getNode(lo).find(".socket.input");
+      output().click();
+      input().click({ force: true });
+      dataflowTile.getOutputNodeValueText().should("contain", "100% closed");
+      simulatorTile.getSimulatorTile().should("contain.text", "Gripper Output: 100");
+
+      // Pressure variable updates when the gripper changes
+      simulatorTile.getSimulatorTile().should("contain.text", "Surface Pressure Sensor: 400");
     });
   });
 });
