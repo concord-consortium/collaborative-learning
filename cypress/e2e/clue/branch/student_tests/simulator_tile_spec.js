eb509972612f8d7bcf1a8f59ec9c58d2d2fe557c
Teale Fristoe <fristoe@gmail.com>
2023-06-29 08:31:41 -0700

    Progress on cypress tests.

--- cypress/e2e/clue/branch/student_tests/simulator_tile_spec.js
+++ cypress/e2e/clue/branch/student_tests/simulator_tile_spec.js
@@ -1,7 +1,9 @@
 import ClueCanvas from '../../../../support/elements/clue/cCanvas';
+import DataflowTile from "../../../../support/elements/clue/DataflowToolTile";
 import SimulatorTile from '../../../../support/elements/clue/SimulatorTile';
 
 let clueCanvas = new ClueCanvas;
+const dataflowTile = new DataflowTile;
 let simulatorTile = new SimulatorTile;
 
 context('Simulator Tile', function () {
@@ -21,7 +23,13 @@ context('Simulator Tile', function () {
       // TODO: These should be removed when we change what's displayed in the simulator tile
       simulatorTile.getSimulatorTile().should("contain.text", "EMG Sensor");
       simulatorTile.getSimulatorTile().should("contain.text", "Surface Pressure Sensor");
-      simulatorTile.getSimulatorTile().should("contain.text", "Grabber Output");
+      simulatorTile.getSimulatorTile().should("contain.text", "Gripper Output");
+      // Icons are being rendered
+      simulatorTile.getSimulatorTile().find(".leading-box.variable-icon").should("have.length", 3);
+
+      cy.get(".arm-image").should("exist");
+      cy.get(".arduino-image").should("exist");
+      cy.get(".gripper-image").should("exist");
     });
     it("edit tile title", () => {
       const newName = "Test Simulation";
@@ -30,6 +38,34 @@ context('Simulator Tile', function () {
       simulatorTile.getSimulatorTileTitle().click();
       simulatorTile.getSimulatorTileTitle().type(newName + '{enter}');
       simulatorTile.getTileTitle().should("contain", newName);
+    });
+    it("links to dataflow tile", () => {
+      clueCanvas.addTile("dataflow");
+
+      const sensor = "sensor";
+      dataflowTile.getCreateNodeButton(sensor).click();
+      dataflowTile.getDropdown(sensor, "sensor-type").click();
+      dataflowTile.getSensorDropdownOptions(sensor).eq(7).find(".label").click(); // EMG
+      dataflowTile.getDropdown(sensor, "sensor-select").click();
+      dataflowTile.getSensorDropdownOptions(sensor).should("have.length", 4);
+      dataflowTile.getSensorDropdownOptions(sensor).eq(0).click();
+      dataflowTile.getNodeValueContainer(sensor).invoke('text').then(parseFloat).should("equal", 0);
+
+      const lo = "live-output";
+      dataflowTile.getCreateNodeButton(lo).click();
+      dataflowTile.getDropdown(lo, "liveOutputType").click();
+      dataflowTile.getDropdownOptions(lo, "liveOutputType").eq(1).click(); // Gripper
+      dataflowTile.getDropdown(lo, "hubSelect").click();
+      dataflowTile.getDropdownOptions(lo, "hubSelect").should("have.length", 4);
+      dataflowTile.getDropdownOptions(lo, "hubSelect").eq(0).click();
+
+      clueCanvas.addTile("simulator");
+      dataflowTile.getDropdown(sensor, "sensor-select").click();
+      dataflowTile.getSensorDropdownOptions(sensor).should("have.length", 5);
+      dataflowTile.getSensorDropdownOptions(sensor).eq(3).click();
+      dataflowTile.getNodeValueContainer(sensor).invoke('text').then(parseFloat).should("be.gt", 0);
+
+
     });
   });
 });
