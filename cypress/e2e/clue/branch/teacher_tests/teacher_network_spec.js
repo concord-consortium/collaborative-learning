c7fa9d69ea73daa0c1afbb5c95d1d3e758c3290b
eireland <eireland@concord.org>
2023-07-10 20:03:59 -0700

    Comments out showing network and my class sections in starred tabs in cypress test

--- cypress/e2e/clue/branch/teacher_tests/teacher_network_spec.js
+++ cypress/e2e/clue/branch/teacher_tests/teacher_network_spec.js
@@ -36,9 +36,9 @@ describe('Networked dividers for networked teacher', () => {
     teacherNetwork.verifyDividerLabel('workspaces', 'my-classes');
     teacherNetwork.verifyDividerLabel('workspaces', 'my-network');
 
-    cy.openSection('my-work', 'starred');
-    teacherNetwork.verifyDividerLabel('starred', 'my-classes');
-    teacherNetwork.verifyDividerLabel('starred', 'my-network');
+    // cy.openSection('my-work', 'starred');
+    // teacherNetwork.verifyDividerLabel('starred', 'my-classes');
+    // teacherNetwork.verifyDividerLabel('starred', 'my-network');
 
     cy.openSection('my-work', 'learning-log');
     teacherNetwork.verifyDividerLabel('learning-log', 'my-classes');
@@ -58,8 +58,8 @@ describe('Networked dividers for networked teacher', () => {
     teacherNetwork.verifyDividerLabel('learning-logs', 'my-classes');
     teacherNetwork.verifyDividerLabel('learning-logs', 'my-network');
 
-    cy.openSection('class-work', 'starred');
-    teacherNetwork.verifyDividerLabel('starred', 'my-classes');
-    teacherNetwork.verifyDividerLabel('starred', 'my-network');
+    // cy.openSection('class-work', 'starred');
+    // teacherNetwork.verifyDividerLabel('starred', 'my-classes');
+    // teacherNetwork.verifyDividerLabel('starred', 'my-network');
   });
 });
