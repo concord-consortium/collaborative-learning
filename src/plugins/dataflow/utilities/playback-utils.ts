2600084e4e9d50ca7ba6a241f926d5122acab5e8
Joe Bacal <bacalj@gmail.com>
2023-07-13 11:27:15 -0400

    wip

--- src/plugins/dataflow/utilities/playback-utils.ts
+++ src/plugins/dataflow/utilities/playback-utils.ts
@@ -1,36 +1,29 @@
 import { kMaxNodeValues } from "../model/utilities/node";
 
 function getPriorCases(index: number, dataSet: any, playhead: number){
-  // if we have less than 17 cases our region begins at 0
-  const offSet = 1; // I'm 99% sure we need this but math is hard
+  // kMaxNodeValues determines how many datapoints are plotted each time
+  const offSet = 1;
   const maxValues = kMaxNodeValues + offSet;
   const pastCalc = playhead - maxValues;
   const regionStart = pastCalc < 0 ? 0 : pastCalc;
+  const countOfCasesToGet = playhead < maxValues ? playhead : maxValues;
 
-  console.log("PB: 1 get the cases from ", regionStart, " to ", playhead);
-
-  // const fetchUs = [];
-  // for (let i = regionStart; i < playhead; i++) {
-  //   fetchUs.push(i);
-  // }
-
-  //console.log("PB: 2 which are indices: fetchUs:", fetchUs);
-
-  // thought we needed the playhead here but I think we just need the regionStart
-  //const cases = dataSet.getCasesAtIndices(regionStart, playhead);
-
-  const numberOfCasesToGet = playhead < maxValues ? playhead : maxValues;
-
-  const cases = dataSet.getCasesAtIndices(regionStart, numberOfCasesToGet);
+  const cases = dataSet.getCasesAtIndices(regionStart, countOfCasesToGet);
   return cases;
 }
 
 export function getRecentValuesForNode(node: any, dataSet: any, playbackIndex: number ){
 
   const priorCases = getPriorCases(playbackIndex, dataSet, playbackIndex);
+  console.log("priorCases", priorCases);
 
-  console.log("PB: 2 and now we have our priorCases:", priorCases, "\n");
-  console.log("\n");
+  const nodeValues = priorCases.map((c: any) => {
+    console.log("I need the attribute id for the node: ", node);
+    console.log("Then, I use that attribute to get the particular value from the case", c);
+  });
+
+  const nv = nodeValues.reverse();
+
 
   const randomDummyValues = Array.from({length: 10}, () => Math.floor(Math.random() * 10));
   return randomDummyValues;
