a80c83f153819e973e9f43c762631a6e10dcb045
Joe Bacal <bacalj@gmail.com>
2023-07-13 15:43:26 -0400

    cleanup

--- src/plugins/dataflow/nodes/dataflow-node-plot.tsx
+++ src/plugins/dataflow/nodes/dataflow-node-plot.tsx
@@ -46,8 +46,6 @@ export const DataflowNodePlot: React.FC<INodePlotProps> = (props) => {
 
   const scaleBtnColorClass= props.data.name.charAt(0).toLowerCase() + props.data.name.slice(1);
 
-  // console.log("| C DataflowNodePlot: props.data", props.data);
-
   return (
     <div className="node-bottom-section">
       <div className="node-bottom-buttons">
--- src/plugins/dataflow/utilities/playback-utils.ts
+++ src/plugins/dataflow/utilities/playback-utils.ts
@@ -13,35 +13,13 @@ function getPriorCases(index: number, dataSet: any, playhead: number){
   return cases;
 }
 
-export function getRecentValuesForNode(node: any, dataSet: any, playbackIndex: number, nodeIndex: number ){ //idx from over there <--
+export function getRecentValuesForNode(node: any, dataSet: any, playbackIndex: number, nodeIndex: number ){
   const attributeId = getAttributeIdForNode(dataSet, nodeIndex);
-  console.log("PB 1: node: ", node.id, node.name, "-> ", attributeId);
-
-
   const priorCases = getPriorCases(playbackIndex, dataSet, playbackIndex);
-  console.log("PB 2: prior cases: \n ", priorCases);
-
   const calculatedRecentValues: any[] = [];
-
   priorCases.forEach((c: any) => {
-    console.log("   ....case:          ", c);
-    console.log("   & node ", node.id, node.name, attributeId);
     const valueOfNodeInCase = dataSet.getValue(c.__id__, attributeId);
     calculatedRecentValues.push(valueOfNodeInCase);
   });
-
-  // const nodeValues = priorCases.map((c: any) => {
-  //   console.log("case node: ", node.id, node.name);
-  //   console.log("Then, I use that attribute to get the particular value from the case", c);
-  // });
-
-  //const nv = nodeValues.reverse();
-
-  const randomDummyValues = Array.from({length: 10}, () => Math.floor(Math.random() * 10));
-
-  console.log("HEY whats the diff: ", calculatedRecentValues, " vs ", randomDummyValues);
-
-
-  //return randomDummyValues;
   return calculatedRecentValues;
 }
