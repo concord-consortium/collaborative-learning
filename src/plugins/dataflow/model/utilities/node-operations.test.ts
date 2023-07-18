4f8c8f48fadb87e1c946eb97435dc1df0a77a788
Joe Bacal <bacalj@gmail.com>
2023-07-18 11:43:28 -0400

    make test cases more complete

--- src/plugins/dataflow/model/utilities/node-operations.test.ts
+++ src/plugins/dataflow/model/utilities/node-operations.test.ts
@@ -3,12 +3,16 @@ import { NodeOperationTypes } from './node';
 describe('NodeOperationTypes', () => {
   it('should return the correct values for round, ceil, and floor methods', () => {
     const testCases = [
+      { input: -1.8,  round: -2,  floor: -2,  ceil: -1 },
+      { input: -1.2,  round: -1,  floor: -2,  ceil: -1 },
+      { input: -0.8,  round: -1,  floor: -1,  ceil: 0 },
+      { input: -0.2,  round: 0,   floor: -1,  ceil: 0 },
+      { input: 0,     round: 0,   floor: 0,   ceil: 0 },
+      { input: 0.2,   round: 0,   floor: 0,   ceil: 1 },
+      { input: 0.8,   round: 1,   floor: 0,   ceil: 1 },
       { input: 1,     round: 1,   floor: 1,   ceil: 1 },
       { input: 1.2,   round: 1,   floor: 1,   ceil: 2 },
-      { input: 1.8,   round: 2,   floor: 1,   ceil: 2 },
-      { input: -1.2,  round: -1,  floor: -2,  ceil: -1 },
-      { input: 1.8,   round: 2,   floor: 1,   ceil: 2 },
-      { input: -1.8,  round: -2,  floor: -2,  ceil: -1 },
+      { input: 1.8,   round: 2,   floor: 1,   ceil: 2 }
     ];
 
     NodeOperationTypes.forEach(operation => {
