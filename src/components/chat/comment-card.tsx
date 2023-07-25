5634c2184b9e25cfed2eaadf6c769cf8df3f254b
dennisrcao <dcao@concord.org>
2023-07-25 15:15:45 -0700

    running into issue with map function, putting console.logs to see if they show up in deployment

--- src/components/chat/comment-card.tsx
+++ src/components/chat/comment-card.tsx
@@ -61,10 +61,16 @@ export const CommentCard: React.FC<IProps> = ({ activeNavTab, user, postedCommen
   const { appConfig } = useStores();
   const { showCommentTag, commentTags, tagPrompt } = appConfig;
 
+  console.log("postedComments:", postedComments);
+  console.log("typeof", typeof (postedComments));
+  console.log("isArray", Array.isArray(postedComments));
+
+
   return (
     <div className="comment-card selected" data-testid="comment-card">
       <div className="comment-card-content selected" data-testid="comment-card-content">
         {postedComments?.map((comment, idx) => {
+            console.log("mapping");
             const userInitialBackgroundColor = ["#f79999", "#ffc18a", "#99d099", "#ff9", "#b2b2ff", "#efa6ef"];
             const commenterInitial = comment.name.charAt(0);
             const userInitialBackgroundColorIndex = parseInt(comment.uid, 10) % 6;
