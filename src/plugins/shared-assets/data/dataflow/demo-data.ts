ee4a642e116eb11ba9d5aed59aeee6f0f15425e4
Joe Bacal <bacalj@gmail.com>
2023-08-01 12:49:16 -0400

    cleanup

--- src/plugins/shared-assets/data/dataflow/demo-data.ts
+++ src/plugins/shared-assets/data/dataflow/demo-data.ts
@@ -1,18 +1,18 @@
-// creates an array of values that ramps up linear from startVal to endVal, with arrLen steps
-function fromTo(startVal: number, endVal: number, arrLen: number): number[] {
+// creates an array of len values, with values increasing linearly from startVal to endVal
+function fromTo(startVal: number, endVal: number, len: number): number[] {
   const ints = [];
-  const step = (endVal - startVal) / (arrLen - 1);
-  for (let i = 0; i < arrLen; i++) {
+  const step = (endVal - startVal) / (len - 1);
+  for (let i = 0; i < len; i++) {
     const val = Math.round(startVal + i * step);
     ints.push(val);
   }
   return ints;
 }
 
-// creates an array of values that base +/- noise, with arrLen steps
-function noisyPlateau(base: number, noise: number, arrLen: number): number[] {
+// creates an array of len values, where each is base +/- noise
+function noisyPlateau(base: number, noise: number, len: number): number[] {
   const ints = [];
-  for (let i = 0; i < arrLen; i++) {
+  for (let i = 0; i < len; i++) {
     const val = base + (Math.random() * noise * 2) - noise;
     ints.push(Math.round(val));
   }
@@ -32,19 +32,12 @@ const slopeDownD = fromTo(140, 40, 3);
 const holdClosed = noisyPlateau(440, 50, 180);
 const holdOpen = noisyPlateau(40, 0, 30);
 
-
 export const demoStreams = {
   "emgLongHold": [
     ...holdOpen,
-    ...slopeUpA,
-    ...slopeUpB,
-    ...slopeUpC,
-    ...slopeUpD,
+    ...slopeUpA, ...slopeUpB, ...slopeUpC, ...slopeUpD,
     ...holdClosed, ...holdClosed,
-    ...slopeDownA,
-    ...slopeDownB,
-    ...slopeDownC,
-    ...slopeDownD,
+    ...slopeDownA, ...slopeDownB, ...slopeDownD, ...slopeDownC,
     ...holdOpen
   ],
   "fsrSqueeze": [2,2,2,54,96,136,190,249,377,640,888,1023,
