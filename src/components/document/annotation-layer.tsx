c0ccd6c4efeeaaed2069dfe648852f0ed28788c3
Teale Fristoe <fristoe@gmail.com>
2023-08-21 10:57:25 -0700

    Enable default offsets for sparrow ends and set default for table cells to be top center.

--- src/components/document/annotation-layer.tsx
+++ src/components/document/annotation-layer.tsx
@@ -45,8 +45,6 @@ export const AnnotationLayer = observer(function AnnotationLayer({
     }
   };
 
-  const rowIds = content?.rowOrder || [];
-
   function getObjectBoundingBox(
     rowId: string, tileId: string, objectId: string, objectType?: string
   ) {
@@ -88,6 +86,20 @@ export const AnnotationLayer = observer(function AnnotationLayer({
   const sourceBoundingBox = sourceTileId && sourceObjectId
     ? getObjectBoundingBoxUnknownRow(sourceTileId, sourceObjectId, sourceObjectType)
     : undefined;
+  function defaultOffset(tileId?: string, objectId?: string, objectType?: string) {
+    return (tileId && objectId
+      ? tileApiInterface?.getTileApi(tileId)?.getObjectDefaultOffsets?.(objectId, objectType)
+      : undefined)
+      ?? OffsetModel.create({});
+  }
+  const sourceOffset = defaultOffset(sourceTileId, sourceObjectId, sourceObjectType);
+
+  const previewArrowSourceX = sourceBoundingBox && sourceOffset
+    ? sourceBoundingBox.left + sourceBoundingBox.width / 2 + sourceOffset.dx
+    : undefined;
+  const previewArrowSourceY = sourceBoundingBox && sourceOffset
+    ? sourceBoundingBox.top + sourceBoundingBox.height / 2 + sourceOffset.dy
+    : undefined;
 
   const handleAnnotationButtonClick = (tileId: string, objectId: string, objectType?: string) => {
     if (!sourceBoundingBox) {
@@ -106,6 +118,7 @@ export const AnnotationLayer = observer(function AnnotationLayer({
         ClueObjectModel.create({ tileId: sourceTileId, objectId: sourceObjectId, objectType: sourceObjectType });
       const targetObject = ClueObjectModel.create({ tileId, objectId, objectType });
       const targetBoundingBox = getObjectBoundingBoxUnknownRow(tileId, objectId, objectType);
+      const targetOffset = defaultOffset(tileId, objectId, objectType);
       let textOffset;
       if (targetBoundingBox) {
         const sourceX = sourceBoundingBox.left + sourceBoundingBox.width / 2;
@@ -115,7 +128,7 @@ export const AnnotationLayer = observer(function AnnotationLayer({
         const { peakDx, peakDy } = getDeafultPeak(sourceX, sourceY, targetX, targetY);
         textOffset = OffsetModel.create({ dx: peakDx, dy: peakDy });
       }
-      const newArrow = ArrowAnnotation.create({ sourceObject, targetObject, textOffset });
+      const newArrow = ArrowAnnotation.create({ sourceObject, sourceOffset, targetObject, targetOffset, textOffset });
       newArrow.setIsNew(true);
       content?.addArrow(newArrow);
       setSourceTileId("");
@@ -128,6 +141,7 @@ export const AnnotationLayer = observer(function AnnotationLayer({
     return getObjectBoundingBoxUnknownRow(object.tileId, object.objectId, object.objectType);
   };
 
+  const rowIds = content?.rowOrder || [];
   const editing = ui.annotationMode !== undefined;
   const hidden = !ui.showAnnotations;
   const classes = classNames("annotation-layer", { editing, hidden });
@@ -184,8 +198,8 @@ export const AnnotationLayer = observer(function AnnotationLayer({
           );
         })}
         <PreviewArrow
-          sourceX={sourceBoundingBox ? sourceBoundingBox.left + sourceBoundingBox.width / 2 : undefined}
-          sourceY={sourceBoundingBox ? sourceBoundingBox.top + sourceBoundingBox.height / 2 : undefined}
+          sourceX={previewArrowSourceX}
+          sourceY={previewArrowSourceY}
           targetX={mouseX}
           targetY={mouseY}
         />
