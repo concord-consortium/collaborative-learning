6aae6751b1cb96e0e1227209d815fc0c40866ce5
Teale Fristoe <fristoe@gmail.com>
2023-08-23 08:11:06 -0700

    185782620 Bound New Sparrows To Document (#1893)
    * Bound the peak of a new sparrow to the document.

--- src/components/annotations/preview-arrow.tsx
+++ src/components/annotations/preview-arrow.tsx
@@ -5,21 +5,28 @@ import { getDefaultPeak } from "./annotation-utilities";
 import { CurvedArrow } from "./curved-arrow";
 
 interface IPreviewArrowProps {
+  documentHeight: number;
+  documentWidth: number;
   sourceX?: number;
   sourceY?: number;
   targetX?: number;
   targetY?: number;
 }
-export function PreviewArrow({ sourceX, sourceY, targetX, targetY }: IPreviewArrowProps) {
+export function PreviewArrow({
+  documentHeight, documentWidth, sourceX, sourceY, targetX, targetY
+}: IPreviewArrowProps) {
   if (sourceX !== undefined && sourceY !== undefined && targetX !== undefined && targetY !== undefined) {
     const { peakX, peakY } = getDefaultPeak(sourceX, sourceY, targetX, targetY);
+    // Bound the peak to the document
+    const _peakX = Math.max(0, Math.min(documentWidth, peakX));
+    const _peakY = Math.max(0, Math.min(documentHeight, peakY));
 
     return (
       <>
         <CurvedArrow
           className="preview-arrow"
-          peakX={peakX}
-          peakY={peakY}
+          peakX={_peakX}
+          peakY={_peakY}
           sourceX={sourceX}
           sourceY={sourceY}
           targetX={targetX}
