632a7fa1c22f49f8c8ad8b98251fee004f85c857
Joe Bacal <bacalj@gmail.com>
2023-08-30 10:30:54 -0400

    comment

--- src/plugins/graph/components/graph.tsx
+++ src/plugins/graph/components/graph.tsx
@@ -110,7 +110,7 @@ export const Graph = observer(function Graph({graphController, graphRef, dotsRef
       treatAs === 'numeric' && graphModel.showAdornment(connectingLines, "Connecting Lines");
     }
 
-    // TODO: use isVisible state, set above, instead of this hack (see comments in GitHub commit)
+    // TODO: use isVisible state, set above, instead of this hack
     if (!connectingLines?.isVisible){
       const dotArea = select(dotsRef.current);
       const anyFoundPath = dotArea.selectAll("path");
