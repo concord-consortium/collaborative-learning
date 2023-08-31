23243e956660038bd2186af3c9fa0ed64a8b0de3
Joe Bacal <bacalj@gmail.com>
2023-08-31 09:00:34 -0400

    wip calculate image and width above

--- src/components/tiles/table/cell-formatter.tsx
+++ src/components/tiles/table/cell-formatter.tsx
@@ -6,21 +6,53 @@ import { useNumberFormat } from "./use-number-format";
 import './cell-formatter.scss';
 
 export const formatValue = (
-    formatter: (n: number | { valueOf(): number }) => string, value: any,
-    width?: number, row?: TRow, rowHeight?: (args: any) => number
-  ) => {
-  if ((value == null) || (value === "")) return <span></span>;
-  const num = Number(value);
-  if (!isFinite(num)) {
-    const cellWidth = (width || kDefaultColumnWidth) - kCellHorizontalPadding;
-    const height = rowHeight && row ? rowHeight({ row }) : kRowHeight;
-    return (
-      <div className="text-cell" style={{ height, width: cellWidth }}>
-        {value}
-      </div>
-    );
+  formatter: (n: number | { valueOf(): number }) => string,
+  value: any,
+  width?: number,
+  row?: TRow,
+  rowHeight?: (args: any) => number,
+  isImg?: boolean
+) => {
+
+  // empty value
+  if (value == null || value === "") {
+    return <span></span>;
   }
-  return <span>{formatter(num)}</span>;
+
+  // string or number value
+  else {
+    const num = Number(value);
+    // string value that may be an image url
+    if (!isFinite(num)) {
+
+      // image url
+      const regex = /^ccimg:\/\//;
+      const isReallyUrl = regex.test(value);
+
+      const cellWidth = (width || kDefaultColumnWidth) - kCellHorizontalPadding;
+      const height = rowHeight && row ? rowHeight({ row }) : kRowHeight;
+
+      // POC - problem is the cell height is still being calculated based on the length of the actual value
+      if (isReallyUrl){
+        return (
+          <div>
+            <img src="https://placehold.co/600x400" height="30"></img>
+          </div>
+        );
+      }
+
+      // not image url, just a string
+      return (
+        <div className="text-cell" style={{ height, width: cellWidth }}>
+          {value}
+        </div>
+      );
+    }
+    // number value
+    else {
+      return <span>{formatter(num)}</span>;
+    }
+  }
 };
 
 interface CellFormatterProps {
