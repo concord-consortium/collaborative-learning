54bbc664fd8445ca745faa49e151bd7c159f7acb
Joe Bacal <bacalj@gmail.com>
2023-09-16 21:52:27 -0400

    types but not quite what we want

--- src/hooks/use-tile-data-merging.ts
+++ src/hooks/use-tile-data-merging.ts
@@ -5,6 +5,7 @@ import { useMergeTileDialog } from "./use-merge-data-dialog";
 import { mergeTwoDataSets } from "../models/data/data-set-utils";
 import { safeJsonParse } from "../utilities/js-utils";
 import { getSnapshot } from "@concord-consortium/mobx-state-tree";
+import { IDataSet, IDataSetSnapshot } from "../models/data/data-set";
 
 interface IProps {
   documentId?: string;
@@ -13,13 +14,13 @@ interface IProps {
 
 export const useTileDataMerging = ({documentId, model}: IProps) => {
    // TODO - typing, stringifying, find the right pattern to follow
-  const localSharedModels = model.content.tileEnv?.sharedModelManager?.getSharedModelsByType("SharedDataSet") || [];
-  const mergableTiles = localSharedModels.filter((m) => (m as any).providerId !== model.id);
+  const localDataSets = model.content.tileEnv?.sharedModelManager?.getSharedModelsByType("SharedDataSet") || [];
+  const mergableTiles = localDataSets.filter((m) => (m as any).providerId !== model.id);
 
   const mergeTileFunc = useCallback((selectedTile: ITileLinkMetadata) => {
-    const sourceDataSnapshot = safeJsonParse(JSON.stringify((selectedTile))).dataSet;
+    const sourceDataSnapshot = safeJsonParse(JSON.stringify((selectedTile))).dataSet as IDataSetSnapshot;
     const localSharedModel = model.content.tileEnv?.sharedModelManager?.getTileSharedModels(model.content)[0] as any;
-    const targetDataSet = localSharedModel?.dataSet;
+    const targetDataSet = localSharedModel?.dataSet as IDataSet;
     if (sourceDataSnapshot && targetDataSet) {
       mergeTwoDataSets(sourceDataSnapshot, targetDataSet);
     }
