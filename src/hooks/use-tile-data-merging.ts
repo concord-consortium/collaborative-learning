e0af450ee63afdb7f94c8633bdcc15c80921dd99
Joe Bacal <bacalj@gmail.com>
2023-09-17 13:54:07 -0400

    simpler

--- src/hooks/use-tile-data-merging.ts
+++ src/hooks/use-tile-data-merging.ts
@@ -3,8 +3,7 @@ import {ITileLinkMetadata } from "../models/tiles/tile-link-types";
 import { ITileModel } from "../models/tiles/tile-model";
 import { useMergeTileDialog } from "./use-merge-data-dialog";
 import { mergeTwoDataSets } from "../models/data/data-set-utils";
-import { safeJsonParse } from "../utilities/js-utils";
-import { IDataSet, IDataSetSnapshot } from "../models/data/data-set";
+import { IDataSet } from "../models/data/data-set";
 import { getSharedDataSets } from "../models/shared/shared-data-utils";
 import { SharedDataSetType } from "../models/shared/shared-data-set";
 import { getSnapshot } from "@concord-consortium/mobx-state-tree";
@@ -18,15 +17,6 @@ function getMergables(model: ITileModel) {
   return docDataSets.filter((m) => (m as SharedDataSetType).providerId !== model.id);
 }
 
-function getSourceSnap(selectedTile: ITileLinkMetadata): IDataSetSnapshot {
-  const justTheDataSet = selectedTile.dataSet as IDataSet;
-  const justDataSnapped = getSnapshot(justTheDataSet);
-  const stringedDataSet = JSON.stringify(selectedTile.dataSet);
-  const parsedDataSet = safeJsonParse(stringedDataSet) as IDataSetSnapshot;
-  console.log("parsedDataSet: ", parsedDataSet, "vs justDataSnapped: ", justDataSnapped);
-  return justDataSnapped; //parsedDataSet;
-}
-
 function getTargetDataSet(model: ITileModel): IDataSet | undefined {
   const manager = model.content.tileEnv?.sharedModelManager;
   const targetModel = manager?.getTileSharedModels(model.content)[0] as SharedDataSetType;
@@ -37,7 +27,7 @@ export const useTileDataMerging = ({model}: IProps) => {
   const mergableTiles = getMergables(model);
 
   const mergeTileFunc = useCallback((selectedTile: ITileLinkMetadata) => {
-    const sourceSnap = getSourceSnap(selectedTile);
+    const sourceSnap = getSnapshot(selectedTile.dataSet as IDataSet);
     const targetDataSet = getTargetDataSet(model);
     if (sourceSnap && targetDataSet) {
       mergeTwoDataSets(sourceSnap, targetDataSet);
