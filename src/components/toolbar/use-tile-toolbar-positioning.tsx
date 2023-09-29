d55f1c220a12b1bf0a6b301a903d4d3d419c7e23
Boris Goldowsky <bgoldowsky@concord.org>
2023-09-29 07:53:59 -0400

    Adjust placement strategy

--- src/components/toolbar/use-tile-toolbar-positioning.tsx
+++ src/components/toolbar/use-tile-toolbar-positioning.tsx
@@ -1,15 +1,19 @@
-import { useFloating, autoUpdate, flip, offset, shift } from "@floating-ui/react";
+import { useFloating, autoUpdate, offset, flip, shift } from "@floating-ui/react";
 
 import "./toolbar.scss";
 
 export function useTileToolbarPositioning(tileElement: HTMLElement|null) {
 
+  const canvas = tileElement?.closest('.canvas') || 'clippingAncestors';
+
   const { refs, placement, floatingStyles } = useFloating({
     open: true,
     placement: "bottom-start",
     whileElementsMounted: autoUpdate,
-    // Offset accounts for margin width. Shift and flip attempt to keep toolbar in the viewport.
-    middleware: [offset({ mainAxis: -2}), flip(), shift()],
+    // Offset accounts for margin width. Shift and flip keep toolbar in the viewport.
+    middleware: [offset({ mainAxis: -2}),
+      flip({mainAxis: true, crossAxis: true, fallbackStrategy: 'initialPlacement', boundary: canvas}),
+      shift({crossAxis: true})],
     elements: {
       reference: tileElement
     }
