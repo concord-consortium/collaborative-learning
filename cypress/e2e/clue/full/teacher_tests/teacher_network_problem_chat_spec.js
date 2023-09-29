3ceee71bcacef63fdd5d3fbb36f4c3811249cfb6
Saravanan Udayakumar <saravanan.udayakumar@tscube.com>
2023-09-29 16:36:06 +0530

    Migrate Automated Test To New Portal Staging

--- cypress/e2e/clue/full/teacher_tests/teacher_network_problem_chat_spec.js
+++ cypress/e2e/clue/full/teacher_tests/teacher_network_problem_chat_spec.js
@@ -1,4 +1,5 @@
 import ChatPanel from "../../../../support/elements/clue/ChatPanel";
+import TeacherDashboard from "../../../../support/elements/clue/TeacherDashboard";
 
 /**
  * Notes:
@@ -14,29 +15,40 @@ import ChatPanel from "../../../../support/elements/clue/ChatPanel";
  */
 
 let chatPanel = new ChatPanel;
+let dashboard = new TeacherDashboard();
 
-const portalUrl = "https://learn.staging.concord.org";
-const offeringId1 = "2000";
-const offeringId2 = "2004";
-const reportUrl1 = "https://learn.staging.concord.org/portal/offerings/" + offeringId1 + "/external_report/49";
-const reportUrl2 = "https://learn.staging.concord.org/portal/offerings/" + offeringId2 + "/external_report/49";
+const portalUrl = "https://learn.portal.staging.concord.org";
+const offeringId1 = "221";
+const offeringId2 = "226";
+const reportUrl1 = "https://learn.portal.staging.concord.org/portal/offerings/" + offeringId1 + "/external_report/11";
+const reportUrl2 = "https://learn.portal.staging.concord.org/portal/offerings/" + offeringId2 + "/external_report/11";
 const clueTeacher1 = {
-  username: "TejalTeacher1",
-  password: "ccpassword",
-  firstname: "Tejal",
+  username: "clueteachertest1",
+  password: "password",
+  firstname: "Clue",
   lastname: "Teacher1"
 };
 const clueTeacher2 = {
-  username: "TejalTeacher2",
-  password: "ccpassword",
-  firstname: "Tejal",
+  username: "clueteachertest2",
+  password: "password",
+  firstname: "Clue",
   lastname: "Teacher2"
 };
 
+function beforeTest(portalUrl, clueTeacher, reportUrl) {
+  cy.login(portalUrl, clueTeacher);
+  cy.launchReport(reportUrl);
+  cy.waitForLoad();
+  dashboard.switchView("Workspace & Resources");
+  chatPanel.getChatPanelToggle().click();
+  cy.wait(4000);
+}
+
 describe('Teachers can communicate back and forth in chat panel', () => {
   // TODO: Re-instate the skipped tests below once learn.staging.concord.org is fully functional again
-  it.skip("verify teacher1 can add comments in Problem tab documents and tiles", ()=> {
-    chatPanel.openTeacherChat(portalUrl, clueTeacher1, reportUrl1);
+  it("verify teacher1 can add comments in Problem tab documents and tiles", ()=> {
+    beforeTest(portalUrl, clueTeacher1, reportUrl1);
+    // chatPanel.openTeacherChat(portalUrl, clueTeacher1, reportUrl1);
     cy.openTopTab("problems");
     cy.openProblemSection("Introduction");
     cy.wait(1000);
@@ -48,8 +60,8 @@ describe('Teachers can communicate back and forth in chat panel', () => {
     cy.wait(1000);
     chatPanel.addCommentAndVerify("This is a teacher1 problem tile comment");
   });
-  it.skip("verify teacher2 can view teacher1's comments and add more comments in Problem tab", () => {
-    chatPanel.openTeacherChat(portalUrl, clueTeacher2, reportUrl2);
+  it("verify teacher2 can view teacher1's comments and add more comments in Problem tab", () => {
+    beforeTest(portalUrl, clueTeacher2, reportUrl2);
     cy.openTopTab("problems");
     cy.openProblemSection("Introduction");
     cy.wait(2000);
@@ -64,8 +76,8 @@ describe('Teachers can communicate back and forth in chat panel', () => {
     chatPanel.verifyCommentThreadContains("This is a teacher1 problem tile comment");
     chatPanel.addCommentAndVerify("This is a teacher2 problem tile comment");
   });
-  it.skip("verify teacher1 can view teacher2's comments in Problem tab", () => {
-    chatPanel.openTeacherChat(portalUrl, clueTeacher1, reportUrl1);
+  it("verify teacher1 can view teacher2's comments in Problem tab", () => {
+    beforeTest(portalUrl, clueTeacher1, reportUrl1);
     cy.openTopTab("problems");
     cy.openProblemSection("Introduction");
     cy.wait(2000);
@@ -78,30 +90,35 @@ describe('Teachers can communicate back and forth in chat panel', () => {
     cy.wait(2000);
     chatPanel.verifyCommentThreadContains("This is a teacher2 problem tile comment");
   });
-    //TODO: verify delete is disabled for now until work is merged to master, but keep the delete to clean up chat space
-  it.skip('verify teacher1 can only delete own comments', () => {
-    cy.get(".user-name").contains("Tejal Teacher2").siblings("[data-testid=delete-message-button]").should("not.exist");
-    cy.get(".user-name").contains("Tejal Teacher1").siblings("[data-testid=delete-message-button]").click();
-    cy.get(".confirm-delete-alert button").contains("Delete").click();
-    // chatPanel.getCommentFromThread().should("not.contain", "This is a teacher1 problem tile comment");
-    cy.openProblemSection("Introduction");
-    cy.get(".user-name").contains("Tejal Teacher2").siblings("[data-testid=delete-message-button]").should("not.exist");
-    cy.get(".user-name").contains("Tejal Teacher1").siblings("[data-testid=delete-message-button]").click();
-    cy.get(".confirm-delete-alert button").contains("Delete").click();
-    // chatPanel.getCommentFromThread().should("not.contain", "This is a teacher1 problem document comment");
-  });
-  it.skip('verify teacher2 does not see teacher1 deleted comments', () => {
-    chatPanel.openTeacherChat(portalUrl, clueTeacher2, reportUrl2);
+  //TODO: verify delete is disabled for now until work is merged to master, but keep the delete to clean up chat space
+  it('verify teacher1 can only delete own comments', () => {
+    beforeTest(portalUrl, clueTeacher1, reportUrl1);
     cy.openTopTab("problems");
     cy.openProblemSection("Introduction");
-    // chatPanel.getCommentFromThread().should("not.contain", "This is a teacher1 problem document comment");
-    cy.get(".user-name").contains("Tejal Teacher2").siblings("[data-testid=delete-message-button]").click();
+    cy.wait(2000);
+    cy.get(".user-name").contains("Clue Teacher2").siblings("[data-testid=delete-message-button]").should("not.exist");
+    cy.get(".user-name").contains("Clue Teacher1").siblings("[data-testid=delete-message-button]").click();
     cy.get(".confirm-delete-alert button").contains("Delete").click();
+    chatPanel.getCommentFromThread().should("not.contain", "This is a teacher1 problem tile comment");
+    cy.clickProblemResourceTile('introduction');
+    cy.get(".user-name").contains("Clue Teacher2").siblings("[data-testid=delete-message-button]").should("not.exist");
+    cy.get(".user-name").contains("Clue Teacher1").siblings("[data-testid=delete-message-button]").click();
+    cy.get(".confirm-delete-alert button").contains("Delete").click();
+    chatPanel.getCommentFromThread().should("not.contain", "This is a teacher1 problem document comment");
+  });
+  it('verify teacher2 does not see teacher1 deleted comments', () => {
+    beforeTest(portalUrl, clueTeacher2, reportUrl2);
+    cy.openTopTab("problems");
+    cy.openProblemSection("Introduction");
+    chatPanel.getCommentFromThread().should("not.contain", "This is a teacher1 problem document comment");
+    cy.get(".user-name").contains("Clue Teacher2").siblings("[data-testid=delete-message-button]").click();
+    cy.get(".confirm-delete-alert button").contains("Delete").click();
+    chatPanel.getCommentFromThread().should("not.exist");
     // chatPanel.getCommentFromThread().should("not.contain", "This is a teacher2 problem document comment");
     cy.clickProblemResourceTile('introduction');
-    // chatPanel.getCommentFromThread().should("not.contain", "This is a teacher1 problem tile comment");
-    cy.get(".user-name").contains("Tejal Teacher2").siblings("[data-testid=delete-message-button]").click();
+    chatPanel.getCommentFromThread().should("not.contain", "This is a teacher1 problem tile comment");
+    cy.get(".user-name").contains("Clue Teacher2").siblings("[data-testid=delete-message-button]").click();
     cy.get(".confirm-delete-alert button").contains("Delete").click();
-    // chatPanel.getCommentFromThread().should("not.exist");
+    chatPanel.getCommentFromThread().should("not.exist");
   });
 });
