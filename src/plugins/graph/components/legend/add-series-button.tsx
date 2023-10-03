bbfa4be73c88ed01d76d1c40e9a13d6e192d7f44
Boris Goldowsky <bgoldowsky@concord.org>
2023-10-03 13:07:56 -0400

    Add new series using setAttributeID

--- src/plugins/graph/components/legend/add-series-button.tsx
+++ src/plugins/graph/components/legend/add-series-button.tsx
@@ -1,27 +1,22 @@
 import { observer } from "mobx-react";
 import React from "react";
-import { useDataConfigurationContext } from "../../hooks/use-data-configuration-context";
-import { IAttributeDescriptionSnapshot } from "../../models/data-configuration-model";
+import { useGraphModelContext } from "../../models/graph-model";
 
 export const AddSeriesButton = observer(function AddSeriesButton() {
-  const dataConfiguration = useDataConfigurationContext();
+  const graphModel = useGraphModelContext();
+  const dataConfiguration = graphModel.config;
   console.log("----<AddSeriesButton>--------");
   console.log("🚀 ~ file: add-series-button.tsx:8 ~ AddSeriesButton ~ dataConfiguration:", dataConfiguration);
 
   function handleClick() {
     console.log("🚀 ~ file: add-series-button.tsx:12 ~ handleClick ~ handleClick:", handleClick);
     // Find first unplotted attribute
-    if (!dataConfiguration || !dataConfiguration.dataset) return;
+    if (!graphModel || !dataConfiguration || !dataConfiguration.dataset) return;
     const datasetAttributes = dataConfiguration.dataset.attributes;
     const plottedAttributeIds = dataConfiguration.uniqueAttributes;
     const first = datasetAttributes.find((attr) => !plottedAttributeIds.includes(attr.id));
     if (first) {
-      console.log('Adding first unused attribute: ', first.name, first.id);
-      const toAdd: IAttributeDescriptionSnapshot = {
-        attributeID: first.id
-      };
-      dataConfiguration.addYAttribute(toAdd);
-      console.log('After add, y attributes are: ', dataConfiguration.yAttributeDescriptions);
+      graphModel.setAttributeID("yPlus", dataConfiguration.dataset.id, first.id);
     } else {
       console.log('No attributes are unplotted');
     }
