847a82aef282a4a7c90066215956712f9859deca
Teale Fristoe <fristoe@gmail.com>
2023-10-10 14:52:54 -0700

    185466012 Allow Units to Specify a Default Simulation (#1987)
    * Use tile setting for defaultSimulation.

--- src/plugins/simulator/simulations/simulations.ts
+++ src/plugins/simulator/simulations/simulations.ts
@@ -2,6 +2,9 @@ import { brainwavesGripperSimulation, kBrainwavesKey } from "./brainwaves-grippe
 import { kTerrariumKey, terrariumSimulation } from "./terrarium/terrarium";
 import { ISimulation } from "./simulation-types";
 
+// The simulation to use when none is specified.
+export const defaultSimulationKey = kBrainwavesKey;
+
 export const simulations: Record<string, ISimulation> = {
   [kBrainwavesKey]: brainwavesGripperSimulation,
   [kTerrariumKey]: terrariumSimulation
