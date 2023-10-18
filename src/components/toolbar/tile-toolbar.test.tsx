1eec4351d84f9c61d53d8a0369fea00b03d03c78
Boris Goldowsky <bgoldowsky@concord.org>
2023-10-18 16:09:00 -0400

    Add app config test

--- src/components/toolbar/tile-toolbar.test.tsx
+++ src/components/toolbar/tile-toolbar.test.tsx
@@ -1,18 +1,20 @@
 import React, { useRef } from "react";
+import { Provider } from "mobx-react";
 import { render, screen } from "@testing-library/react";
 import { TileToolbarButton } from "./tile-toolbar-button";
 import { registerTileToolbarButtons, registerTileToolbarConfig } from "./toolbar-button-manager";
-import CopyIcon from "../../../assets/icons/copy/copy-icon-default.svg";
 import { TileToolbar } from "./tile-toolbar";
 import { ITileModel, TileModel } from "../../models/tiles/tile-model";
 import { defaultTextContent } from "../../models/tiles/text/text-content";
+import { specStores } from "../../models/stores/spec-stores";
+import { TileModelContext } from "../tiles/tile-api";
+
+import CopyIcon from "../../../assets/icons/copy/copy-icon-default.svg";
 
 // The text tile needs to be registered so the TileModel.create
 // knows it is a supported tile type
 import "../../models/tiles/text/text-registration";
-import { specStores } from "../../models/stores/spec-stores";
-import { Provider } from "mobx-react";
-import { TileModelContext } from "../tiles/tile-api";
+import { specAppConfig } from "../../models/stores/spec-app-config";
 
 const clickHandler = jest.fn();
 
@@ -20,18 +22,32 @@ function SampleToolbarButton() {
   return <TileToolbarButton onClick={clickHandler}><CopyIcon/></TileToolbarButton>;
 }
 
+const sampleButtons = [
+  {
+    name: "default",
+    title: "Default Button",
+    component: SampleToolbarButton,
+  },
+  {
+    name: "non-default",
+    title: "Non-default button",
+    component: SampleToolbarButton,
+  }
+];
+
 interface ISampleTileProps {
-  model: ITileModel
+  type: string;
+  model: ITileModel;
 }
 
-function SampleTile({model}: ISampleTileProps) {
+function SampleTile({type, model}: ISampleTileProps) {
   const tileElt = useRef<HTMLDivElement>(null);
   return (
     <TileModelContext.Provider value={model}>
       <div ref={tileElt}>
         Tile content.
       </div>
-      <TileToolbar readOnly={false} tileElement={tileElt.current} tileType="test" />
+      <TileToolbar readOnly={false} tileElement={tileElt.current} tileType={type} />
     </TileModelContext.Provider>
   );
 }
@@ -52,29 +68,45 @@ describe("Tile toolbar button", () => {
     const stores = specStores();
     stores.ui.setSelectedTileId(model.id);
 
-    registerTileToolbarButtons("test",
-    [
-      {
-        name: "default",
-        title: "Default Button",
-        component: SampleToolbarButton,
-      },
-      {
-        name: "non-default",
-        title: "Non-default button",
-        component: SampleToolbarButton,
-      }
-    ]);
+    registerTileToolbarButtons("test", sampleButtons);
     registerTileToolbarConfig("test", ["default"]);
 
     render(
       <Provider stores={stores}>
-        <SampleTile model={model}/>
+        <SampleTile type="test" model={model}/>
       </Provider>
     );
     expect(screen.getByTestId("tile-toolbar")).toBeInTheDocument();
     expect(screen.getByTestId("tile-toolbar")).toContainHTML("Default Button");
     expect(screen.getByTestId("tile-toolbar")).not.toContainHTML("Non-default Button");
+  });
+
+  it("can read tools list from app configuration", () => {
+    const model = TileModel.create({content: defaultTextContent()});
+    const stores = specStores({
+      appConfig: specAppConfig({
+        config: {
+          settings: {
+            test: {
+              tools: ["default"]
+            }
+          }
+        }
+        })
+      });
+    stores.ui.setSelectedTileId(model.id);
+
+    registerTileToolbarButtons("test", sampleButtons);
+
+    render(
+      <Provider stores={stores}>
+        <SampleTile type="test" model={model}/>
+      </Provider>
+    );
+    expect(screen.getByTestId("tile-toolbar")).toBeInTheDocument();
+    expect(screen.getByTestId("tile-toolbar")).toContainHTML("Default Button");
+    expect(screen.getByTestId("tile-toolbar")).not.toContainHTML("Non-default Button");
+
   });
 
 });
