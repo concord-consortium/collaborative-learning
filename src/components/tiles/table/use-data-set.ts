94e33d48b3c59e4f6407350054d145e5f997e801
Teale Fristoe <fristoe@gmail.com>
2023-11-13 13:12:41 -0800

    Clean up.

--- src/components/tiles/table/use-data-set.ts
+++ src/components/tiles/table/use-data-set.ts
@@ -38,7 +38,9 @@ export const useDataSet = ({
     if (dataSet.selectedCells.length === 1) {
       const _selectedCell = dataSet.selectedCells[0];
       selectedCellIndecies.selectedCellColumnIndex = dataSet.attrIndexFromID(_selectedCell.attributeId) ?? -1;
-      selectedCellIndecies.selectedCellRowIndex = dataSet.caseIndexFromID(_selectedCell.caseId);
+      selectedCellIndecies.selectedCellRowIndex = _selectedCell.caseId === inputRowId.current
+        ? rows.length - 1
+        : dataSet.caseIndexFromID(_selectedCell.caseId);
     }
     return selectedCellIndecies;
   }
@@ -46,20 +48,16 @@ export const useDataSet = ({
     // Only modify the selection if a single cell is selected
     if (dataSet.selectedCells.length !== 1) return;
     const { selectedCellColumnIndex, selectedCellRowIndex } = getSelectedCellIndicies();
-    // if (selectedCellColumnIndex === -1 && selectedCellRowIndex === -1) return;
-    // if (dataSet.selectedCells.length !== 1) return;
 
     // Determine if we're moving forwards or backwards
-    // const _selectedCell = dataSet.selectedCells[0];
-    // const selectedCellRowIndex = dataSet.caseIndexFromID(_selectedCell.caseId);
-    // const selectedCellColumnIndex = dataSet.attrIndexFromID(_selectedCell.attributeId) ?? -1;
     const forward = (selectedCellRowIndex < position.rowIdx) ||
       (selectedCellRowIndex === position.rowIdx && selectedCellColumnIndex < position.idx);
 
     // Set the dataSet's selected cell
-    const newRowId = dataSet.caseIDFromIndex(position.rowIdx);
+    const newRowId = position.rowIdx === rows.length - 1
+      ? inputRowId.current
+      : dataSet.caseIDFromIndex(position.rowIdx);
     const newColumnId = dataSet.attrIDFromIndex(position.idx - 1);
-    console.log(`@@@ newRowId`, newRowId);
     if (newColumnId && newRowId
       && !(selectedCellColumnIndex === position.idx - 1 && selectedCellRowIndex === position.rowIdx)
     ) {
@@ -67,15 +65,12 @@ export const useDataSet = ({
       triggerRowChange();
     }
 
+    // Update the position if it's not a legal option (if we're in the control or delete column).
+    // Note that rdg will not allow us to move to a row outside of the grid
     let newPosition = { ...position };
     while(!isCellSelectable(newPosition, columns, readOnly)) {
       if (forward) {
-        if (newPosition.rowIdx >= rows.length - 1) {
-          // If we've moved down past the last row, go back to the top
-          // TODO Should we create a new row in this case?
-          // onAddRows([{}]);
-          // newPosition.rowIdx = rows.length - 2;
-        } else if (newPosition.rowIdx === rows.length - 1 && newPosition.idx >= columns.length - 1) {
+        if (newPosition.rowIdx === rows.length - 1 && newPosition.idx >= columns.length - 1) {
           // move from last cell to { 0, 1 }
           newPosition = { rowIdx: 0, idx: 1 };
         } else if (++newPosition.idx >= columns.length) {
@@ -84,11 +79,7 @@ export const useDataSet = ({
           ++newPosition.rowIdx;
         }
       } else {
-        if (newPosition.rowIdx < 0) {
-          // If we've moved above the first row, go to the bottom
-          // NOTE This never happens because RDG won't let you select rows < 0
-          newPosition.rowIdx = rows.length - 1;
-        } else if (newPosition.rowIdx === 0 && newPosition.idx < 1) {
+        if (newPosition.rowIdx === 0 && newPosition.idx < 1) {
           // move from first cell to bottom right cell
           newPosition = { rowIdx: rows.length - 1, idx: columns.length };
         } else if (--newPosition.idx < 1) {
@@ -99,14 +90,13 @@ export const useDataSet = ({
       }
     }
 
+    // Update rdg if we changed the
     if ((newPosition.rowIdx !== position.rowIdx) || (newPosition.idx !== position.idx)) {
-      console.log(`!!! gridRef.selectCell`, newPosition);
       gridRef.current?.selectCell(newPosition);
     }
   };
 
   const getUpdatedRowAndColumn = (_rows?: TRow[], _columns?: TColumn[]) => {
-    console.log(`ooo getUpdatedRowAndColumn`, _rows);
     const rs = _rows ?? rows;
     const cs = _columns ?? columns;
     const selectedCellIndecies = getSelectedCellIndicies();
@@ -114,28 +104,20 @@ export const useDataSet = ({
     // If the row index is -1, assume we're adding to a new row at the bottom
     const selectedCellRowIndex = selectedCellIndecies.selectedCellRowIndex >= 0
       ? selectedCellIndecies.selectedCellRowIndex : rs.length - 1;
-    // const selectedCellRowIndex = selectedCell.current?.rowIdx;
-    // const selectedCellColumnIndex = selectedCell.current?.idx;
     const updatedRow = (selectedCellRowIndex != null) && (selectedCellRowIndex >= 0)
       ? rs[selectedCellRowIndex] : undefined;
-    console.log(`  o updatedRow`, updatedRow);
-    console.log(` oo selectedCellColumnIndex`, selectedCellColumnIndex);
     const updatedColumn = (selectedCellColumnIndex != null) && (selectedCellColumnIndex >= 0)
       ? cs[selectedCellColumnIndex + 1] : undefined;
-    console.log(`  o updatedColumn`, updatedColumn);
     return { selectedCellRowIndex, selectedCellColIndex: selectedCellColumnIndex, updatedRow, updatedColumn };
   };
 
   const formatter = useNumberFormat();
   const onRowsChange = (_rows: TRow[]) => {
-    console.log(`--- onRowsChange`);
     // for now, assume that all changes are single cell edits
     const { selectedCellRowIndex, updatedRow, updatedColumn } = getUpdatedRowAndColumn(_rows);
-    console.log(` -- updatedRow, updatedColumn`, updatedRow, updatedColumn);
     if (!readOnly && updatedRow && updatedColumn) {
       const originalValue = dataSet.getValue(updatedRow.__id__, updatedColumn.key);
       const originalStrValue = formatValue({ formatter, value: originalValue, lookupImage });
-      console.log(` -- updatedValue, originalValue`, updatedRow[updatedColumn.key], originalStrValue);
       // only make a change if the value has actually changed
       if (updatedRow[updatedColumn.key] !== originalStrValue) {
         const updatedCaseValues: ICase = {
@@ -144,12 +126,10 @@ export const useDataSet = ({
         };
         const inputRowIndex = _rows.findIndex(row => row.__id__ === inputRowId.current);
         if ((inputRowIndex >= 0) && (selectedCellRowIndex === inputRowIndex)) {
-          console.log(`  - adding row`, updatedCaseValues);
           onAddRows([updatedCaseValues]);
           inputRowId.current = uniqueId();
         }
         else {
-          console.log(`  - updating row`, updatedCaseValues);
           onUpdateRow(updatedCaseValues);
         }
       }
