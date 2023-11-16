f1fd565f18aea2db0f9bf2d595213348883b1c61
Tejal Shah <tejal.shah@tscube.com>
2023-11-16 18:05:16 -0500

    Fixing missing semi-colons

--- cypress/support/elements/clue/DataCardToolTile.js
+++ cypress/support/elements/clue/DataCardToolTile.js
@@ -134,16 +134,16 @@ class DataCardToolTile {
     return this.getToolbarButton('dataset-view-button', tileIndex, workspaceClass);
   }
   getDragHandle(cardIndex, workspaceClass, tileIndex = 0) {
-    const selector = ".sort-area-grid .cell.stack .drag-handle svg"
+    const selector = ".sort-area-grid .cell.stack .drag-handle svg";
     return this.getTile(tileIndex, workspaceClass).find(`${selector}`).eq(cardIndex);
   }
   getDropZone(cardIndex, workspaceClass, tileIndex = 0) {
-    const selector = ".sort-area-grid .cell.stack .stack-drop-zone.show-droppable"
+    const selector = ".sort-area-grid .cell.stack .stack-drop-zone.show-droppable";
     return this.getTile(tileIndex, workspaceClass).find(`${selector}`).eq(cardIndex);
   }
   dragCardToStack(dragCard, dropStack) {
     this.getDragHandle(dragCard).click()
-    .trigger("mousedown", {force:true})
+    .trigger("mousedown", {force:true});
     this.getDropZone(dropStack)
     .trigger('mousemove', {force:true })
     .trigger("mouseup", {force:true })
