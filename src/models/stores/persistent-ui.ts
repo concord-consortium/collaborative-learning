17d467de1491998709de9185a4bec20b1b585c21
Joseph Bacal <bacalj@gmail.com>
2023-12-05 14:34:54 -0500

    next steps, wip

--- src/models/stores/persistent-ui.ts
+++ src/models/stores/persistent-ui.ts
@@ -229,6 +229,11 @@ export const PersistentUIModel = types
       self.setOpenSubTab(navTab, subTab);
     },
     async initializePersistentUISync(user: UserModelType, db: DB){
+      // TASKS
+      // use one path, which will allow you to use .set instead of .update
+      // (we may still need two refs because we don't know if they get out of date? but probably just fine to have one)
+      // move the computation of the persistentUI path into db.ts where getOfferingUserPath is defined (make a sibling)
+      // it will be getPersistentUIPath
       const userPath = db.firebase.getOfferingUserPath(user);
       const uiPath = userPath + "/persistentUI";
       const getRef = db.firebase.ref(uiPath);
