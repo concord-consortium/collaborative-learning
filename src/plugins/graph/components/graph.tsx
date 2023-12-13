9bb133b2ff7db2aba85b9e39d61ce35a553e74ce
Teale Fristoe <fristoe@gmail.com>
2023-12-13 06:56:07 -0800

    Always show edit axis boxes.

--- src/plugins/graph/components/graph.tsx
+++ src/plugins/graph/components/graph.tsx
@@ -64,7 +64,6 @@ export const Graph = observer(
     svgRef = useRef<SVGSVGElement>(null),
     plotAreaSVGRef = useRef<SVGSVGElement>(null),
     backgroundSvgRef = useRef<SVGGElement>(null);
-  const showEditableGraphValue = graphModel.isLinkedToDataSet;
 
   useEffect(function setupPlotArea() {
     if (xScale && xScale?.length > 0) {
@@ -288,7 +287,6 @@ export const Graph = observer(
             />
           }
           {
-            showEditableGraphValue &&
             axes.map((axis: AxisPlace, idx) => {
               const axisModel = graphModel?.getAxis(axis);
               const minVal = isNumericAxisModel(axisModel) ? axisModel.min : kDefaultNumericAxisBounds[0];
