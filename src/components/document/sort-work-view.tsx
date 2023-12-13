ebded4c76fc08a4102f613ebf79d179ad9fdd902
Joseph Bacal <bacalj@gmail.com>
2023-12-13 14:28:13 -0500

    initial list of documents for rendering what is in store

--- src/components/document/sort-work-view.tsx
+++ src/components/document/sort-work-view.tsx
@@ -1,10 +1,26 @@
 import { observer } from "mobx-react";
+import { useStores } from "../../hooks/use-stores";
 import React from "react";
 
 export const SortWorkView:React.FC = observer(function SortWorkView(){
+  const stores = useStores();
+  const allDocuments = JSON.parse(JSON.stringify(stores.documents.all));
+
+
   return (
     <div key="sort-work-view">
-      get some work and make it sortable!
+      <ul>
+        <li>These are documents already in the store by default</li>
+        <li>The next step will be to gain access to all of them - getting them in the store first</li>
+        <li>So that - getting the correct keys in the store, will probably happen elsewhere</li>
+        <li>Then we can start to sort them</li>
+      </ul>
+      {allDocuments.map((doc: any) => {
+        return (
+          <li key={doc.key}>{doc.key} | {doc.type} | {doc.properties.pubCount } | user: { doc.uid} </li>
+        );
+      })}
+
     </div>
   );
 });
