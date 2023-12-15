2cb2574fc16f5b77c30ca90b24590622e63b8b4f
Joseph Bacal <bacalj@gmail.com>
2023-12-15 15:39:00 -0500

    cleanup

--- src/lib/db-listeners/db-problem-documents-listener.ts
+++ src/lib/db-listeners/db-problem-documents-listener.ts
@@ -19,25 +19,19 @@ export class DBProblemDocumentsListener extends BaseListener {
 
   public start() {
     const { user } = this.db.stores;
-    //console.log("|> 1 start has a single user, that is the logged in user:", user );
-
     // both teachers and students listen to all problem documents
     // but only teachers listen to all content.  students only listen
     // to content of users in their group to reduce network traffic
     return new Promise<void>((resolve, reject) => {
-      //console.log("|> 2 path is calculated to a ref that is all users in the offering:", this.db.firebase.getOfferingUsersPath(user));
       const offeringUsersRef = this.offeringUsersRef = this.db.firebase.ref(
         this.db.firebase.getOfferingUsersPath(user));
       this.debugLogHandler("#start", "adding", "once", offeringUsersRef);
       // once is called immediately, and will proceed to resolve promise even if there is no value
       offeringUsersRef.once("value")
         .then((snapshot) => {
-          //console.log("|> 3 that ref is onced to a snapshot", snapshot );
-          //console.log("|> 4 and that snapshot is passed to handleLoadOfferingUsersProblemDocuments...");
           this.handleLoadOfferingUsersProblemDocuments(snapshot);
           // We have to listen to both events because of a race condition of the documents
           // not being set when the child is added
-          console.log("|> 4(OJO!) we add listers for child_added and child_changed to the ref, basically telling them to handleOfferingUser again when things change",);
           this.debugLogHandlers("#start", "adding", ["child_added", "child_changed"], offeringUsersRef);
           offeringUsersRef.on("child_added",
             this.onLoadOfferingUserChildAdded = this.handleLoadOfferingUserAddedOrChanged("child_added"));
@@ -57,12 +51,9 @@ export class DBProblemDocumentsListener extends BaseListener {
     }
   }
 
-  // {{HANDLEDOCS}}
   private handleLoadOfferingUsersProblemDocuments = (snapshot: firebase.database.DataSnapshot) => {
-
     const { user: { id: selfUserId }, documents } = this.db.stores;
     const users: DBOfferingUserMap = snapshot.val();
-    //console.log("|> 5 handleLoadOfferingUsersProblemDocuments loops over and passes each of user metadata to handleOfferingUser:...", users);
     this.debugLogSnapshot("#handleLoadOfferingUsersProblemDocuments", snapshot);
     forEach(users, (user: DBOfferingUser) => {
       if (user) {
@@ -76,7 +67,6 @@ export class DBProblemDocumentsListener extends BaseListener {
     !currentUser?.self && documents.resolveRequiredDocumentPromisesWithNull([ProblemDocument, PlanningDocument]);
   };
 
-  // {{ HANDLEADDORCHANGE }}
   private handleLoadOfferingUserAddedOrChanged = (eventType: string) => (snapshot: firebase.database.DataSnapshot) => {
     const user: DBOfferingUser = snapshot.val();
     this.debugLogSnapshot(`#handleLoadOfferingUserAddedOrChanged (${eventType})`, snapshot);
@@ -85,8 +75,6 @@ export class DBProblemDocumentsListener extends BaseListener {
     }
   };
 
-  // --listener-- 5
-  // {{ MAINEVENT }}
   private handleOfferingUser = (user: DBOfferingUser) => {
     if (!user.self?.uid) return;
     const { documents, user: currentUser } = this.db.stores;
@@ -97,10 +85,7 @@ export class DBProblemDocumentsListener extends BaseListener {
       documents.resolveRequiredDocumentPromiseWithNull(ProblemDocument);
     }
 
-    //console.log("|> ... 6  handleOfferingUser takes user...", user.self.uid);
-    // console.log("|> ... 7 and loops over documents in store already:", documents);
     forEach(user.documents, document => {
-      //console.log("|> ... ...  8  it considers if doc is in store...and...passes it to appropriate db method", document.documentKey);
       if (!document?.documentKey || !document?.self?.uid) return;
       const existingDoc = documents.getDocument(document.documentKey);
       if (existingDoc) {
--- src/lib/db-listeners/db-student-personal-docs-listener.ts
+++ src/lib/db-listeners/db-student-personal-docs-listener.ts
@@ -1,20 +1,11 @@
 import firebase from "firebase/app";
-import { forEach, size } from "lodash";
 import { DB } from "../db";
-import { DBOfferingUser, DBOfferingUserMap } from "../db-types";
 import { OtherDocumentType, PersonalDocument } from "../../models/document/document-types";
 import { BaseListener } from "./base-listener";
 
 export class DBStudentPersonalDocsListener extends BaseListener {
   private db: DB;
   private offeringUsersRef: firebase.database.Reference | null  = null;
-  private documentType: OtherDocumentType; // not sure we'll need this
-  private onUserChildAdded: (snapshot: firebase.database.DataSnapshot) => void;
-  private onUserChildChanged: (snapshot: firebase.database.DataSnapshot) => void;
-  //private handlePersonalDocAdded: (snapshot: firebase.database.DataSnapshot) => void;
-  private relevantUsers: string[];
-  private usersDocumentMetadataPaths: string[];
-  private classPath: string;
 
   constructor(db: DB, documentType: OtherDocumentType) {
     super("DBStudentPersonalDocsListener");
@@ -28,29 +19,11 @@ export class DBStudentPersonalDocsListener extends BaseListener {
         this.db.firebase.getOfferingUsersPath(user)
       );
 
-      console.log("|| good path, for example: ", this.db.firebase.getOfferingUsersPath(user));
-
-      this.classPath = this.db.firebase.getClassPath(user);
-      // console.log("|| classPath:", this.classPath);
-
-      // const firstSnap = offeringUsersRef.once("value").then((snapshot) => snapshot.val());
-      // console.log("|| firstSnap:", firstSnap);
-      // this.relevantUsers = Object.keys(firstSnap).filter(key => key !== user.id);
-      // this.usersDocumentMetadataPaths = this.relevantUsers.map(key => {
-      //   return `${this.classPath}/users/${key}/personalDocs`;
-      // });
-
-      // console.log("|| relevantUsers:", this.relevantUsers);
-
-      // resolve();
+      const classPath = this.db.firebase.getClassPath(user);
       offeringUsersRef.once("value").then((snapshot) => {
           const snapVal = snapshot.val();
           const userKeys = Object.keys(snapVal).filter(key => key !== user.id);
-          const userPaths = userKeys.map(key => `${this.classPath}/users/${key}/personalDocs`);
-          this.usersDocumentMetadataPaths = userPaths;
-
-          // console.log("|| userDocumentMetadataPaths:", userPaths);
-          // create a firebase ref for each path in userPaths
+          const userPaths = userKeys.map(key => `${classPath}/users/${key}/personalDocs`);
           const userPersonalDocsRefs = userPaths.map(path => this.db.firebase.ref(path));
           userPersonalDocsRefs.forEach(ref => {
             ref.on("child_added", this.handlePersonalDocAdded);
@@ -78,58 +51,7 @@ export class DBStudentPersonalDocsListener extends BaseListener {
 
     if (existingDoc) return;
 
-
-
-    //console.log("|| DOCUMENT:", document);
-
     this.db.createDocumentModelFromOtherDocument(docMetaSnap, PersonalDocument);
-      // this.db.createDocumentModelFromProblemMetadata(PersonalDocument as any, document.self.uid, document)
-      // .then((docModel) => {
-      //   if (isCurrentUser) {
-      //     documents.resolveRequiredDocumentPromise(docModel);
-      //     syncStars(docModel, this.db);
-      //   }
-      // });
-
-  };
-
-  // {{HANDLEDOCS}}
-  private handlePersonalDocs = (snapshot: firebase.database.DataSnapshot) => {
-    const snapVal = snapshot.val();
-    console.log("|| handlePersonalDocs, looking for student personal docs!", snapVal);
-    const users: DBOfferingUserMap = snapshot.val();
-    const { user: currentUser } = this.db.stores;
-    forEach(users, (user: DBOfferingUser) => {
-      const isCurrentUser = String(user.self.uid) === currentUser.id;
-      if (user && !isCurrentUser) {
-        this.handleOfferingUser(user);
-      }
-    });
-  };
-
-  // {{ HANDLEADDORCHANGE}}
-  private handleAddOrChange = (eventType: string) => (snapshot: firebase.database.DataSnapshot) => {
-    const user: DBOfferingUser = snapshot.val();
-    this.debugLogSnapshot(`#handleLoadOfferingUserAddedOrChanged (${eventType})`, snapshot);
-    if (user) {
-      this.handleOfferingUser(user);
-    }
-  };
-
-  // {{ MAINEVENT }}
-  private handleOfferingUser = (user: DBOfferingUser) => {
-    console.log("|| handleOfferingUser, looking for a student!", user);
-    if (!user.self?.uid) return;
-    const { documents, user: currentUser } = this.db.stores;
-    const isCurrentUser = String(user.self.uid) === currentUser.id;
-
-    if (size(user.documents) === 0) {
-      documents.resolveRequiredDocumentPromiseWithNull(PersonalDocument);
-    }
-
-    forEach(user.documents, document => {
-
-    });
   };
 }
 
