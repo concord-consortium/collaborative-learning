c507e45f398cd3260ca7386df88b3aa7d21003cd
Joseph Bacal <bacalj@gmail.com>
2023-12-18 12:23:46 -0500

    setting up

--- src/components/document/sort-work-view.tsx
+++ src/components/document/sort-work-view.tsx
@@ -2,24 +2,25 @@ import { observer } from "mobx-react";
 import { useStores } from "../../hooks/use-stores";
 import React from "react";
 import { ProblemDocument } from "../../models/document/document-types";
+import { SortWorkHeader } from "../navigation/sort-work-header";
 
 import "./sort-work-view.scss";
 
 export const SortWorkView:React.FC = observer(function SortWorkView(){
   const stores = useStores();
   const allDocuments = stores.documents;
-  console.log("\t🥩 allDocuments:", allDocuments);
-  // // Sort by ... [Group Dropdown] [All Dropdown]
   const problemDocuments = allDocuments.byType(ProblemDocument);
-  console.log("\t🥩 problemDocuments:", problemDocuments);
 
   return (
-    <div
-      key="sort-work-view"
-      className="sort-work-view"
-    >
-      <div className="sort-work-header"/>
-      <div className="sort-work-documents">
+    <div key="sort-work-view" className="sort-work-view">
+      <SortWorkHeader />
+
+      {/* <DocumentCollectionList
+        subTab={}
+        tabSpec={myWorkTabSpec}
+      /> */}
+
+      {/* <div className="sort-work-documents">
         <div className="live-problem-documents">
           {"Problem Documents (Not Published) - Include documents from all assignments"}
           {
@@ -31,13 +32,13 @@ export const SortWorkView:React.FC = observer(function SortWorkView(){
             })
           }
         </div>
+
         <div className="live-personal-documents">
           {"Personal Documents (Not Published)"}
         </div>
 
-
-      </div>
-
+      </div> */}
     </div>
   );
 });
+
