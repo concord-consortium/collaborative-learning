a29a740eb1f5de5a7673f82c2f7632f9b03cd6bc
Teale Fristoe <fristoe@gmail.com>
2023-12-20 09:17:29 -0800

    Show x attribute in layer legend.

--- src/plugins/graph/components/legend/layer-legend.tsx
+++ src/plugins/graph/components/legend/layer-legend.tsx
@@ -2,6 +2,7 @@ import { observer } from "mobx-react";
 import React, { useContext } from "react";
 import { AttributeType } from "../../../../models/data/attribute";
 import { IDataSet } from "../../../../models/data/data-set";
+import { axisPlaceToAttrRole } from "../../graph-types";
 import { GraphPlace } from "../../imports/components/axis-graph-shared";
 import { SimpleAttributeLabel } from "../simple-attribute-label";
 import { AddSeriesButton } from "./add-series-button";
@@ -12,6 +13,8 @@ import { isSharedDataSet, SharedDataSet, SharedDataSetType } from "../../../../m
 import { useDataConfigurationContext } from "../../hooks/use-data-configuration-context";
 
 import RemoveDataIcon from "../../assets/remove-data-icon.svg";
+import XAxisIcon from "../../assets/x-axis-icon.svg";
+// import YAxisIcon from "../../assets/y-axis-icon.svg";
 
 interface ILayerLegendProps {
   onChangeAttribute: (place: GraphPlace, dataSet: IDataSet, attrId: string, oldAttrId?: string) => void;
@@ -31,6 +34,7 @@ export const LayerLegend = observer(function LayerLegend(props: ILayerLegendProp
   const graphModel = useGraphModelContext();
   const dataConfiguration = useDataConfigurationContext();
   const readOnly = useContext(ReadOnlyContext);
+  const xAttrId = dataConfiguration?.attributeID(axisPlaceToAttrRole.bottom);
 
   function handleRemoveIconClick() {
     if (dataConfiguration?.dataset) {
@@ -104,16 +108,34 @@ export const LayerLegend = observer(function LayerLegend(props: ILayerLegendProp
     <>
       { dataConfiguration?.dataset !== undefined &&
         <div className="legend-title-row">
-          <div className="legend-title">
-            Data from: <strong>{getOriginString()}</strong>&nbsp;
-          </div>
-          { !readOnly &&
-            <div className="legend-icon">
-              <button onClick={handleRemoveIconClick} className="remove-button" title="Unlink data provider">
-                <RemoveDataIcon />
-              </button>
+          <div className="legend-cell-1">
+            { !readOnly &&
+              <div className="legend-icon">
+                <button onClick={handleRemoveIconClick} className="remove-button" title="Unlink data provider">
+                  <RemoveDataIcon />
+                </button>
+              </div>
+            }
+            <div className="legend-title">
+              Data from: <strong>{getOriginString()}</strong>&nbsp;
             </div>
-          }
+          </div>
+          <div className="legend-cell-2">
+            { xAttrId &&
+              <>
+                <div className="axis-icon">
+                  <XAxisIcon />
+                </div>
+                <SimpleAttributeLabel
+                  place="bottom"
+                  attrId={xAttrId}
+                  onChangeAttribute={onChangeAttribute}
+                  onRemoveAttribute={onRemoveAttribute}
+                  onTreatAttributeAs={onTreatAttributeAs}
+                />
+              </>
+            }
+          </div>
         </div>
       }
       {legendItemRows}
