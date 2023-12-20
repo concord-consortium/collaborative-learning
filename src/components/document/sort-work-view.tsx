8172d430724e76db643eb257c88cd264a0d1a43b
Dennis Cao <dennisrcao@gmail.com>
2023-12-20 12:18:56 -0800

    Get styling for thumbnails

--- src/components/document/sort-work-view.tsx
+++ src/components/document/sort-work-view.tsx
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react";
 import { useStores } from "../../hooks/use-stores";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { SortWorkHeader } from "../navigation/sort-work-header";
 import { ICustomDropdownItem } from "../../clue/components/custom-select";
 import { DecoratedDocumentThumbnailItem } from "../thumbnail/decorated-document-thumbnail-item";
@@ -10,80 +10,43 @@ import { DocumentContextReact } from "./document-context";
 import "../thumbnail/document-type-collection.sass";
 import "./sort-work-view.scss";
 
-const getGroupsOfDocuments = (documents: any[], sortBy: string) => {
-  const groupLabel = () => {
-    if (sortBy === "group"){
-      // who is the user that owns the document?
-      // what group are they in on the problem we are on now? (thats in groups.store)
-      // THAT is the group that we will place this document in (NOT the group found in doc.groupId)
-    }
-    if (sortBy === "name"){
-      //...
-    }
-  };
-
-  /* return
-  [
-    {
-      groupLabel: "2" | "Bob Smith", (depending on sortBy)
-      groupKey: <unique>
-      documents: [
-        { actualDocumentObj from store },
-        { actualDocumentObj from store },
-        { actualDocumentObj from store }
-      ]
-    }
-  ]
-  */
-};
-
 export const SortWorkView:React.FC = observer(function SortWorkView(){
   const sortOptions = ["Group", "Student"];
   const stores = useStores();
   const [sortBy, setSortBy] = useState("Group");
-  // const [sortedAll, setSortedAll] = useState<any[]>(stores.documents.all);
-
-  const problemAndPersonalDocs = stores.documents.all; //TODO: filter out published and...look in story
 
   const sortByOptions: ICustomDropdownItem[] = sortOptions.map((option)=>({
     text: option,
     onClick: () => setSortBy(option)
   }));
 
-  useEffect(() => {
-    // TODO: implement the sort
-    console.log("| sort by: ", sortBy);
-   }, [sortBy]);
-
-  // TODO: real calculation of this boolean
-  // The section config is the source of this truth on other paths
   const shouldHandleStarClick = true;
 
   return (
     <div key="sort-work-view" className="sort-work-view">
       <SortWorkHeader sortBy={sortBy} sortByOptions={sortByOptions} />
-
       <div className="documents-panel">
-
-        {
-          stores.documents.all.map((doc:any, idx: number) => {
-            const documentContext = getDocumentContext(doc);
-            return (
-              <DocumentContextReact.Provider key={doc.key} value={documentContext}>
-                <pre style={{padding: "0px", margin: "0px", fontSize: "9px"}}>{ doc.key }</pre>
-                <DecoratedDocumentThumbnailItem
-                  key={doc.key}
-                  scale={0.1}
-                  document={doc}
-                  tab={'sort-work'}
-                  shouldHandleStarClick={shouldHandleStarClick}
-                  allowDelete={false}
-                />
-              </DocumentContextReact.Provider>
-            );
-          })
-        }
-
+        <div className={"tab-panel-documents-section"}>
+          <div className={"list"}>
+            {
+              stores.documents.all.map((doc:any, idx: number) => {
+                const documentContext = getDocumentContext(doc);
+                return (
+                  <DocumentContextReact.Provider key={doc.key} value={documentContext}>
+                    <DecoratedDocumentThumbnailItem
+                      key={doc.key}
+                      scale={0.1}
+                      document={doc}
+                      tab={'sort-work'}
+                      shouldHandleStarClick={shouldHandleStarClick}
+                      allowDelete={false}
+                    />
+                  </DocumentContextReact.Provider>
+                );
+              })
+            }
+          </div>
+        </div>
       </div>
     </div>
   );
