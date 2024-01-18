cf08ab8d8145fff54831a29babff197433ca6fb3
Boris Goldowsky <bgoldowsky@concord.org>
2024-01-18 14:50:07 -0500

    Revert "Define a rounding function"
    This reverts commit 3697d77fd12164dbfed0eb3401a1ea3464de1782.

--- src/utilities/math-utils.ts
+++ src/utilities/math-utils.ts
@@ -12,20 +12,3 @@ export type Point = [x: number, y: number];
 export function isFiniteNumber(x: any): x is number {
   return x != null && Number.isFinite(x);
 }
-
-/**
- * Rounds a number in an intuitive way.
- * The digits parameter is used as a number of significant digits to maintain for small numbers.
- * However, rounding never carries beyond the integer part of a number, even if it contains more than
- * that number of significant digits.
- * @param x number to round
- * @param digits number of digits of precision to maintain in decimals
- * @returns a rounded number
- */
-export function roundForDisplay(x: number, digits: number): number {
-  const intRange = Math.pow(10, digits);
-  if (x<-intRange || x>intRange) {
-    return Math.round(x);
-  }
-  return +x.toPrecision(digits);
-}
