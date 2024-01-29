17a2235da8882c32f811f426110e31e89ad85539
Dennis Cao <dennisrcao@gmail.com>
2024-01-29 11:14:01 -0800

    Console.logs

--- src/components/navigation/sort-work-header.tsx
+++ src/components/navigation/sort-work-header.tsx
@@ -10,6 +10,9 @@ interface ISortHeaderProps{
 }
 
 export const SortWorkHeader:React.FC<ISortHeaderProps>= observer(function SortWorkView({sortBy, sortByOptions}){
+
+  // console.log("\t🥩 sortByOptions:", sortByOptions);
+  // console.log("\t🥩 sortBy:", sortBy);
   return (
     <div className="sort-work-header">
       <div className="header-text">Sort by</div>
