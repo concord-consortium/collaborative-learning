e237fc9756006db36437b2ef0846e5c06363024b
Boris Goldowsky <bgoldowsky@concord.org>
2024-01-29 13:11:04 -0500

    More startup phases defined

--- src/models/stores/stores.ts
+++ src/models/stores/stores.ts
@@ -197,7 +197,8 @@ class Stores implements IStores{
     if (unitJson.status === 404) {
       unitJson = await getUnitJson(appConfig.defaultUnit, appConfig);
     }
-
+    removeLoadingMessage("Loading curriculum content");
+    showLoadingMessage("Setting up curriculum content");
     // read the unit content, but don't instantiate section contents (DocumentModels) yet
     const unit = createUnitWithoutContent(unitJson);
 
@@ -207,10 +208,12 @@ class Stores implements IStores{
     appConfig.setConfigs([unit.config || {}, _investigation?.config || {}, _problem?.config || {}]);
 
     // load/initialize the necessary tools
+    showLoadingMessage("Loading tile types");
     const { authorTools = [], toolbar = [], tools: tileTypes = [] } = appConfig;
     const unitTileTypes = new Set(
       [...toolbar.map(button => button.id), ...authorTools.map(button => button.id), ...tileTypes]);
     await registerTileTypes([...unitTileTypes]);
+    removeLoadingMessage("Loading tile types");
 
     // We are changing our observable state here so we need to be in an action.
     // Because this is an async function, we'd have to switch it to a flow to
@@ -243,7 +246,7 @@ class Stores implements IStores{
       if (tabs.length > 0) {
         this.persistentUI.setActiveNavTab(tabs[0].tab);
       }
-      removeLoadingMessage("Loading curriculum content");
+      removeLoadingMessage("Setting up curriculum content");
     });
 
     addDisposer(unit, when(() => {
