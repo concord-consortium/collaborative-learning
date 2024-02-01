ea8d90802f90fae0a8d3a057d6d909e4c073adb7
Boris Goldowsky <bgoldowsky@concord.org>
2024-02-01 10:06:34 -0500

    Make sure import statements don't run unless needed

--- src/register-tile-types.ts
+++ src/register-tile-types.ts
@@ -3,66 +3,67 @@ import { removeLoadingMessage, showLoadingMessage } from "./utilities/loading-ut
 import "./models/tiles/placeholder/placeholder-registration";
 import "./models/tiles/unknown-content-registration";
 
-function loggedLoad(name: string, imports: Promise<any>[]) {
+function loggedLoad(name: string, imports: () => Promise<unknown>[]) {
   const message = "Loading tile: " + name;
-  return () => {
+  return async () => {
     showLoadingMessage(message);
-    return Promise.all(imports)
-      .then(() => { removeLoadingMessage(message); });
+    await Promise.all(imports());
+    removeLoadingMessage(message);
   };
 }
 
 const gTileRegistration: Record<string, () => void> = {
-  "DataCard": loggedLoad("DataCard", [
+
+  "DataCard": loggedLoad("DataCard", () => [
     import(/* webpackChunkName: "DataCard" */"./plugins/data-card/data-card-registration"),
     import(/* webpackChunkName: "SharedDataSet" */"./models/shared/shared-data-set-registration")
   ]),
-  "Dataflow": loggedLoad("Dataflow", [
+  "Dataflow": loggedLoad("Dataflow", () => [
     import(/* webpackChunkName: "Dataflow" */"./plugins/dataflow/dataflow-registration"),
     import(/* webpackChunkName: "SharedVariables" */"./plugins/shared-variables/shared-variables-registration"),
     import(/* webpackChunkName: "SharedDataSet" */"./models/shared/shared-data-set-registration")
   ]),
-  "Diagram": loggedLoad("Diagram", [
+  "Diagram": loggedLoad("Diagram", () => [
     import(/* webpackChunkName: "Diagram" */"./plugins/diagram-viewer/diagram-registration"),
     import(/* webpackChunkName: "SharedVariables" */"./plugins/shared-variables/shared-variables-registration")
   ]),
-  "Drawing": loggedLoad("Drawing", [
+  "Drawing": loggedLoad("Drawing", () => [
     import(/* webpackChunkName: "Drawing" */"./plugins/drawing/drawing-registration")
   ]),
-  "ErrorTest": loggedLoad("ErrorTest", [
+  "ErrorTest": loggedLoad("ErrorTest", () => [
     import(/* webpackChunkName: "Error" */"./plugins/error-test/error-test-registration")
   ]),
-  "Expression": loggedLoad("Expression", [
+  "Expression": loggedLoad("Expression", () => [
     import(/* webpackChunkName: "Expression" */"./plugins/expression/expression-registration")
   ]),
-  "Geometry": loggedLoad("Geometry", [
+  "Geometry": loggedLoad("Geometry", () => [
     import(/* webpackChunkName: "Geometry" */"./models/tiles/geometry/geometry-registration"),
     import(/* webpackChunkName: "SharedDataSet" */"./models/shared/shared-data-set-registration")
   ]),
-  "Graph": loggedLoad("Graph", [
+  "Graph": loggedLoad("Graph", () => [
     import(/* webpackChunkName: "Graph" */"./plugins/graph/graph-registration"),
     import(/* webpackChunkName: "SharedDataSet" */"./models/shared/shared-data-set-registration"),
     import(/* webpackChunkName: "SharedCaseMetadata" */"./models/shared/shared-case-metadata-registration")
   ]),
-  "Image": loggedLoad("Image", [
+  "Image": loggedLoad("Image", () => [
     import(/* webpackChunkName: "Image" */"./models/tiles/image/image-registration")
   ]),
-  "Numberline": loggedLoad("Numberline", [
+  "Numberline": loggedLoad("Numberline", () => [
     import(/* webpackChunkName: "Numberline" */"./plugins/numberline/numberline-registration"),
     import(/* webpackChunkName: "SharedDataSet" */"./models/shared/shared-data-set-registration")
   ]),
-  "Simulator": loggedLoad("Simulator", [
+  "Simulator": loggedLoad("Simulator", () => [
     import(/* webpackChunkName: "Simulator" */"./plugins/simulator/simulator-registration"),
     import(/* webpackChunkName: "SharedVariables" */"./plugins/shared-variables/shared-variables-registration")
   ]),
-  "Starter": loggedLoad("Starter", [
+  "Starter": loggedLoad("Starter", () => [
     import(/* webpackChunkName: "Starter" */"./plugins/starter/starter-registration")
   ]),
-  "Table": loggedLoad("Table", [
+  "Table": loggedLoad("Table", () => [
     import(/* webpackChunkName: "Table" */"./models/tiles/table/table-registration"),
     import(/* webpackChunkName: "SharedDataSet" */"./models/shared/shared-data-set-registration")
   ]),
-  "Text": loggedLoad("Text", [
+  "Text": loggedLoad("Text", () => [
     import(/* webpackChunkName: "Text" */"./models/tiles/text/text-registration")
   ])
 };
