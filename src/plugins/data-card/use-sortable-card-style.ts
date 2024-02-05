91f7950ce9942a2f1df8a75c18c1516eaf77f4ba
Joseph Bacal <bacalj@gmail.com>
2024-02-05 16:06:43 -0500

    better sliding poc

--- src/plugins/data-card/use-sortable-card-style.ts
+++ src/plugins/data-card/use-sortable-card-style.ts
@@ -1,10 +1,17 @@
-import classNames
- from "classnames";
+import classNames from "classnames";
 interface IProps {
   transform?: { x: number; y: number } | null;
   indexInStack: number;
   atStackTop: boolean;
   stackIsExpanded: boolean;
+}
+
+function getTiltAngle(index: number, stackIsExpanded: boolean) {
+  if (stackIsExpanded) {
+    return 0;
+  }
+  const angles = [0, 1.25, -1.25, 3.5, -3.5];
+  return angles[index % angles.length];
 }
 
 export const useSortableCardStyles = (props: IProps) => {
@@ -15,12 +22,13 @@ export const useSortableCardStyles = (props: IProps) => {
     {
       "at-stack-top": atStackTop,
       "in-expanded-stack": stackIsExpanded,
-       "in-collapsed-stack": !stackIsExpanded
+      "in-collapsed-stack": !stackIsExpanded
     },
   );
 
   let dynamicStyles;
 
+  // the only transform we apply is the one created by mouse dragging
   if (transform) {
     dynamicStyles = {
       transform: `translate3d(${transform.x}px, ${transform.y - 25}px, 0)`,
@@ -28,15 +36,24 @@ export const useSortableCardStyles = (props: IProps) => {
       opacity: stackIsExpanded ? 1 : 0.8,
     };
   }
+  // otherwise we may have some transforms based on the index in the stack and the expansion state
+  else {
+    const rotationString = !stackIsExpanded
+      ? `rotate(${getTiltAngle(indexInStack, stackIsExpanded)}deg)`
+      : `rotate(0deg)`;
+
+    const translateYString = !stackIsExpanded
+      ? `translateY(${indexInStack * -100}%)`
+      : `translateY(0)`;
+
+    dynamicStyles = {
+      zIndex: indexInStack,
+      transform: `${rotationString} ${translateYString}`,
+      transition: "transform 0.5s",
+    };
+  }
 
   return {dynamicClasses, dynamicStyles};
 };
 
-/**
- *   const angle = getTiltAngle(indexInStack);
- * const getTiltAngle = (index: number) => {
-  const angles = [-2, -1, 0, 1, 2];
-  const angle = angles[index % angles.length];
-  return angle;
-};
- */
+
