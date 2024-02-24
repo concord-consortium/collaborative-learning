336eb86e58d42dbcd2b1c495fd47872db29e1034
Joseph Bacal <bacalj@gmail.com>
2024-02-24 17:35:27 -0500

    set up a "newConfiguration" to work with

--- cms/src/cms-collections.ts
+++ cms/src/cms-collections.ts
@@ -2,47 +2,82 @@ import { CmsConfig, CmsField } from "netlify-cms-core";
 import { urlParams } from "../../src/utilities/url-params";
 import { AppConfigModel, AppConfigModelSnapshot } from "../../src/models/stores/app-config-model";
 import appConfigJson from "../../src/clue/app-config.json";
-import { UnitModelType, getUnitJson } from "../../src/models/curriculum/unit";
+import { getUnitJson } from "../../src/models/curriculum/unit";
 
 const appConfig = AppConfigModel.create(appConfigJson as AppConfigModelSnapshot);
 const unit = urlParams.unit ?? "sas";
-let unitJson: Promise<UnitModelType> | undefined;
+let unitJson: any;
 
 getUnitJson(unit, appConfig).then((json) => {
   unitJson = json;
   getCmsCollections();
 });
 
-export function getCmsCollections(){
-  console.log("| getCmsCollections returning config based on unitJson |", unitJson);
-  // TODO: why is below succeeding in surfacing "Teacher Guides" as a section type?
-  return  [{
-    name: "sections",
-    label: "Curriculum Sections",
-    label_singular: "Curriculum Section",
-    identifier_field: "type",
-    format: "json",
-    folder: urlParams.unit ? `curriculum/${urlParams.unit}` : `curriculum`,
-    nested: {
-      depth: 6,
+const defaultConfiguration = [{
+  name: "sections",
+  label: "Curriculum Sections",
+  label_singular: "Curriculum Section",
+  identifier_field: "type",
+  format: "json",
+  folder: urlParams.unit ? `curriculum/${urlParams.unit}` : `curriculum`,
+  nested: {
+    depth: 6,
+  },
+  fields: [
+    {
+      label: "Type",
+      name: "type",
+      widget: "string"
     },
-    fields: [
-      {
-        label: "Type",
-        name: "type",
-        widget: "string"
-      },
-      {
-        label: "Preview Link",
-        name: "preview-link",
-        required: false,
-        widget: "preview-link"
-      } as CmsField,
-      {
-        label: "Content",
-        name: "content",
-        widget: "clue" as any
-      }
-    ],
-  }] as CmsConfig["collections"];
+    {
+      label: "Preview Link",
+      name: "preview-link",
+      required: false,
+      widget: "preview-link"
+    } as CmsField,
+    {
+      label: "Content",
+      name: "content",
+      widget: "clue" as any
+    }
+  ],
+}];
+
+const newConfiguration = [{
+  name: "sections",
+  label: "Curriculum Sections",
+  label_singular: "Curriculum Section",
+  identifier_field: "type",
+  format: "json",
+  folder: urlParams.unit ? `curriculum/${urlParams.unit}` : `curriculum`,
+  nested: {
+    depth: 6,
+  },
+  fields: [
+    {
+      label: "Type",
+      name: "type",
+      widget: "string"
+    },
+    {
+      label: "Preview Link",
+      name: "preview-link",
+      required: false,
+      widget: "preview-link"
+    } as CmsField,
+    {
+      label: "Content",
+      name: "content",
+      widget: "clue" as any
+    }
+  ],
+}];
+
+export function getCmsCollections(){
+  if (!unitJson) return defaultConfiguration as CmsConfig["collections"];
+  if (unitJson.code === "moth") {
+    return newConfiguration as CmsConfig["collections"];
+  } else {
+    return defaultConfiguration as CmsConfig["collections"];
+  }
 }
