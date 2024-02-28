9a13d012d16d40acc8b85f08c45ac3ba99afe985
Joseph Bacal <bacalj@gmail.com>
2024-02-28 13:58:03 -0500

    wip, get the proper strings in the proper places in the various conditions

--- src/hooks/use-document-caption.tsx
+++ src/hooks/use-document-caption.tsx
@@ -1,7 +1,7 @@
 import { useFirestoreTeacher } from "./firestore-hooks";
 import { useAppConfig, useClassStore, useProblemStore, useUserStore } from "./use-stores";
 import { DocumentModelType } from "../models/document/document";
-import { isPublishedType, isUnpublishedType, SupportPublication } from "../models/document/document-types";
+import { isPublishedType, isUnpublishedType } from "../models/document/document-types";
 import { getDocumentDisplayTitle } from "../models/document/document-utils";
 
 export function useDocumentCaption(document: DocumentModelType, isStudentWorkspaceDoc?: boolean) {
@@ -12,13 +12,13 @@ export function useDocumentCaption(document: DocumentModelType, isStudentWorkspa
   const { type, uid } = document;
   const pubVersion = document.pubVersion;
   const teacher = useFirestoreTeacher(uid, user.network || "");
-  if (type === SupportPublication) {
-    const caption = document.getProperty("caption") || "Support";
-    return pubVersion ? `${caption} v${pubVersion}` : `${caption}`;
-  }
   const userName = classStore.getUserById(uid)?.displayName || teacher?.name ||
-                    (document.isRemote ? teacher?.name : "") || "Unknown User";
+  (document.isRemote ? teacher?.name : "") || "Unknown User";
 
+  // if (type === SupportPublication) {
+  //   const caption = document.getProperty("caption") || "Support";
+  //   return pubVersion ? `${caption} v${pubVersion}` : `${caption}`;
+  // }
 
   const hasNamePrefix =  document.isRemote || isPublishedType(type) || isUnpublishedType(type) || isStudentWorkspaceDoc;
   const namePrefix = hasNamePrefix ? `${userName}: ` : "";
