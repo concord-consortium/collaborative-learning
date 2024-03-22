6eb90e70a6b1c261c5078b9b97c9017993071a4e
Joe Bacal <bacalj@gmail.com>
2024-03-22 10:35:39 -0400

    assets file

---
+++ src/plugins/simulator/simulations/potentiometer-servo/potentiometer-servo-assets.tsx
@@ -0,0 +1,29 @@
+import boardBackExpanded from "./assets/arduino-back-expanded.svg";
+//import boardBackSmall from "./assets/arduino-back-small.svg"; // TODO ask Ryan for exportable version of this
+
+
+
+import potBaseExpanded from "./assets/potentiometer-base-expanded.svg";
+import potBaseSmall from "./assets/potentiometer-base-minimized.svg";
+import potDial from "./assets/pot-top.svg";
+
+import servoBaseExpanded from "./assets/servo-base-expanded.svg";
+import servoBaseSmall from "./assets/servo-base-minimized.svg";
+import servoArm from "./assets/servo-arm.svg";
+
+export const servoImages = {
+  baseExpanded: servoBaseExpanded,
+  baseSmall: servoBaseSmall,
+  arm: servoArm
+};
+
+export const potentiometerImages = {
+  baseExpanded: potBaseExpanded,
+  baseSmall: potBaseSmall,
+  dial: potDial
+};
+
+export const boardImages = {
+  backExpanded: boardBackExpanded
+};
+
