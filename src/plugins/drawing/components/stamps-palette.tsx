3b732090fee92729d6241f41f59255f15bd14faa
Joe Bacal <bacalj@gmail.com>
2024-05-23 16:19:20 -0400

    actually we can simplifty and not calculate rows at all

--- src/plugins/drawing/components/stamps-palette.tsx
+++ src/plugins/drawing/components/stamps-palette.tsx
@@ -1,4 +1,3 @@
-import classNames from "classnames";
 import React from "react";
 import { StampModelType } from "../model/stamp";
 import { StampButton } from "./stamp-button";
@@ -8,19 +7,10 @@ interface IProps {
   selectedStampIndex?: number;
   onSelectStampIndex: (index: number) => void;
 }
-export const StampsPalette: React.FC<IProps> = ({ stamps, selectedStampIndex, onSelectStampIndex }) => {
-  const rowClass = classNames(
-    "toolbar-palette", "stamps",
-    {
-      "one-row": stamps.length <= 6,
-      "two-rows": stamps.length > 6 && stamps.length <= 12,
-      "three-rows": stamps.length > 12 && stamps.length <= 18,
-      "four-rows": stamps.length > 18
-    }
-  );
 
+export const StampsPalette: React.FC<IProps> = ({ stamps, selectedStampIndex, onSelectStampIndex }) => {
   return (
-    <div className={rowClass}>
+    <div className="toolbar-palette stamps">
       <div className="palette-buttons">
         {stamps.map((stamp, i) =>
           <StampButton key={stamp.url} stamp={stamp}
