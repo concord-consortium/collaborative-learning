e44e2c3924bf051e1a0e76f8d71d82ba610c96b2
Joe Bacal <bacalj@gmail.com>
2024-05-24 10:24:57 -0400

    example of removing dead button definitions (and all specified buttonComponents)

--- src/plugins/drawing/components/drawing-object-manager.tsx
+++ src/plugins/drawing/components/drawing-object-manager.tsx
@@ -3,15 +3,13 @@ import { types } from "mobx-state-tree";
 import { DrawingComponentType, DrawingObject, DrawingObjectType,
   DrawingTool, HandleObjectHover, HandleObjectDrag,
   IDrawingLayer, IToolbarButtonProps } from "../objects/drawing-object";
-import { EllipseComponent, EllipseDrawingTool, EllipseObject, EllipseToolbarButton } from "../objects/ellipse";
-import { ImageComponent, ImageObject, StampDrawingTool, StampToolbarButton } from "../objects/image";
-import { LineComponent, LineDrawingTool, LineObject, LineToolbarButton } from "../objects/line";
-import { RectangleComponent, RectangleDrawingTool, RectangleObject,
-  RectangleToolbarButton} from "../objects/rectangle";
-import { VectorComponent, VectorDrawingTool, VectorObject, VectorToolbarButton } from "../objects/vector";
-import { DeleteButton, DuplicateButton, SelectToolbarButton } from "./drawing-toolbar-buttons";
+import { EllipseComponent, EllipseDrawingTool, EllipseObject } from "../objects/ellipse";
+import { ImageComponent, ImageObject, StampDrawingTool } from "../objects/image";
+import { LineComponent, LineDrawingTool, LineObject } from "../objects/line";
+import { RectangleComponent, RectangleDrawingTool, RectangleObject} from "../objects/rectangle";
+import { VectorComponent, VectorDrawingTool, VectorObject } from "../objects/vector";
 import { SelectionDrawingTool } from "./selection-drawing-tool";
-import { TextComponent, TextDrawingTool, TextObject, TextToolbarButton } from "../objects/text";
+import { TextComponent, TextDrawingTool, TextObject } from "../objects/text";
 
 export interface IDrawingObjectInfo {
   type: string;
@@ -24,7 +22,7 @@ export interface IDrawingToolInfo {
   // using a simple `typeof DrawingTool` can't be used because that type
   // is an abstract class so can't be instantiated.
   toolClass?: { new(drawingLayer: IDrawingLayer): DrawingTool };
-  buttonComponent: React.ComponentType<IToolbarButtonProps>;
+  buttonComponent?: React.ComponentType<IToolbarButtonProps>; // HEY: may be deleting all of these
 }
 
 const gDrawingObjectInfos: Record<string, IDrawingObjectInfo | undefined> = {
@@ -64,45 +62,36 @@ const gDrawingToolInfos: Record<string, IDrawingToolInfo | undefined> = {
   select: {
     name: "select",
     toolClass: SelectionDrawingTool,
-    buttonComponent: SelectToolbarButton
   },
   line: {
     name: "line",
-    toolClass: LineDrawingTool,
-    buttonComponent: LineToolbarButton
+    toolClass: LineDrawingTool
   },
   vector: {
     name: "vector",
     toolClass: VectorDrawingTool,
-    buttonComponent: VectorToolbarButton
   },
   rectangle: {
     name: "rectangle",
-    toolClass: RectangleDrawingTool,
-    buttonComponent: RectangleToolbarButton
+    toolClass: RectangleDrawingTool
   },
   ellipse: {
     name: "ellipse",
-    toolClass: EllipseDrawingTool,
-    buttonComponent: EllipseToolbarButton
+    toolClass: EllipseDrawingTool
   },
   text: {
     name: "text",
-    toolClass: TextDrawingTool,
-    buttonComponent: TextToolbarButton
+    toolClass: TextDrawingTool
   },
   stamp: {
     name: "stamp",
-    toolClass: StampDrawingTool,
-    buttonComponent: StampToolbarButton
+    toolClass: StampDrawingTool
   },
   duplicate: {
-    name: "duplicate",
-    buttonComponent: DuplicateButton
+    name: "duplicate"
   },
   delete: {
-    name: "delete",
-    buttonComponent: DeleteButton
+    name: "delete"
   }
 };
 
@@ -135,7 +124,7 @@ export function renderDrawingObject(drawingObject: DrawingObjectType, readOnly=f
                                     handleHover?: HandleObjectHover, handleDrag?: HandleObjectDrag) {
   const DrawingObjectComponent = getDrawingObjectComponent(drawingObject);
   if (!DrawingObjectComponent) return null;
-  const element = (<DrawingObjectComponent key={drawingObject.id} model={drawingObject} readOnly={readOnly} 
+  const element = (<DrawingObjectComponent key={drawingObject.id} model={drawingObject} readOnly={readOnly}
                     handleHover={handleHover} handleDrag={handleDrag}/>);
   if (drawingObject.visible) {
     return element;
