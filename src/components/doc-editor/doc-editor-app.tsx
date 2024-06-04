0cb85ead3619bc9492905f570ed896f5a4579400
Scott Cytacki <scytacki@concord.org>
2024-06-03 21:48:45 -0400

    update the status string
    Also update the remote doc when opening a file document

--- src/components/doc-editor/doc-editor-app.tsx
+++ src/components/doc-editor/doc-editor-app.tsx
@@ -22,22 +22,28 @@ import "./doc-editor-app.scss";
 
 const kDocEditorDocKey = "clue-doc-editor-doc";
 
+// These are defined at the module level so the initialDoc can be used in two useState
+// initializers. The initialDoc should only change when the page is loaded so this is
+// safe.
+const {document: documentURL, readOnly, noStorage } = urlParams;
+const savedDocString = noStorage ? undefined : window.sessionStorage.getItem(kDocEditorDocKey);
+const initialDoc = savedDocString ? JSON.parse(savedDocString) : defaultDocumentModel;
+
 export const DocEditorApp = observer(function DocEditorApp() {
-  const {document: documentURL, readOnly, noStorage } = urlParams;
 
   const appConfig = useAppConfig();
   const [document, setDocument] = useState(() => {
-    const savedDocString = noStorage ? undefined : window.sessionStorage.getItem(kDocEditorDocKey);
-    const initalDoc = savedDocString ? JSON.parse(savedDocString) : defaultDocumentModel;
-
-    return createDocumentModelWithEnv(appConfig, initalDoc);
+    return createDocumentModelWithEnv(appConfig, initialDoc);
   });
-  const [remoteDocument] = useState(() => {
-    return createDocumentModelWithEnv(appConfig, getSnapshot(document));
+  const [remoteDocument, setRemoteDocument] = useState(() => {
+    return createDocumentModelWithEnv(appConfig, initialDoc);
   });
   const [fileHandle, setFileHandle] = useState<FileSystemHandle|undefined>();
   const [sectionSnapshot, setSectionSnapshot] = useState<any>();
-  const [fileName, setFileName] = useState<string>("<no file loaded>");
+  const [fileName, setFileName] = useState<string>("");
+  const [status, setStatus] = useState<string>(() =>
+    savedDocString ? "<restored from storage>" : "<new document>"
+  );
   const [settings] = useState(() => {
     const _settings = DocEditorSettings.create();
     // Expose the settings to make it easier for Cypress to change them
@@ -58,10 +64,12 @@ export const DocEditorApp = observer(function DocEditorApp() {
     } else {
       documentContentSnapshot = _parsedText;
     }
-    setDocument(createDocumentModelWithEnv(appConfig, {
+    const snapshot = {
       ...defaultDocumentModelParts,
       content: documentContentSnapshot
-    }));
+    };
+    setDocument(createDocumentModelWithEnv(appConfig, snapshot));
+    setRemoteDocument(createDocumentModelWithEnv(appConfig, snapshot));
   }, [appConfig]);
 
   useEffect(() => {
@@ -70,6 +78,13 @@ export const DocEditorApp = observer(function DocEditorApp() {
 
       if (!noStorage) {
         window.sessionStorage.setItem(kDocEditorDocKey, snapString);
+        if (!fileHandle) {
+          setStatus("<saved to storage>");
+        }
+      }
+
+      if (fileHandle) {
+        setStatus(`file: ${fileName} <modified>`);
       }
 
       // Emulate what happens during remote syncing.
@@ -79,7 +94,7 @@ export const DocEditorApp = observer(function DocEditorApp() {
       const jsonFiltered = JSON.parse(snapString);
       applySnapshot(remoteDocument, jsonFiltered);
     });
-  }, [document, noStorage, remoteDocument]);
+  }, [document, fileHandle, fileName, remoteDocument]);
 
   // Handle opening both section documents with a content field
   // and basic documents which are just the content itself
@@ -88,6 +103,7 @@ export const DocEditorApp = observer(function DocEditorApp() {
     setFileHandle(_fileHandle);
     const file = await _fileHandle.getFile();
     setFileName(file.name);
+    setStatus(`file: ${file.name}`);
     const text = await file.text();
     loadDocument(text);
   }
@@ -99,12 +115,14 @@ export const DocEditorApp = observer(function DocEditorApp() {
     }
 
     let _fileHandle = fileHandle;
+    let _fileName = fileName;
     if (!_fileHandle) {
       _fileHandle = await (window as any).showSaveFilePicker();
       setFileHandle(_fileHandle);
       if (_fileHandle) {
         const file = await (_fileHandle as any).getFile();
-        setFileName(file.name);
+        _fileName = file.name;
+        setFileName(_fileName);
       }
 
     }
@@ -127,6 +145,8 @@ export const DocEditorApp = observer(function DocEditorApp() {
     await writable.write(contents);
     // Close the file and write the contents to disk.
     await writable.close();
+
+    setStatus(`file: ${_fileName}`);
   }
 
   const [showSettings] = useCustomModal({
@@ -173,7 +193,7 @@ export const DocEditorApp = observer(function DocEditorApp() {
       // This error is printed in the console as an "Uncaught (in promise)..."
       throw Error(`Request rejected with exception ${error}`);
     });
-  }, [documentURL, loadDocument]);
+  }, [loadDocument]);
 
   // This is wrapped in a div.primary-workspace so it can be used with cypress
   // tests that are looking for stuff in a div like this
@@ -186,7 +206,7 @@ export const DocEditorApp = observer(function DocEditorApp() {
               <div className="actions left">
                 <button onClick={handleOpen}>open</button>
                 <button onClick={handleSave}>save</button>
-                <span className="filename">{fileName}</span>
+                <span className="status">{status}</span>
                 <button onClick={handleClear}>reset doc</button>
                 <button onClick={handleSettings}>settings</button>
                 <DocumentAnnotationToolbar/>
