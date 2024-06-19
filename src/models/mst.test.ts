cc4883b68f358c9657a096f07e574e9dfe812cde
Scott Cytacki <scytacki@concord.org>
2024-06-18 22:19:03 -0400

    convert null to NaN in the graph Point model
    In the process of this change I also found that preProcessSnapshot has some nuance on how it works.
    When `prop: types.maybe(Model)` is used, then it undefined is passed to preProcessSnapshot.
    To respect the `types.maybe`, preProcessSnapshot has to return undefined.

--- src/models/mst.test.ts
+++ src/models/mst.test.ts
@@ -923,4 +923,66 @@ describe("mst", () => {
     const rehydrated = JSON.parse(snapshotString);
     expect(rehydrated).toEqual({propValue: null});
   });
+
+  test("preProcessSnapshot gets called with undefined in some cases", () => {
+    let snapshotPassedToPreProcess = "Initial Value" as any;
+    const TestObject = types
+      .model("TestObject", {
+        prop: types.optional(types.string, "default")
+      })
+      .preProcessSnapshot(snapshot => {
+        snapshotPassedToPreProcess = snapshot;
+        return snapshot;
+      });
+
+    const TestContainer = types.
+      model("TestContainer", {
+        child: types.maybe(TestObject)
+      });
+
+    const container1 = TestContainer.create({});
+    expect(snapshotPassedToPreProcess).toBeUndefined();
+    expect(container1.child).toBeUndefined();
+
+    const container2 = TestContainer.create({child: {prop: "value"}});
+    expect(snapshotPassedToPreProcess).toEqual({prop: "value"});
+    expect(container2.child?.prop).toBe("value");
+
+    const container3 = TestContainer.create({child: {}});
+    expect(snapshotPassedToPreProcess).toEqual({});
+    expect(container3.child?.prop).toBe("default");
+  });
+
+  test("preProcessSnapshot should return undefined to respect `types.maybe`", () => {
+    let snapshotPassedToPreProcess = "Initial Value" as any;
+    let valueReturnedByPreProcess: any;
+    const TestObject = types
+      .model("TestObject", {
+        prop: types.optional(types.string, "default")
+      })
+      .preProcessSnapshot(snapshot => {
+        snapshotPassedToPreProcess = snapshot;
+        return valueReturnedByPreProcess;
+      });
+
+    const TestContainer = types.
+      model("TestContainer", {
+        child: types.maybe(TestObject)
+      });
+
+    // Note: the MST type system doesn't want you to return undefined from preProcessSnapshot
+    // but you have to if you want the child to be undefined as expected
+    valueReturnedByPreProcess = undefined;
+    const container1 = TestContainer.create({});
+    expect(snapshotPassedToPreProcess).toBeUndefined();
+    expect(container1.child).toBeUndefined();
+
+    valueReturnedByPreProcess = {};
+    const container2 = TestContainer.create({});
+    expect(container2.child?.prop).toBe("default");
+
+    valueReturnedByPreProcess = {prop: "value"};
+    const container3 = TestContainer.create({});
+    expect(container3.child?.prop).toBe("value");
+  });
 });
