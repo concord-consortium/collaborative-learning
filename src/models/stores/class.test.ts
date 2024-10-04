3535d1f45c0132fda60c76e608f8b3d8850d4f56
Boris Goldowsky <bgoldowsky@concord.org>
2024-10-04 16:01:10 -0400

    Add test

--- src/models/stores/class.test.ts
+++ src/models/stores/class.test.ts
@@ -62,4 +62,38 @@ describe("Class model", () => {
     // There is always a exemplar user added to the class
     expect(clazz.users.size).toEqual(2);
   });
+
+  it("includes AI user when configured", () => {
+    const clazz = ClassModel.create({
+      name: "test class",
+      classHash: "test",
+    });
+    const classInfo: ClassInfo = {
+      name: "test class",
+      classHash: "test",
+      students: [
+        {
+          type: "student",
+          className: "test class",
+          classHash: "test",
+          offeringId: "1",
+          id: "1",
+          portal: "test",
+          firstName: "First",
+          lastName: "Student",
+          fullName: "First Student",
+          initials: "FS",
+        }
+      ],
+      teachers: [],
+      localTimestamp: Date.now()
+    };
+    clazz.updateFromPortal(classInfo, true);
+
+    // Student 1, II, and AI
+    expect(clazz.users.size).toEqual(3);
+    expect(clazz.getUserById("1")).toBeDefined();
+    expect(clazz.getUserById("ivan_idea_1")).toBeDefined();
+    expect(clazz.getUserById("ada_insight_1")).toBeDefined();
+  });
 });
