bf691ffe86ead4f24907e99eabc37bd0ac488aca
Ethan McElroy <emcelroy@concord.org>
2024-10-09 18:01:03 -0400

    chore: always zoom in/out in intervals of 10%

--- src/plugins/drawing/toolbar-buttons/zoom-buttons.tsx
+++ src/plugins/drawing/toolbar-buttons/zoom-buttons.tsx
@@ -10,7 +10,7 @@ import ZoomInIcon from "../../../clue/assets/icons/zoom-in-icon.svg";
 import ZoomOutIcon from "../../../clue/assets/icons/zoom-out-icon.svg";
 import FitViewIcon from "../../../clue/assets/icons/fit-view-icon.svg";
 
-const zoomFactor = 1.25;
+const zoomStep = 0.1;
 const minZoom = 0.1;
 const maxZoom = 2;
 
@@ -19,7 +19,8 @@ export const ZoomInButton = observer(function ZoomInButton({ name }: IToolbarBut
   const disabled = drawingModel?.zoom >= maxZoom;
 
   function handleClick() {
-    drawingModel?.setZoom(Math.min(maxZoom, drawingModel.zoom * zoomFactor));
+    const roundedZoom = Math.round((drawingModel.zoom + zoomStep) * 10) / 10;
+    drawingModel?.setZoom(Math.min(maxZoom, roundedZoom));
   }
 
   return (
@@ -39,7 +40,8 @@ export const ZoomOutButton = observer(function ZoomOutButton({ name }: IToolbarB
   const disabled = drawingModel?.zoom <= minZoom;
 
   function handleClick() {
-    drawingModel?.setZoom(Math.max(minZoom, drawingModel.zoom / zoomFactor));
+    const roundedZoom = Math.round((drawingModel.zoom - zoomStep) * 10) / 10;
+    drawingModel?.setZoom(Math.max(minZoom, roundedZoom));
   }
 
   return (
