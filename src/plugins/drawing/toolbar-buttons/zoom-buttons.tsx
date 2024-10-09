9cd68a1d5000fe907d5d4195045c0d0457a45224
Ethan McElroy <emcelroy@concord.org>
2024-10-09 17:54:10 -0400

    fix: inconsistencies with fit all functionality

--- src/plugins/drawing/toolbar-buttons/zoom-buttons.tsx
+++ src/plugins/drawing/toolbar-buttons/zoom-buttons.tsx
@@ -64,23 +64,15 @@ export const FitAllButton = observer(function FitAllButton({ name }: IToolbarBut
     const canvasSize = drawingAreaContext?.getVisibleCanvasSize();
     if (canvasSize) {
       const bb = drawingModel.objectsBoundingBox;
-      // Calculate the full width and height of the drawing content bounding box.
       const contentWidth = bb.se.x - bb.nw.x;
       const contentHeight = bb.se.y - bb.nw.y;
-      // Find the optimal zoom level to fit the content inide the viewable area.
       const optimalZoom = Math.min((canvasSize.x - padding) / contentWidth, (canvasSize.y - padding) / contentHeight);
       const legalZoom = Math.max(minZoom, Math.min(maxZoom, optimalZoom));
-      drawingModel?.setZoom(legalZoom);
+      const requiredOffsetX = Math.abs(bb.nw.x * legalZoom);
+      const requiredOffsetY = Math.abs(bb.nw.y * legalZoom);
 
-      // Get the lowest x and y coordinates of all the objects and adjust the offset accordingly
-      // If the lowest coordinate is negative, adjust the offset. Otherwise, set it to 0.
-      const lowestYCoord = bb.nw.y;
-      const lowestXCoord = bb.nw.x;
-      const offsetX = drawingModel.offsetX;
-      const offsetY = drawingModel.offsetY;
-      const newOffsetX = lowestXCoord < 0 ? (offsetX - lowestXCoord) * legalZoom : 0;
-      const newOffsetY = lowestYCoord < 0 ? (offsetY - lowestYCoord) * legalZoom : 0;
-      drawingModel?.setOffset(newOffsetX, newOffsetY);
+      drawingModel?.setZoom(legalZoom);
+      drawingModel?.setOffset(requiredOffsetX, requiredOffsetY);
     }
   }
 
