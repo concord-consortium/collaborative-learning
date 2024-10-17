eb04be9fc718dfde4006f5340071b42eaaafcf0f
Boris Goldowsky <bgoldowsky@concord.org>
2024-10-17 14:35:27 -0400

    Update index.ts

--- functions-v2/src/index.ts
+++ functions-v2/src/index.ts
@@ -1,7 +1,7 @@
 import * as admin from "firebase-admin";
 export {onUserDocWritten} from "./on-user-doc-written";
 export {atMidnight} from "./at-midnight";
-export {onAnalyzableDocWritten} from "./on-analyzable-doc-written";
+export {onAnalyzableTestDocWritten, onAnalyzableProdDocWritten} from "./on-analyzable-doc-written";
 export {onAnalysisDocumentPending} from "./on-analysis-document-pending";
 export {onAnalysisDocumentImaged} from "./on-analysis-document-imaged";
 
