72d87094242bd97448616e235fa0bc8490a69fa7
Doug Martin <dmartin@concord.org>
2025-03-12 17:02:46 -0400

    Added placeholder filter to getAllTileIds()

--- src/models/document/base-document-content.ts
+++ src/models/document/base-document-content.ts
@@ -2,7 +2,7 @@ import { each } from "lodash";
 import { types, getType, getEnv, SnapshotIn } from "mobx-state-tree";
 import { kPlaceholderTileDefaultHeight } from "../tiles/placeholder/placeholder-constants";
 import {
-  getPlaceholderSectionId, isPlaceholderTile, PlaceholderContentModel
+  getPlaceholderSectionId, isPlaceholderTile, kPlaceholderTileType, PlaceholderContentModel
 } from "../tiles/placeholder/placeholder-content";
 import { getTileContentInfo, IDocumentExportOptions } from "../tiles/tile-content-info";
 import { ITileContentModel, ITileEnvironment, TileContentModel } from "../tiles/tile-content";
@@ -201,14 +201,17 @@ export const BaseDocumentContentModel = types
         return sharedModels;
       },
       getAllTileIds(includeTeacherContent: boolean) {
-        // returns all tile ids in document order filtered by includeTeacherContent
+        // returns all non-placeholder tile ids in document order filtered by includeTeacherContent
         return self.rowOrder.reduce((tileIds: string[], rowId) => {
           const row = self.rowMap.get(rowId);
           if (row) {
             const publicTileIds = row.tiles
               .filter(tile => {
-                const display = self.tileMap.get(tile.tileId)?.display;
-                return display !== "teacher" || includeTeacherContent;
+                const tileInfo = self.tileMap.get(tile.tileId);
+                if (!tileInfo) return false;
+                const {display, content} = tileInfo;
+                const isPlaceholder = content?.type === kPlaceholderTileType;
+                return !isPlaceholder && (display !== "teacher" || includeTeacherContent);
               })
               .map(tile => tile.tileId);
             tileIds.push(...publicTileIds);
