a6c30e6aba5bd6f19675a0d40bbd2f72fdbc99ca
Boris Goldowsky <bgoldowsky@concord.org>
2025-05-08 16:35:11 -0400

    PR comments

--- src/lib/logger-utils.ts
+++ src/lib/logger-utils.ts
@@ -1,6 +1,5 @@
 import { DocumentModelType } from "../models/document/document";
 import { SectionModelType } from "../models/curriculum/section";
-import { getParentWithTypeName } from "../utilities/mst-utils";
 
 type ModelTypeUnion = DocumentModelType | SectionModelType | null;
 
@@ -12,10 +11,6 @@ export const getTileTitleForLogging = (tileId: string, docOrSection?: ModelTypeU
  * Otherwise undefined is returned.
  */
 export const getTileContainerForLogging = (tileId: string, docOrSection?: ModelTypeUnion) => {
-  const row = docOrSection?.content?.getRowForTile(tileId);
-  if (row) {
-    const container = getParentWithTypeName(row, "TileModel");
-    return container?.id;
-  }
-  return undefined;
+  const tile = docOrSection?.content?.getTileContainingTileId(tileId);
+  return tile?.id;
 };
