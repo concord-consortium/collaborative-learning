ed837ef21bb0736197addade36e0e97805e908da
eireland <eireland@concord.org>
2025-06-16 16:54:47 -0700

    Adds highlight button to text tile toolbar

--- src/components/tiles/text/toolbar/highlight-button.tsx
+++ src/components/tiles/text/toolbar/highlight-button.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Editor, EFormat, Node, Range, selectedNodesOfType, useSlate } from "@concord-consortium/slate-editor";
-
-// import { useLinkDialog } from "../dialog/use-link-dialog";
+import React, { useContext, useState } from "react";
+import { Editor, Range, useSlate }
+  from "@concord-consortium/slate-editor";
 import { TileToolbarButton } from "../../../toolbar/tile-toolbar-button";
 import { IToolbarButtonComponentProps } from "../../../toolbar/toolbar-button-manager";
+import { TextContentModelContext } from "../text-content-context";
 
 import HighlightToolIcon from "../../../../assets/icons/text/highlight-text-icon.svg";
 
@@ -11,22 +11,20 @@ export const HighlightButton = ({name}: IToolbarButtonComponentProps) => {
   const editor = useSlate();
   const { selection } = editor;
   const isCollapsed = selection ? Range.isCollapsed(selection) : true;
-  const selectedLinks = selectedNodesOfType(editor, EFormat.link);
-  const selectedLink = selectedLinks[0] || undefined;
-  const isSelected = !!selectedLink;
+  const isSelected = !!selection && !isCollapsed;
   const disabled = isCollapsed && !isSelected;
-  const text = isSelected
-    ? Node.string(selectedLink)
-    : selection
+  const textContent = useContext(TextContentModelContext);
+  const text = isSelected && selection
     ? Editor.string(editor, selection)
     : "";
-  // const [showModal] = useLinkDialog({ editor, selectedLink, text });
+  const [toggleHighlight, setToggleHighlight] = useState(false);
+
   const handleClick = (event: React.MouseEvent) => {
     event.preventDefault();
-    console.log("Highlight button clicked");
+    setToggleHighlight(!toggleHighlight);
   };
-  return(
-    <TileToolbarButton name={name} title="Highlight" disabled={disabled} selected={isSelected} onClick={handleClick}>
+  return (
+    <TileToolbarButton name={name} title="Highlight" disabled={disabled} selected={toggleHighlight} onClick={handleClick}>
       <HighlightToolIcon/>
     </TileToolbarButton>
   );
