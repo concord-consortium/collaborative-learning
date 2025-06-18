a210c5a2828b32c52d875ac95170cbd4becd93ab
eireland <eireland@concord.org>
2025-06-18 16:40:34 -0700

    Unhighlights text. Adds undo redo of highlights

--- src/components/tiles/text/toolbar/highlight-button.tsx
+++ src/components/tiles/text/toolbar/highlight-button.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { v4 as uuid } from "uuid";
+import { Path } from "slate";
 import { Editor, Range, Transforms, useSlate } from "@concord-consortium/slate-editor";
 import { HighlightsPlugin, kHighlightTextPluginName, kHighlightFormat, HighlightElement } from "../../../../plugins/text/highlights-plugin";
 import { TileToolbarButton } from "../../../toolbar/tile-toolbar-button";
@@ -34,9 +35,30 @@ export const HighlightButton = ({name}: IToolbarButtonComponentProps) => {
     Transforms.collapse(editor, { edge: "end" });
   }
 
+  const unwrapHighlightChip = (editor: Editor) => {
+    if (!editor.selection) return;
+    // Find the highlight chip node at selection
+    const [chipEntry] = Editor.nodes(editor, {
+      match: n => (n as any).type === kHighlightFormat,
+      at: editor.selection
+    });
+
+    if (chipEntry) {
+      const [chipNode, chipPath] = chipEntry as [HighlightElement, Path];
+      const chipNodeChild = chipNode.children[0] as { text: string };
+      const text = chipNodeChild.text || ""; // Assume the first child has the text
+
+      Transforms.insertNodes(editor, { text }, { at: chipPath });
+      Transforms.removeNodes(editor, { at: chipPath });
+    }
+  };
 
   const handleClick = (event: React.MouseEvent) => {
     event.preventDefault();
+    if (isHighlightedText) {
+      unwrapHighlightChip(editor);
+      return;
+    }
     if (!editor.selection || Range.isCollapsed(editor.selection)) return;
 
     const selectedText = Editor.string(editor, editor.selection);
