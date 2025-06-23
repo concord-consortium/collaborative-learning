338d5c80e036f4ec1d1adf4739b56a78062c28a0
eireland <eireland@concord.org>
2025-06-23 13:12:13 -0700

    PR Fix

--- src/components/tiles/text/toolbar/highlight-button.tsx
+++ src/components/tiles/text/toolbar/highlight-button.tsx
@@ -1,9 +1,7 @@
-import React, { useContext, useState } from "react";
-import { Editor, Range, useSlate }
-  from "@concord-consortium/slate-editor";
+import React, { useState } from "react";
+import { Range, useSlate } from "@concord-consortium/slate-editor";
 import { TileToolbarButton } from "../../../toolbar/tile-toolbar-button";
 import { IToolbarButtonComponentProps } from "../../../toolbar/toolbar-button-manager";
-import { TextContentModelContext } from "../text-content-context";
 
 import HighlightToolIcon from "../../../../assets/icons/text/highlight-text-icon.svg";
 
@@ -13,10 +11,6 @@ export const HighlightButton = ({name}: IToolbarButtonComponentProps) => {
   const isCollapsed = selection ? Range.isCollapsed(selection) : true;
   const isSelected = !!selection && !isCollapsed;
   const disabled = isCollapsed && !isSelected;
-  const textContent = useContext(TextContentModelContext);
-  const text = isSelected && selection
-    ? Editor.string(editor, selection)
-    : "";
   const [toggleHighlight, setToggleHighlight] = useState(false);
 
   const handleClick = (event: React.MouseEvent) => {
