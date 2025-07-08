702b649d1bb234d64d5148f049628d27e3b77554
Ethan McElroy <emcelroy@concord.org>
2025-07-08 19:53:05 -0400

    chore: update tests

--- src/lib/firebase-config.test.ts
+++ src/lib/firebase-config.test.ts
@@ -1,6 +1,6 @@
 export {}; // isolatedModules compatibility
 
-const getConfigWithEnv = (env: string | undefined) => {
+const getConfigWithEnv = (env?: string) => {
   jest.resetModules();
   jest.doMock("../utilities/url-params", () => ({
     urlParams: { firebaseEnv: env }
@@ -9,32 +9,47 @@ const getConfigWithEnv = (env: string | undefined) => {
   return require("./firebase-config").firebaseConfig();
 };
 
+const prodAuthDomain = "collaborative-learning-ec215.firebaseapp.com";
+const prodDatabaseURL = "https://collaborative-learning-ec215.firebaseio.com";
+const prodProjectId = "collaborative-learning-ec215";
+const stagingAuthDomain = "collaborative-learning-staging.firebaseapp.com";
+const stagingDatabaseURL = "https://collaborative-learning-staging-default-rtdb.firebaseio.com";
+const stagingProjectId = "collaborative-learning-staging";
+
 describe("firebaseConfig", () => {
   afterEach(() => {
     jest.resetModules();
   });
 
   it("returns the correct config for production by default", () => {
-    const config = getConfigWithEnv(undefined);
+    const config = getConfigWithEnv();
     expect(config.apiKey).toBeDefined();
-    expect(config.authDomain).toBe("collaborative-learning-ec215.firebaseapp.com");
-    expect(config.databaseURL).toBe("https://collaborative-learning-ec215.firebaseio.com");
-    expect(config.projectId).toBe("collaborative-learning-ec215");
+    expect(config.authDomain).toBe(prodAuthDomain);
+    expect(config.databaseURL).toBe(prodDatabaseURL);
+    expect(config.projectId).toBe(prodProjectId);
   });
 
   it("returns the correct config for production when specified by URL param", () => {
     const config = getConfigWithEnv("production");
     expect(config.apiKey).toBeDefined();
-    expect(config.authDomain).toBe("collaborative-learning-ec215.firebaseapp.com");
-    expect(config.databaseURL).toBe("https://collaborative-learning-ec215.firebaseio.com");
-    expect(config.projectId).toBe("collaborative-learning-ec215");
+    expect(config.authDomain).toBe(prodAuthDomain);
+    expect(config.databaseURL).toBe(prodDatabaseURL);
+    expect(config.projectId).toBe(prodProjectId);
   });
 
   it("returns the correct config for staging when specified by URL param", () => {
     const config = getConfigWithEnv("staging");
     expect(config.apiKey).toBeDefined();
-    expect(config.authDomain).toBe("collaborative-learning-staging.firebaseapp.com");
-    expect(config.databaseURL).toBe("https://collaborative-learning-staging-default-rtdb.firebaseio.com");
-    expect(config.projectId).toBe("collaborative-learning-staging");
+    expect(config.authDomain).toBe(stagingAuthDomain);
+    expect(config.databaseURL).toBe(stagingDatabaseURL);
+    expect(config.projectId).toBe(stagingProjectId);
+  });
+
+  it("returns the correct config for production when an unrecognized value is provided", () => {
+    const config = getConfigWithEnv("does-not-exist");
+    expect(config.apiKey).toBeDefined();
+    expect(config.authDomain).toBe(prodAuthDomain);
+    expect(config.databaseURL).toBe(prodDatabaseURL);
+    expect(config.projectId).toBe(prodProjectId);
   });
 });
