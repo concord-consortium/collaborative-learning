a9feedee9dff0dfa4d43406578a541669f59de32
Teale Fristoe <fristoe@gmail.com>
2026-01-11 19:21:20 -0800

    Improve pot sim summary.

--- shared/simulations/potentiometer-servo/potentiometer-servo.ts
+++ shared/simulations/potentiometer-servo/potentiometer-servo.ts
@@ -9,7 +9,8 @@ export const potentiometerAndServoValues = {
   },
   kResistReadingKey: {
     value: "resist_reading_key",
-    description: "The name of the resistance reading variable, which is based on the potentiometer angle."
+    description: "The name of the resistance reading variable, which is based on the potentiometer angle. "
+      + "This is the value accessible in the Dataflow program."
   },
   kServoAngleKey: {
     value: "servo_angle_key",
@@ -19,11 +20,12 @@ export const potentiometerAndServoValues = {
   maxPotAngle: { value: 270, description: "The maximum angle of the potentiometer in degrees. 10-bit ADC" },
   minResistReading: { value: 0, description: "The minimum resistance reading from the potentiometer" },
   maxResistReading: { value: 1023, description: "The maximum resistance reading from the potentiometer" },
-  minServoAngle: { value: 0, description: "The minimum angle of the servo in degrees" }
+  minServoAngle: { value: 0, description: "The minimum angle of the servo in degrees" },
+  maxServoAngle: { value: 180, description: "The maximum angle of the servo in degrees" }
 };
 
 export const potentiometerAndServoData: ISimulationData = {
   // eslint-disable-next-line max-len
-  description: `This simulation shows an Arduino microprocessor with inputs for battery power (Voltage) or a digital sensor such as a potentiometer dial, and outputs such as a connection to a servo which can have its angle set. As the simulated Arduino is programmed in a separate Dataflow tile, the instructions in the program are shown overlapping the Arduino to associate the electric microcircuits with the functions they perform.`,
+  description: `This simulation shows an Arduino microprocessor with inputs for battery power (Voltage) or a digital sensor such as a potentiometer dial, and outputs such as a connection to a servo which can have its angle set. The value of the potentiometer can be set via a slider. At its minimum value, it points to the bottom left, rotating clockwise to reach the bottom right at maximum value. At its minimum value, the servo points left, and it rotates counterclockwise to point right at maximum value. As the simulated Arduino is programmed in a separate Dataflow tile, the instructions in the program are shown overlapping the Arduino to associate the electric microcircuits with the functions they perform.`,
   values: potentiometerAndServoValues
 };
