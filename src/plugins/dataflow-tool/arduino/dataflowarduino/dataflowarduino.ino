ab6b54d29ae09c60c3b032975983b67c23c35720
Scott Cytacki <scytacki@concord.org>
2026-01-19 07:48:38 -0500

    fix issue with bare arduino usage
    The ccemgfsr sketch looks at the state of sensitivity button. If it stays high, then it goes into a infinite loop. With a bare arduino this pin is floating so it can be high or low. If it is high the arduino will block and stop reading.
    
    In theory this could be a problem with the spiker shield too. If there is no pull down resistor on this button it could be floating and just stay high all of the time. Arduino has internal pull up resistors, but this won't help since we need the "unpressed" state to be low not high.

--- src/plugins/dataflow-tool/arduino/dataflowarduino/dataflowarduino.ino
+++ src/plugins/dataflow-tool/arduino/dataflowarduino/dataflowarduino.ino
@@ -1,60 +1,40 @@
-/*
+  /*
   * ----------------------------------------------------------------------------------------------------
-  * Muscle SpikerShield Arduino UNO Code for Interface with Dataflow
-  * This code is up to date for use with the "Claw Pro R3"
-  * This code also supports the Potentiometer/Servo Assembly
-  *
   * This sketch allows for two way communication between Concord Consortium's Dataflow Tile and
-  * Backyard Brains' Muscle Spiker Shield & associated hardware.
+  * an Arduino controlling a Servo motor.
   *
   * Based on original script by Backyard Brains 2015, by Marcio Amorim and Stanislav Mircic
   * Adapted by Concord Consortium, 2023.
   *
-  * These two resources were very helpful for writing the serial reception code:
-  * https://www.programmingelectronics.com/serial-read/
-  * https://docs.arduino.cc/built-in-examples/strings/StringToInt
-  *
-  * Tested with Muscle SpikerShield V2.31 / CLAW PRO R3 / Dataflow
   * ----------------------------------------------------------------------------------------------------
   */
 
   #include <Servo.h>
-  #include <Adafruit_MLX90614.h>
+
+  // Function to calculate free RAM between heap and stack
+  // Returns number of free bytes
+  int freeMemory() {
+    extern int __heap_start, *__brkval;
+    int v;
+    return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+  }
 
   #define SERVO_PIN 2
   #define SENSITIVITY_BUTTON_PIN 7
   #define NUM_LED 6                           //number of LEDs in LED bar
-  #define MINIMUM_SERVO_UPDATE_TIME 100       //update servo position every 100ms
+  #define MINIMUM_SEND_TIME 100       //send outputs every 100ms
 
-  Servo Gripper;
-  Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+  Servo servo;
 
-  byte ledPins[] = {8, 9, 10, 11, 12, 13};
-
-  // Chosen sensitivity will appear as approximate max to user
-  int sensitivities[] = {200, 350, 520, 680, 840, 1000};
-  int lastSensitivitiesIndex = 3;
-
-  int emgSaturationValue = 0;
-  int emgReading;
-  int fsrReading;
   int a1Reading;
-  float tmpReading;
-  byte ledbarHeight = 0;                      //temporary variable for led bar height
 
   unsigned long oldTime = 0;                  //timestamp of last servo angle update (ms)
   int oldDegrees = 0;                         //old value of angle for servo
   int newDegree;                              //new value of angle for servo
 
-  // these will be used to assmble strings for Dataflow
-  String emgId = "emg";
-  String fsrId = "fsr";
-  String tmpId = "tmp";
+  // these will be used to assemble strings for Dataflow
   String a1Id = "a1";
   String kvSeparator = ":";
-  String emgStringOut = "";
-  String fsrStringOut = "";
-  String tmpStringOut = "";
   String a1StringOut = "";
 
   const int BUFFER_SIZE = 4;                  // Accommodate <=3 digit angle, plus delimiter
@@ -62,22 +42,37 @@
 
   const unsigned int MAX_ANGLE_BYTE_LENGTH = 4;
 
+  // Pre-allocate string capacity to avoid heap fragmentation
+  // Format: "xxx:1234" = 3 char id + 1 colon + up to 4 digits = ~10 chars
+  // tmpReading is float, so allow more space: "tmp:-123.45" = ~12 chars
+  const int STRING_RESERVE_SIZE = 16;
+
+  // Memory tracking
+  // Note: this is disabled to avoid sending unexpected messages to Dataflow
+  String memId = "mem";
+  String memStringOut = "";
+  int initialFreeMemory = 0;
+
   /* setup */
   void setup(){
     Serial.begin(9600);
-    Gripper.attach(SERVO_PIN);
-    mlx.begin();
-    pinMode(SENSITIVITY_BUTTON_PIN, INPUT);
-    for(int i = 0; i < NUM_LED; i++){
-      pinMode(ledPins[i], OUTPUT);
-    }
-    emgSaturationValue = sensitivities[lastSensitivitiesIndex];
+    servo.attach(SERVO_PIN);
+
+    // Reserve memory for output strings to prevent fragmentation
+    a1StringOut.reserve(STRING_RESERVE_SIZE);
+    memStringOut.reserve(STRING_RESERVE_SIZE);
+
+    // Record initial free memory for leak detection
+    initialFreeMemory = freeMemory();
+    // Probably want to remove this, because the serial reader won't expect it.
+    // Serial.print("Initial free memory: ");
+    // Serial.println(initialFreeMemory);
   }
 
   /* main loop */
   void loop()
   {
-    // 1 handle incoming directions for Gripper and pass to Gripper
+    // 1 handle incoming directions for Servo and pass to Servo
     while (Serial.available() > 0){
       static char message[MAX_ANGLE_BYTE_LENGTH];
       static unsigned int message_pos = 0;
@@ -92,67 +87,29 @@
       // otherwise we have reached a newline or end of max length, so that's the whole message
       else {
         message[message_pos] = '\0';
-        String asString = String(message);
-        Gripper.write(asString.toInt());
+        servo.write(atoi(message));
         message_pos = 0;
       }
     }
 
-    // 2 handle any clicks of the sensitivity button - adjust sensitivity
-    if (digitalRead(SENSITIVITY_BUTTON_PIN)){
-      for(int j = 0; j < NUM_LED; j++){
-        digitalWrite(ledPins[j], LOW);
-      }
-
-      //increment sensitivity index
-      lastSensitivitiesIndex++;
-      if(lastSensitivitiesIndex==NUM_LED){
-        lastSensitivitiesIndex = 0;
-      }
-
-      //light up LED at lastSensitivitiesIndex position for visual feedback
-      emgSaturationValue = sensitivities[lastSensitivitiesIndex];
-      digitalWrite(ledPins[lastSensitivitiesIndex], HIGH);
-
-      //wait user to release button
-      while (digitalRead(SENSITIVITY_BUTTON_PIN)){
-        delay(10);
-      }
-      delay(100);
-    }
-
-    // 3 Collect EMG, Force, and Temperature readings
-    // (note that A2 is pin used for FSR in newer Muscle SpikerShield)
-    emgReading = analogRead(A0);
+    // 2 Collect A1 readings
     a1Reading = analogRead(A1);
-    fsrReading = analogRead(A2);
-    tmpReading = mlx.readObjectTempC();
 
-    // 4 Turn off LEDs, then light up 1 -6 of them to reflect sensitivity
-    for(int j = 0; j < NUM_LED; j++){
-      digitalWrite(ledPins[j], LOW);
-    }
-    emgReading= constrain(emgReading, 30, emgSaturationValue);
-    ledbarHeight = map(emgReading, 30, emgSaturationValue, 0, NUM_LED);
-    for(int k = 0; k < ledbarHeight; k++){
-      digitalWrite(ledPins[k], HIGH);
-    }
-
-    // 5 if enough time has passed send readings to serial out (where Dataflow will find it)
-    if (millis() - oldTime > MINIMUM_SERVO_UPDATE_TIME){
+    // 3 if enough time has passed send readings to serial out (where Dataflow will find it)
+    if (millis() - oldTime > MINIMUM_SEND_TIME){
       oldTime = millis();
       oldDegrees = newDegree;
 
       // assemble keyed strings so Dataflow knows what is what
-      emgStringOut = String(emgId + kvSeparator + emgReading);
-      fsrStringOut = String(fsrId + kvSeparator + fsrReading);
-      tmpStringOut = String(tmpId + kvSeparator + tmpReading);
-      a1StringOut = String(a1Id + kvSeparator + a1Reading);
+      // Using assignment to reuse pre-allocated memory from reserve()
+      a1StringOut = a1Id + kvSeparator + a1Reading;
 
       // send to Dataflow via serial out
-      Serial.println(emgStringOut);
-      Serial.println(fsrStringOut);
-      Serial.println(tmpStringOut);
       Serial.println(a1StringOut);
+
+      // This is disabled because some versions of the CLUE code can't handle unexpected messages.
+      // Output current free memory for leak detection
+      // memStringOut = memId + kvSeparator + freeMemory();
+      // Serial.println(memStringOut);
     }
 }
