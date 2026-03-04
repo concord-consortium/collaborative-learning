5b8b24e67352805b214396a58c557cffd2f0aade
Teale Fristoe <fristoe@gmail.com>
2026-03-04 14:45:07 -0800

    Start fleshing out data setup section.

--- src/plugins/wave-runner/components/wave-runner-tile.tsx
+++ src/plugins/wave-runner/components/wave-runner-tile.tsx
@@ -8,7 +8,7 @@ import "./wave-runner.scss";
 
 export const WaveRunnerToolComponent: React.FC<ITileProps> = observer(() => {
   const { width: containerWidth, ref: containerRef } = useResizeDetector();
-  const vertical = !containerWidth || containerWidth < 450;
+  const vertical = !containerWidth || containerWidth < 600;
 
   return (
     <div className="tile-content wave-runner-tool">
@@ -18,6 +18,30 @@ export const WaveRunnerToolComponent: React.FC<ITileProps> = observer(() => {
         <div className={classNames("wave-runner-sections", { vertical, horizontal: !vertical })}>
           <div className="wave-runner-section data-setup">
             <div className="wave-runner-section-title">Data Setup</div>
+            <div className="wave-runner-field-row">
+              <div className="wave-runner-field">
+                <label className="wave-runner-field-label">Station</label>
+                <select className="wave-runner-dropdown">
+                  <option>Choose a station</option>
+                </select>
+              </div>
+              <div className="wave-runner-field">
+                <label className="wave-runner-field-label">Model</label>
+                <select className="wave-runner-dropdown">
+                  <option>Choose a model</option>
+                </select>
+              </div>
+            </div>
+            <div className="wave-runner-field-row">
+              <div className="wave-runner-field">
+                <label className="wave-runner-field-label">Start Date and Time</label>
+                <input className="wave-runner-datetime" type="datetime-local" readOnly />
+              </div>
+              <div className="wave-runner-field">
+                <label className="wave-runner-field-label">End Date and Time</label>
+                <input className="wave-runner-datetime" type="datetime-local" readOnly />
+              </div>
+            </div>
           </div>
           <div className="wave-runner-section status-and-output">
             <div className="wave-runner-section-title">Status and Output</div>
