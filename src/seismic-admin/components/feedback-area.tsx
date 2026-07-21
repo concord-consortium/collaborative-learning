ee3aa72a527a15c5ea00dabdd5140c3d3b9e80bf
Teale Fristoe <fristoe@gmail.com>
2026-07-21 07:10:53 -0700

    Use classNames instead of clsx.

--- src/seismic-admin/components/feedback-area.tsx
+++ src/seismic-admin/components/feedback-area.tsx
@@ -1,14 +1,14 @@
+import classNames from "classnames";
 import { observer } from "mobx-react-lite";
 import React from "react";
 import { useSeismicAdminStore } from "../hooks/use-seismic-admin-stores";
 import "./feedback-area.scss";
-import clsx from "clsx";
 
 export const FeedbackArea = observer(function FeedbackArea() {
   const store = useSeismicAdminStore();
 
   return (
-    <div className={clsx("feedback-area", { busy: store.isBusy })} role="status">
+    <div className={classNames("feedback-area", { busy: store.isBusy })} role="status">
       {store.feedback}
     </div>
   );
